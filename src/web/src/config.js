d619bd7ae926ec5593f7525c76b005a1cb6c469d
Max Parker <maxrparker@gmail.com>
2022-11-08 01:59:54 -0700

    v bump 1.1.2

--- src/web/src/config.js
+++ src/web/src/config.js
@@ -71,4 +71,4 @@ export const sections = [
 export const environment = process.env.NODE_ENV;
 export const apiBaseUrl =
 	process.env.NODE_ENV == 'production' ? '' : 'http://localhost:3000';
-export const appVersion = '1.1.1';
+export const appVersion = '1.1.2';
