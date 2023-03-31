6013ee46cf8a4b00e0927d782a389f8312cc05c3
Max Parker <maxrparker@gmail.com>
2023-03-31 15:11:51 -0700

    TravCom routes. I forgot to push these..

--- src/api/routes/travCom-router.ts
+++ src/api/routes/travCom-router.ts
@@ -9,65 +9,31 @@ const db = knex(TRAVCOM_DB_CONFIG);
 export const travComRouter = express.Router();
 
 travComRouter.get("/ARInvoices", RequiresAuth, async function (req: Request, res: Response) {
-  const result = await db("ARInvoices").select(
-    "InvoiceID",
-    "InvoiceNumber",
-    "ProfileNumber",
-    "ProfileName",
-    "Department",
-    "BookingDate",
-    "SystemDate",
-    "Description",
-    "BookingAgent",
-    "InvoiceRemarks"
-  );
+  const result = await db("dbo.ARInvoicesNoHealth").select();
   res.status(200).json({ data: result });
 });
 
 travComRouter.get("/ARInvoices/:id", RequiresAuth, async function (req: Request, res: Response) {
-  const result = await db("ARInvoices")
-    .where({ InvoiceID: req.params.id })
-    .select(
-      "InvoiceDetailID",
-      "InvoiceID",
-      "TransactionType",
-      "VendorNumber",
-      "VendorName",
-      "ProductCode",
-      "PassengerName",
-      "TicketNumber",
-      "PublishedFair",
-      "SellingFair",
-      "ReferenceFair",
-      "LowFair",
-      "Tax1",
-      "GrossAmount",
-      "CommissionAmount",
-      "VatOnCommission",
-      "FreeFieldA",
-      "TravelDate",
-      "ReturnDate",
-      "NumberOfDays",
-      "CityCode",
-      "AddedBy"
-    );
+  const result = await db("dbo.ARInvoicesNoHealth").where({ InvoiceID: req.params.id }).select();
+  res.status(200).json({ data: result });
+});
+
+travComRouter.get("/ARInvoiceDetails", RequiresAuth, async function (req: Request, res: Response) {
+  const result = await db("dbo.ARInvoicesDetailsNoHealth").select();
+  res.status(200).json({ data: result });
+});
+
+travComRouter.get("/ARInvoiceDetails/:id", RequiresAuth, async function (req: Request, res: Response) {
+  const result = await db("dbo.ARInvoicesDetailsNoHealth").where({ InvoiceID: req.params.id }).select();
   res.status(200).json({ data: result });
 });
 
 travComRouter.get("/segments", RequiresAuth, async function (req: Request, res: Response) {
-  const result = await db("segments").select(
-    "segmentID",
-    "invoiceID",
-    "invoiceDetailsID",
-    "LegNumber",
-    "DepartureCityCode",
-    "DepartureInfo",
-    "ArrivalCityCode",
-    "ArrivalInfo",
-    "AirlineCode",
-    "FlightNumber",
-    "ClassOfService",
-    "FareBasis"
-  );
+  const result = await db("segments").select();
+  res.status(200).json({ data: result });
+});
+
+travComRouter.get("/segments/:id", RequiresAuth, async function (req: Request, res: Response) {
+  const result = await db("segments").where({ InvoiceID: req.params.id }).select();
   res.status(200).json({ data: result });
 });
