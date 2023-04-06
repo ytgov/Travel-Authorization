a6ddad7897872e1c63880677923aff22fc085b06
Max Parker <maxrparker@gmail.com>
2023-04-06 14:09:53 -0700

    Role changed to roles.

--- src/api/data/migrations/015_role.ts
+++ src/api/data/migrations/015_role.ts
@@ -1,12 +1,12 @@
 import * as knex from "knex";
 
 exports.up = function (knex: knex.Knex, Promise: any) {
-  return knex.schema.createTable("role", function (t) {
+  return knex.schema.createTable("roles", function (t) {
     t.increments("id").notNullable().primary();
     t.string("name").notNullable();
   });
 };
 
 exports.down = function (knex: knex.Knex, Promise: any) {
-  return knex.schema.dropTable("role");
+  return knex.schema.dropTable("roles");
 };
