7fbbce10e9b3c54902e0cedfe0767dd4de1af60e
Max Parker <maxrparker@gmail.com>
2023-04-06 14:02:40 -0700

    Have to change this to role, push it, take down role and then push roles again...

--- src/api/data/migrations/015_role.ts
+++ src/api/data/migrations/015_role.ts
@@ -1,12 +1,12 @@
 import * as knex from "knex";
 
 exports.up = function (knex: knex.Knex, Promise: any) {
-  return knex.schema.createTable("roles", function (t) {
+  return knex.schema.createTable("role", function (t) {
     t.increments("id").notNullable().primary();
     t.string("name").notNullable();
   });
 };
 
 exports.down = function (knex: knex.Knex, Promise: any) {
-  return knex.schema.dropTable("roles");
+  return knex.schema.dropTable("role");
 };
