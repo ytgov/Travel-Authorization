bd62a400e1f3084187c9bec6eda525f97d92be92
Marlen Brunner <klondikemarlen@gmail.com>
2023-09-26 09:40:26 -0700

    :construction: Add accommodation types to UI.
    Also fix some value binding.

--- src/web/src/modules/travelForm/views/travel-form-create/details-tab/DetailsFormCard.vue
+++ src/web/src/modules/travelForm/views/travel-form-create/details-tab/DetailsFormCard.vue
@@ -9,16 +9,18 @@
         ref="form"
         lazy-validation
       >
+        <!-- Depending on in territory flag we will load a different list of destinations -->
         <v-checkbox
-          :input-value="request.allTravelWithinTerritory"
+          :value="request.allTravelWithinTerritory"
           label="In Territory"
           dense
           required
           @change="updateAllTravelWithinTerritory"
         >
         </v-checkbox>
+        <!-- Depending on trip type we will show a completely different form here -->
         <v-select
-          :input-value="tripType"
+          :value="tripType"
           :items="tripTypes"
           :rules="[required]"
           label="Trip Type"
@@ -27,7 +29,16 @@
           outlined
           @change="updateTripType"
         ></v-select>
-        <!-- Depending on trip type we will show a completely different form here -->
+        <!-- If accommodation type is other, support text field entry -->
+        <v-select
+          v-model="accommodationType"
+          :items="accommodationTypes"
+          :rules="[required]"
+          label="Type of Accommodation"
+          dense
+          background-color="white"
+          outlined
+        ></v-select>
         <v-row>
           <v-col
             cols="12"
@@ -52,6 +63,8 @@ export default {
     required: (v) => !!v || "This field is required",
     tripTypes: ["Round Trip", "One Way", "Muli-Destination"],
     tripType: "One Way",
+    accommodationType: "Hotel",
+    accommodationTypes: ["Hotel", "Private", "Other:"],
   }),
   computed: {
     ...mapState("travelForm", ["request", "destinations"]),
@@ -66,11 +79,13 @@ export default {
   },
   methods: {
     ...mapActions("travelForm", ["loadDestinations"]),
-    updateAllTravelWithinTerritory() {
-      // trigger load of different destination list
+    updateAllTravelWithinTerritory(value) {
+      // TODO: trigger load of different destination list
+      this.allTravelWithinTerritory = value
     },
-    updateTripType() {
-      // update one of several boolean values on the form object
+    updateTripType(value) {
+      // TODO: update one of several boolean values on the form object
+      this.tripType = value
     },
   },
 }
