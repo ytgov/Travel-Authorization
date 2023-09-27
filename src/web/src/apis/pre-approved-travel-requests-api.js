b1100911dae293c07f62962cf15a43f199793ed2
Marlen Brunner <klondikemarlen@gmail.com>
2023-09-27 11:09:34 -0700

    :construction: Add front-end wiring for loading pre-approved travel requests.

---
+++ src/web/src/apis/pre-approved-travel-requests-api.js
@@ -0,0 +1,9 @@
+import http from "@/apis/http-client"
+
+export const preApprovedTravelRequestsApi = {
+  list({ where, ...otherParams } = {}) {
+    return http.get("/api/pre-approved-travel-requests", { params: { where, ...otherParams } }).then(({ data }) => data)
+  },
+}
+
+export default preApprovedTravelRequestsApi
--- src/web/src/modules/travelForm/views/travel-form-create/details-tab/ApprovalsFormCard.vue
+++ src/web/src/modules/travelForm/views/travel-form-create/details-tab/ApprovalsFormCard.vue
@@ -9,20 +9,91 @@
         ref="form"
         lazy-validation
       >
-        TODO
+        <v-text-field
+          :value="estimatedCost"
+          :rules="[required]"
+          label="Estimated Cost"
+          background-color="white"
+          prefix="$"
+          disabled
+          dense
+          outlined
+          required
+        ></v-text-field>
+        <v-text-field
+          v-model="travelAdvanceInDollars"
+          :rules="[required, isInteger]"
+          label="Travel Advance"
+          background-color="white"
+          prefix="$"
+          dense
+          outlined
+          required
+        ></v-text-field>
+        <v-select
+          v-model="request.preappId"
+          :items="preApprovedTravelRequests"
+          :loading="loadingPreApprovedTravelRequests"
+          :rules="[required]"
+          label="Pre-approved Travel Request?"
+          background-color="white"
+          dense
+          outlined
+          required
+        ></v-select>
       </v-form>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
+import { mapState } from "vuex"
+import { isEmpty } from "lodash"
+
+import preApprovedTravelRequestsApi from "@/apis/pre-approved-travel-requests-api"
+
 export default {
   name: "ApprovalsFormCard",
   data: () => ({
     required: (v) => !!v || "This field is required",
+    isInteger: (v) => v == 0 || Number.isInteger(Number(v)) || "This field must be a number",
+    estimatedCost: 1230.45, // TODO: figure out what would be required to generate this value
+    preApprovedTravelRequests: [],
+    loadingPreApprovedTravelRequests: false,
   }),
-  computed: {},
-  async mounted() {},
-  methods: {},
+  computed: {
+    ...mapState("travelForm", ["request"]),
+    travelAdvanceInDollars: {
+      get() {
+        return Math.ceil(this.request.travelAdvanceInCents / 100.0)
+      },
+      set(value) {
+        this.request.travelAdvanceInCents = Math.ceil(value * 100)
+      },
+    },
+  },
+  async mounted() {
+    await this.loadPreApprovedTravelRequests()
+  },
+  methods: {
+    loadPreApprovedTravelRequests() {
+      if (isEmpty(this.request.department)) {
+        console.warn(
+          "This request doesn't have an associated department ... yet? " +
+            "Consider loading the user before calling this action."
+        )
+      }
+
+      this.loadingPreApprovedTravelRequests = true
+      return preApprovedTravelRequestsApi
+        .list({ where: { department: this.request.department } })
+        .then(({ preApprovedTravelRequests }) => {
+          this.preApprovedTravelRequests = preApprovedTravelRequests
+        })
+        .finally(() => {
+          this.loadingPreApprovedTravelRequests = true
+        })
+    },
+  },
 }
 </script>
