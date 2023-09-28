8d1729fe11a7362b2b893fa4d65b31d39565648f
Marlen Brunner <klondikemarlen@gmail.com>
2023-09-28 11:10:02 -0700

    :sparkles: Add qs param encoding to base axios.

--- src/web/src/apis/http-client.js
+++ src/web/src/apis/http-client.js
@@ -1,20 +1,24 @@
-import axios from "axios";
+import axios from "axios"
+import qs from "qs"
 
-import { getInstance } from "@/auth";
-import { apiBaseUrl } from "@/config";
+import { getInstance } from "@/auth"
+import { apiBaseUrl } from "@/config"
 
 export const httpClient = axios.create({
   baseURL: apiBaseUrl,
   headers: {
     "Content-Type": "application/json",
   },
-});
+  paramsSerializer: (params) => {
+    return qs.stringify(params, { arrayFormat: "brackets" })
+  },
+})
 
-httpClient.interceptors.request.use(async config => {
-  const auth0 = await getInstance();
-  const accessToken = await auth0.getTokenSilently();
-  config.headers["Authorization"] = `Bearer ${accessToken}`;
-  return config;
-});
+httpClient.interceptors.request.use(async (config) => {
+  const auth0 = await getInstance()
+  const accessToken = await auth0.getTokenSilently()
+  config.headers["Authorization"] = `Bearer ${accessToken}`
+  return config
+})
 
-export default httpClient;
+export default httpClient
