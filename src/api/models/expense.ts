71828b71e7669135f04ae49be66b10d96e09c4c1
Marlen Brunner <klondikemarlen@gmail.com>
2023-10-03 11:37:16 -0700

    :sparkles: Add expense model.
    Expense model is _also_ the "estimate" model, just with a type of "Estimate".
    Maybe this is an implementation of single table inheritance?

---
+++ src/api/models/expense.ts
@@ -0,0 +1,39 @@
+import BaseModel from "./base-model"
+
+// TODO: replace this with a boolean of isEstimate or
+// move estimates to there own table.
+// It's also possible that this is a single table inheritance model,
+// and there should be two models, one for each "type".
+export enum ExpenseTypes {
+  Estimates = "Estimates",
+  Expenses = "Expenses"
+}
+
+export class Expense extends BaseModel {
+  id: number
+  taid: number
+  description: string
+  date: Date | null
+  cost: number
+  currency: string
+  type: string
+  receiptImage: Buffer | null
+  fileSize: number | null
+  fileName: string | null
+
+  constructor(attributes: Pick<Expense, "id" | "taid" | "description" | "cost" | "currency" | "type"> & Partial<Expense>) {
+    super()
+    this.id = attributes.id
+    this.taid = attributes.taid
+    this.description = attributes.description
+    this.date = attributes.date || null
+    this.cost = attributes.cost
+    this.currency = attributes.currency
+    this.type = attributes.type
+    this.receiptImage = attributes.receiptImage || null
+    this.fileSize = attributes.fileSize || null
+    this.fileName = attributes.fileName || null
+  }
+}
+
+export default Expense
