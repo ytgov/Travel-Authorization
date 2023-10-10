32f9a446a6307632f7f2f59ee67f1ae61b9d6c97
Marlen Brunner <klondikemarlen@gmail.com>
2023-10-10 12:12:54 -0700

    :recycle: Add accommodation type to stops model.

--- src/api/models/stop.ts
+++ src/api/models/stop.ts
@@ -3,19 +3,21 @@ import BaseModel from "./base-model"
 export class Stop extends BaseModel {
   id: number
   taid: number // TODO: taid === forms.id, rename this column to form_id
-  locationId?: number | null
-  departureDate?: Date | null
-  departureTime?: string | null
-  transport?: string | null
+  locationId: number | null
+  departureDate: Date | null
+  departureTime: string | null
+  transport: string | null
+  accommodationType: string | null
 
   constructor(attributes: Pick<Stop, "id" | "taid"> & Partial<Stop>) {
     super()
     this.id = attributes.id
     this.taid = attributes.taid
-    this.locationId = attributes.locationId
-    this.departureDate = attributes.departureDate
-    this.departureTime = attributes.departureTime
-    this.transport = attributes.transport
+    this.locationId = attributes.locationId || null
+    this.departureDate = attributes.departureDate || null
+    this.departureTime = attributes.departureTime || null
+    this.transport = attributes.transport || null
+    this.accommodationType = attributes.accommodationType || null
   }
 }
 
