ad07fa2bb70ff37978fba8ab5b35a610f8faf752
Marlen Brunner <klondikemarlen@gmail.com>
2023-10-10 16:06:28 -0700

    :lock: Add policy check to estimate generation controller.

--- src/api/controllers/forms/estimates/generate-controller.ts
+++ src/api/controllers/forms/estimates/generate-controller.ts
@@ -1,10 +1,32 @@
-import BaseController from "../../base-controller";
+import BaseController from "../../base-controller"
+
+import { Expense, Form } from "../../../models"
+import { ExpensesPolicy } from "../../../policies"
 
 export class GenerateController extends BaseController {
-  create() {
+  async create() {
+    const expense = await this.buildExpense()
+    const policy = this.buildPolicy(expense)
+    if (!policy.create()) {
+      return this.response
+        .status(403)
+        .json({ message: "You are not authorized to create this expense." })
+    }
+
     return this.response.status(201).json({
-      message: "TODO: generate estimates"
+      message: "TODO: generate estimates",
     })
+  }
+
+  private async buildExpense() {
+    const attributes = this.request.body
+    const { taid: formId } = attributes
+    const form = await Form.findByPk(formId)
+    return new Expense({ ...attributes, form })
+  }
+
+  private buildPolicy(record: Expense): ExpensesPolicy {
+    return new ExpensesPolicy(this.currentUser, record)
   }
 }
 
