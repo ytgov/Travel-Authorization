4098f93752fb10cf1ad896b0aeb3278df6ef712c
Marlen Brunner <klondikemarlen@gmail.com>
2023-10-16 13:53:45 -0700

    :butterfly: Rename destinations table to locations and add timestamp columns.

---
+++ api/src/data/migrations/20231016203328_rename-destinations-to-locations.ts
@@ -0,0 +1,16 @@
+import { Knex } from "knex"
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.renameTable("destinations", "locations")
+  await knex.schema.table("locations", (table) => {
+    table.timestamps(true, true)
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.table("locations", (table) => {
+    table.dropColumn("created_at")
+    table.dropColumn("updated_at")
+  })
+  await knex.schema.renameTable("locations", "destinations")
+}
