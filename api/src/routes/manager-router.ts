5ca3e1f6db9ac48a608d5bad2ec6044dc7bd98d1
Marlen Brunner <klondikemarlen@gmail.com>
2023-10-17 11:57:39 -0700

    :art: Prettify the manager router for easier reading.

--- api/src/routes/manager-router.ts
+++ api/src/routes/manager-router.ts
@@ -1,55 +1,57 @@
-import express, { Request, Response } from "express";
-import { ReturnValidationErrors } from "../middleware";
-import { DB_CONFIG } from "../config";
-import knex from "knex";
-import { UserService } from "../services";
-import { v4 as uuid } from "uuid";
-import * as formHelper from "../utils/formHelper";
-import { auth } from "express-openid-connect";
+import express, { Request, Response } from "express"
+import knex from "knex"
 
-const db = knex(DB_CONFIG);
+import { ReturnValidationErrors } from "@/middleware"
+import { DB_CONFIG } from "@/config"
+import { UserService } from "@/services"
 
-export const managerRouter = express.Router();
-const userService = new UserService();
+const db = knex(DB_CONFIG)
 
-managerRouter.get("/forms/:formId", ReturnValidationErrors, async function (req: Request, res: Response) {
-  try {
-    let user = await userService.getByEmail(req.user.email);
-    let auth = await db("forms")
-      .select("*")
-      .where("supervisorEmail", "=", user.email)
-      .andWhere("formid", "=", req.params.formId)
-      .first();
+export const managerRouter = express.Router()
+const userService = new UserService()
 
-    if (auth) {
-      auth.stops = await db("stops").select("*").where("taid", "=", auth.id);
+managerRouter.get(
+  "/forms/:formId",
+  ReturnValidationErrors,
+  async function (req: Request, res: Response) {
+    try {
+      let user = await userService.getByEmail(req.user.email)
+      let auth = await db("forms")
+        .select("*")
+        .where("supervisorEmail", "=", user.email)
+        .andWhere("formid", "=", req.params.formId)
+        .first()
 
-      res.status(200).json(auth);
-    } else {
-      res.status(404).json("Form not found");
+      if (auth) {
+        auth.stops = await db("stops").select("*").where("taid", "=", auth.id)
+
+        res.status(200).json(auth)
+      } else {
+        res.status(404).json("Form not found")
+      }
+
+      res.status(200).json(auth)
+    } catch (error: any) {
+      console.log(error)
+      res.status(500).json("Internal Server Error")
     }
-
-    res.status(200).json(auth);
-  } catch (error: any) {
-    console.log(error);
-    res.status(500).json("Internal Server Error");
   }
-});
+)
 
 managerRouter.get("/forms", ReturnValidationErrors, async function (req: Request, res: Response) {
   try {
-    let user = await userService.getByEmail(req.user.email);
+    let user = await userService.getByEmail(req.user.email)
     // let auth = await db("forms").select("*").where("supervisorEmail", "=", user.email);
-    let auth = await db("forms").select("*").where("supervisorEmail", "=", "user.email");
+    let auth = await db("forms").select("*").where("supervisorEmail", "=", "user.email")
 
     for (let index = 0; index < auth.length; index++) {
-      auth[index].stops = await db("stops").select("*").where("taid", "=", auth[index].id);
-      let departureDate = await db("stops").min("departureDate").where("taid", "=", auth[index].id);
-      auth[index].departureDate = departureDate[0].min;
+      auth[index].stops = await db("stops").select("*").where("taid", "=", auth[index].id)
+      let departureDate = await db("stops").min("departureDate").where("taid", "=", auth[index].id)
+      auth[index].departureDate = departureDate[0].min
     }
-    res.status(200).json(auth);
+    res.status(200).json(auth)
   } catch (error: any) {
-    console.log(error);
-    res.status(500).json("Internal Server Error");
+    console.log(error)
+    res.status(500).json("Internal Server Error")
   }
-});
+})
