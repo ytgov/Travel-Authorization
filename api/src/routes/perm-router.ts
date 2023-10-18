ad65fc11e5e8b7fe3bec3e664ad4deacc8d47cc5
Marlen Brunner <klondikemarlen@gmail.com>
2023-10-17 17:01:30 -0700

    :art: Prettify "perm" router.
    I think it short for permissions.

--- api/src/routes/perm-router.ts
+++ api/src/routes/perm-router.ts
@@ -1,29 +1,30 @@
-import express, { Request, Response } from "express";
-import { ReturnValidationErrors } from "../middleware";
-import { DB_CONFIG } from "../config";
-import knex from "knex";
-import { UserService } from "../services";
+import express, { Request, Response } from "express"
+import knex from "knex"
 
-const db = knex(DB_CONFIG);
+import { ReturnValidationErrors } from "@/middleware"
+import { DB_CONFIG } from "@/config"
+import { UserService } from "@/services"
 
-export const permRouter = express.Router();
-const userService = new UserService();
+const db = knex(DB_CONFIG)
+
+export const permRouter = express.Router()
+const userService = new UserService()
 
 permRouter.get("/", ReturnValidationErrors, async function (req: Request, res: Response) {
   try {
-    let users = await userService.getAccessFor("Max.parker@yukon.ca");
-    res.status(200).json(users);
+    let users = await userService.getAccessFor("Max.parker@yukon.ca")
+    res.status(200).json(users)
   } catch (error: any) {
-    console.log(error);
-    res.status(500).json("Internal Server Error");
+    console.log(error)
+    res.status(500).json("Internal Server Error")
   }
-});
+})
 
 permRouter.post("/", ReturnValidationErrors, async function (req: Request, res: Response) {
   try {
-    await db.transaction(async trx => {
-      let user = await userService.getByEmail(req.user.email);
-      let stops = req.body.stops;
+    await db.transaction(async (trx) => {
+      let user = await userService.getByEmail(req.user.email)
+      let stops = req.body.stops
       let authInsert = {
         firstname: req.body.firstname,
         lastname: req.body.lastname,
@@ -35,48 +36,48 @@ permRouter.post("/", ReturnValidationErrors, async function (req: Request, res: 
         datebacktowork: req.body.datebacktowork,
         userid: user.id,
         preappid: 0,
-        approved: false
-      };
+        approved: false,
+      }
 
-      let id = await db("auth").insert(authInsert, "taid").transacting(trx).returning("taid");
+      let id = await db("auth").insert(authInsert, "taid").transacting(trx).returning("taid")
 
       for (let index = 0; index < stops.length; index++) {
         let stop = {
           taid: id[0],
           destination: stops[index].destination,
           arrivaldate: stops[index].arrivaldate,
-          departuredate: stops[index].departuredate
-        };
-        await db("stops").insert(stop).transacting(trx);
+          departuredate: stops[index].departuredate,
+        }
+        await db("stops").insert(stop).transacting(trx)
       }
-    });
+    })
   } catch (error: any) {
-    console.log(error);
-    res.status(500).json("Insert failed");
+    console.log(error)
+    res.status(500).json("Insert failed")
   }
-});
+})
 
 permRouter.delete("/:id", ReturnValidationErrors, async function (req: Request, res: Response) {
   try {
-    let result = await db("Forms").where("id", "=", req.params.id).del();
+    let result = await db("Forms").where("id", "=", req.params.id).del()
     if (result) {
-      res.status(200).json("Delete successful");
-      console.log("Delete successful", req.params.id);
+      res.status(200).json("Delete successful")
+      console.log("Delete successful", req.params.id)
     } else {
-      res.status(500).json("Delete failed");
+      res.status(500).json("Delete failed")
     }
   } catch (error: any) {
-    res.status(500).json("Delete failed");
+    res.status(500).json("Delete failed")
   }
-});
+})
 
 permRouter.put("/:id", ReturnValidationErrors, async function (req: Request, res: Response) {
   try {
-    await db("Forms").where("id", "=", req.params.id).update(req.body);
-    console.log("Entry updated", req.body);
-    res.status(200).json("Updated successful");
+    await db("Forms").where("id", "=", req.params.id).update(req.body)
+    console.log("Entry updated", req.body)
+    res.status(200).json("Updated successful")
   } catch (error: any) {
-    console.log(error.name, error.detail);
-    res.status(500).json("Update failed");
+    console.log(error.name, error.detail)
+    res.status(500).json("Update failed")
   }
-});
+})
