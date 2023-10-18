387d10d94b3007beb3036216606186f6d0fcbdd5
Marlen Brunner <klondikemarlen@gmail.com>
2023-10-18 11:35:42 -0700

    :beetle: Fix up formatting after bad format.
    Using prettier instead of volar, so I can get the single attribute per line to work.

--- web/src/modules/travelForm/views/travel-form-edit/details-tab/DetailsFormCard.vue
+++ web/src/modules/travelForm/views/travel-form-edit/details-tab/DetailsFormCard.vue
@@ -1,17 +1,43 @@
 <template>
-  <v-card elevation="2" class="default">
+  <v-card
+    elevation="2"
+    class="default"
+  >
     <v-card-title> Details </v-card-title>
     <v-card-text>
-      <v-form ref="form" lazy-validation>
+      <v-form
+        ref="form"
+        lazy-validation
+      >
         <v-row>
-          <v-col cols="12" md="2">
+          <v-col
+            cols="12"
+            md="2"
+          >
             <!-- Depending on in territory flag we will load a different list of destinations -->
-            <v-checkbox v-model="currentForm.allTravelWithinTerritory" label="In Territory?" dense required>
+            <v-checkbox
+              v-model="currentForm.allTravelWithinTerritory"
+              label="In Territory?"
+              dense
+              required
+            >
             </v-checkbox>
           </v-col>
-          <v-col cols="12" md="3">
-            <v-select :value="tripType" :items="tripTypes" :rules="[required]" label="Trip Type" background-color="white"
-              dense outlined required @change="updateTripType"></v-select>
+          <v-col
+            cols="12"
+            md="3"
+          >
+            <v-select
+              :value="tripType"
+              :items="tripTypes"
+              :rules="[required]"
+              label="Trip Type"
+              background-color="white"
+              dense
+              outlined
+              required
+              @change="updateTripType"
+            ></v-select>
           </v-col>
         </v-row>
 
@@ -21,17 +47,45 @@
         <div v-else>Trip type {{ tripType }} not implemented!</div>
 
         <v-row>
-          <v-col cols="12" md="1">
-            <v-text-field v-model="currentForm.travelDuration" :rules="[required, isNumber]" label="# Days"
-              background-color="white" dense outlined required></v-text-field>
+          <v-col
+            cols="12"
+            md="1"
+          >
+            <v-text-field
+              v-model="currentForm.travelDuration"
+              :rules="[required, isNumber]"
+              label="# Days"
+              background-color="white"
+              dense
+              outlined
+              required
+            ></v-text-field>
           </v-col>
-          <v-col cols="12" md="2">
-            <v-text-field v-model="currentForm.daysOffTravelStatus" :rules="[isNumber]" label="Days on non-travel status"
-              background-color="white" dense required outlined></v-text-field>
+          <v-col
+            cols="12"
+            md="2"
+          >
+            <v-text-field
+              v-model="currentForm.daysOffTravelStatus"
+              :rules="[isNumber]"
+              label="Days on non-travel status"
+              background-color="white"
+              dense
+              required
+              outlined
+            ></v-text-field>
           </v-col>
-          <v-col cols="12" md="3">
-            <DatePicker v-model="currentForm.dateBackToWork" :min="finalDestination.departureDate" :rules="[required]"
-              text="Expected Date return to work" required />
+          <v-col
+            cols="12"
+            md="3"
+          >
+            <DatePicker
+              v-model="currentForm.dateBackToWork"
+              :min="finalDestination.departureDate"
+              :rules="[required]"
+              text="Expected Date return to work"
+              required
+            />
           </v-col>
         </v-row>
       </v-form>
