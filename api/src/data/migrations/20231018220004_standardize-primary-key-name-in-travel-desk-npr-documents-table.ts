3362da13cd31507a078418d7da78f6bd1d9ce1cc
Marlen Brunner <klondikemarlen@gmail.com>
2023-10-18 15:55:44 -0700

    :butterfly: Standardize travel_desk_passenger_name_record_documents.
    Rename documentID to id.

---
+++ api/src/data/migrations/20231018220004_standardize-primary-key-name-in-travel-desk-npr-documents-table.ts
@@ -0,0 +1,24 @@
+import { Knex } from "knex"
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.table("travel_desk_passenger_name_record_documents", async (table) => {
+    table.dropPrimary()
+    table.renameColumn("documentID", "id")
+    table.primary(["id"])
+  })
+
+  await knex.raw(
+    'ALTER SEQUENCE "travel_desk_passenger_name_record_documents_documentID_seq" RENAME TO "travel_desk_passenger_name_record_documents_id_seq"'
+  )
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.table("travel_desk_passenger_name_record_documents", async (table) => {
+    table.dropPrimary()
+    table.renameColumn("id", "documentID")
+    table.primary(["documentID"])
+  })
+  await knex.raw(
+    'ALTER SEQUENCE "travel_desk_passenger_name_record_documents_id_seq" RENAME TO "travel_desk_passenger_name_record_documents_documentID_seq"'
+  )
+}
