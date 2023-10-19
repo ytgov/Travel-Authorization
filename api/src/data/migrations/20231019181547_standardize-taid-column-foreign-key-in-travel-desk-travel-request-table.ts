daaa0f9928fabfd474dab89866b0a17c7efe3e10
Marlen Brunner <klondikemarlen@gmail.com>
2023-10-19 11:19:25 -0700

    :butterfly: Standardize travel_desk_travel_requests.TAID to travel_authorization_id

---
+++ api/src/data/migrations/20231019181547_standardize-taid-column-foreign-key-in-travel-desk-travel-request-table.ts
@@ -0,0 +1,21 @@
+import { Knex } from "knex"
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.table("travel_desk_travel_requests", async (table) => {
+    table.dropForeign(["TAID"])
+    table.renameColumn("TAID", "travel_authorization_id")
+    table
+      .foreign("travel_authorization_id")
+      .references("id")
+      .inTable("travel_authorizations")
+      .onDelete("CASCADE")
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.table("travel_desk_travel_requests", async (table) => {
+    table.dropForeign(["travel_authorization_id"])
+    table.renameColumn("travel_authorization_id", "TAID")
+    table.foreign("TAID").references("id").inTable("travel_authorizations").onDelete("CASCADE")
+  })
+}
