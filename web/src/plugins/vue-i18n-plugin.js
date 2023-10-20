fb9aede0680027bc5dc2df117f2343fe0b026249
Marlen Brunner <klondikemarlen@gmail.com>
2023-10-20 16:32:51 -0700

    :mute: Slience translation warnings, since it always shows up even when config is correct.

--- web/src/plugins/vue-i18n-plugin.js
+++ web/src/plugins/vue-i18n-plugin.js
@@ -5,7 +5,9 @@ function createI18n(VueInstance) {
   VueInstance.use(VueI18n)
 
   const i18n = new VueI18n({
-    // silentTranslationWarn: true,
+    // I can't get VueI18n to detect that the locale is set, even though
+    // it shows as set everywhere, so I'm silencing this warning.
+    silentTranslationWarn: true,
     locale: "en",
     messages: {
       en,
