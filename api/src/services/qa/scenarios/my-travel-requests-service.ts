8cc696b724a946598b80add30dec589e396212c5
Marlen Brunner <klondikemarlen@gmail.com>
2023-10-24 16:46:50 -0700

    :seedling: Add view itinerary travel authorization scenario.

--- api/src/services/qa/scenarios/my-travel-requests-service.ts
+++ api/src/services/qa/scenarios/my-travel-requests-service.ts
@@ -1,8 +1,9 @@
+import { isNil } from "lodash"
 import { v4 as uuid } from "uuid"
 import moment from "moment"
 
 import BaseService from "@/services/base-service"
-import { TravelAuthorization, Stop, Location, User } from "@/models"
+import { TravelAuthorization, Stop, Location, User, TravelDeskTravelRequest } from "@/models"
 
 export class MyTravelRequestsService extends BaseService {
   private user: User
@@ -231,6 +232,94 @@ export class MyTravelRequestsService extends BaseService {
         locationId: calgaryLocation.id,
         departureDate: lastWeek.toDate(),
         departureTime: "00:00:00",
+      },
+    })
+
+    // Phase: Travel Planning
+    // Location: Calgary
+    // Description: FN Finance meeting #1
+    // Start Date: 12-May-2023
+    // End Date: 14-May-2023
+    // Travel Auth Status: Booked
+    // Travel Action: View Itinerary
+    const [travelAuthorization5] = await TravelAuthorization.findOrCreate({
+      where: {
+        userId: this.user.id,
+        slug: uuid(),
+        status: TravelAuthorization.Statuses.BOOKED,
+        eventName: "FN Finance meeting #1",
+      },
+      defaults: {
+        userId: this.user.id,
+        slug: uuid(),
+        status: TravelAuthorization.Statuses.BOOKED,
+        eventName: "FN Finance meeting #1",
+      },
+    })
+    const [_firstStop5] = await Stop.findOrCreate({
+      where: {
+        travelAuthorizationId: travelAuthorization5.id,
+        locationId: calgaryLocation.id,
+        departureDate: nextWeek.toDate(),
+        departureTime: "00:00:00",
+      },
+      defaults: {
+        travelAuthorizationId: travelAuthorization5.id,
+        locationId: calgaryLocation.id,
+        departureDate: nextWeek.toDate(),
+        departureTime: "00:00:00",
+      },
+    })
+    const [_lastStop5] = await Stop.findOrCreate({
+      where: {
+        travelAuthorizationId: travelAuthorization5.id,
+        locationId: calgaryLocation.id,
+        departureDate: nextWeek.clone().add(2, "days").toDate(),
+        departureTime: "00:00:00",
+      },
+      defaults: {
+        travelAuthorizationId: travelAuthorization5.id,
+        locationId: calgaryLocation.id,
+        departureDate: nextWeek.clone().add(2, "days").toDate(),
+        departureTime: "00:00:00",
+      },
+    })
+
+    if (isNil(this.user.firstName)) {
+      throw new Error("User must have a first name")
+    }
+    if (isNil(this.user.lastName)) {
+      throw new Error("User must have a last name")
+    }
+    if (isNil(travelAuthorization5.eventName)) {
+      throw new Error("Travel Authorization must have an event name")
+    }
+    const [_travelDeskTravelRequest1] = await TravelDeskTravelRequest.findOrCreate({
+      where: {
+        travelAuthorizationId: travelAuthorization5.id,
+        legalFirstName: this.user.firstName,
+        legalLastName: this.user.lastName,
+        strAddress: "NOT_SURE_WHERE_THIS_DATA_SHOULD_COME_FROM",
+        city: "NOT_SURE_WHERE_THIS_DATA_SHOULD_COME_FROM",
+        province: "NOT_SURE_WHERE_THIS_DATA_SHOULD_COME_FROM",
+        postalCode: "NOT_SURE_WHERE_THIS_DATA_SHOULD_COME_FROM",
+        travelPurpose: travelAuthorization5.eventName, // Not sure if this is correct, or even relevant?
+        busPhone: "123-456-7890", // This field should likely not be required?
+        busEmail: "john.doe@example.com", // This field should likely not be required?
+        status: TravelDeskTravelRequest.Statuses.BOOKED,
+      },
+      defaults: {
+        travelAuthorizationId: travelAuthorization5.id,
+        legalFirstName: this.user.firstName,
+        legalLastName: this.user.lastName,
+        strAddress: "NOT_SURE_WHERE_THIS_DATA_SHOULD_COME_FROM",
+        city: "NOT_SURE_WHERE_THIS_DATA_SHOULD_COME_FROM",
+        province: "NOT_SURE_WHERE_THIS_DATA_SHOULD_COME_FROM",
+        postalCode: "NOT_SURE_WHERE_THIS_DATA_SHOULD_COME_FROM",
+        travelPurpose: travelAuthorization5.eventName, // Not sure if this is correct, or even relevant?
+        busPhone: "123-456-7890", // This field should likely not be required?
+        busEmail: "john.doe@example.com", // This field should likely not be required?
+        status: TravelDeskTravelRequest.Statuses.BOOKED,
       },
     })
   }
