853ae295b5f87b671768bad38c6b0cb097e4d225
Marlen Brunner <klondikemarlen@gmail.com>
2023-10-23 21:18:00 -0700

    :label: Magic type inference show constructor as empty if no args passed.
    e.g.
    NonAsyncService.perform() shows type of
    (method) BaseService.perform<typeof NonAsyncService>(this: typeof NonAsyncService): string
    instead of
    (method) BaseService.perform<typeof NonAsyncService>(this: typeof NonAsyncService, ...args: any[]): string

--- api/src/services/base-service.ts
+++ api/src/services/base-service.ts
@@ -1,9 +1,15 @@
+type HasNoArgsConstructor<T> = T extends { new (): any } ? true : false
+
+type CleanConstructorParameters<T extends typeof BaseService> = HasNoArgsConstructor<T> extends true
+  ? []
+  : ConstructorParameters<T>
+
 export class BaseService {
   constructor(...args: any[]) {}
 
   static perform<T extends typeof BaseService>(
     this: T,
-    ...args: ConstructorParameters<T>
+    ...args: CleanConstructorParameters<T>
   ): ReturnType<InstanceType<T>["perform"]> {
     const instance = new this(...args)
     return instance.perform()
