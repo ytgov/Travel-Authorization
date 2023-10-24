5238ed6c2e6ca2312704b19c697dd01afa52837b
Marlen Brunner <klondikemarlen@gmail.com>
2023-10-24 08:25:00 -0700

    :art: Prettify router index for easier editing.

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,31 +1,31 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from "vue"
+import VueRouter from "vue-router"
 
-import NotFound from "../views/NotFound.vue";
-import AdminUserForm from "../components/Administration/UserManagement/UserComponent/Form";
-import AdminDashboard from "../components/Administration/Administration";
-import UserManagement from "../components/Administration/UserManagement/Grid";
-import FlightEstimate from "../components/Administration/RatesEstimateManagement/AirEstimate";
-import PoolCarCost from "../components/Administration/RatesEstimateManagement/PoolCarCost";
-import RentalCarEstimates from "../components/Administration/RatesEstimateManagement/RentalCarEstimate";
-import YGRates from "../components/Administration/RatesEstimateManagement/YGRates";
-import TravelAgents from "../components/Administration/LookupTableManagement/TravelAgents";
-import HealthCheck from "../components/HealthCheck";
+import NotFound from "../views/NotFound.vue"
+import AdminUserForm from "../components/Administration/UserManagement/UserComponent/Form"
+import AdminDashboard from "../components/Administration/Administration"
+import UserManagement from "../components/Administration/UserManagement/Grid"
+import FlightEstimate from "../components/Administration/RatesEstimateManagement/AirEstimate"
+import PoolCarCost from "../components/Administration/RatesEstimateManagement/PoolCarCost"
+import RentalCarEstimates from "../components/Administration/RatesEstimateManagement/RentalCarEstimate"
+import YGRates from "../components/Administration/RatesEstimateManagement/YGRates"
+import TravelAgents from "../components/Administration/LookupTableManagement/TravelAgents"
+import HealthCheck from "../components/HealthCheck"
 
-import preapprovedRouter from "../modules/preapproved/router/index.js";
-import travelDeskRouter from "../modules/travelDesk/router/index.js";
-import travelRequestRouter from "../modules/travel-form/router/index.js";
-import flightExpenseRouter from "../modules/flightExpenses/router/index.js";
-import reportsRouter from "../modules/reports/router/index.js";
+import preapprovedRouter from "../modules/preapproved/router/index.js"
+import travelDeskRouter from "../modules/travelDesk/router/index.js"
+import travelRequestRouter from "../modules/travel-form/router/index.js"
+import flightExpenseRouter from "../modules/flightExpenses/router/index.js"
+import reportsRouter from "../modules/reports/router/index.js"
 
-import authenticationRouter from "../modules/authentication/router";
-import homeRouter from "../modules/home/router";
+import authenticationRouter from "../modules/authentication/router"
+import homeRouter from "../modules/home/router"
 
 import store from "../store"
 
 // import { authGuard } from "../auth/authGuard";
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
@@ -99,50 +99,50 @@ const routes = [
     name: "Not Found",
     component: NotFound,
   },
-];
+]
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
-});
+})
 
-import { getInstance } from "@/auth";
-let authService;
+import { getInstance } from "@/auth"
+let authService
 
 router.beforeEach(async (to, from, next) => {
-  var requiresAuth = to.meta.requiresAuth || false;
+  var requiresAuth = to.meta.requiresAuth || false
 
   if (!requiresAuth) {
-    return next();
+    return next()
   }
 
   if (!authService) {
-    authService = await getInstance();
+    authService = await getInstance()
   }
 
   const guardAction = () => {
     if (authService.isAuthenticated) {
-      return next();
+      return next()
     }
 
-    authService.loginWithRedirect({ appState: { targetUrl: to.fullPath } });
-  };
+    authService.loginWithRedirect({ appState: { targetUrl: to.fullPath } })
+  }
 
   // If the Auth0Plugin has loaded already, check the authentication state
   if (!authService.isLoading) {
-    return guardAction();
+    return guardAction()
   }
 
   authService.$watch("isLoading", (isLoading) => {
     if (isLoading === false) {
-      return guardAction();
+      return guardAction()
     }
-  });
-});
+  })
+})
 
 router.afterEach(async () => {
   return await store.dispatch("auth/checkAuthentication")
-});
+})
 
-export default router;
+export default router
