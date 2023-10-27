69a5ccdf52bfd8047809b64258d8666c42c89fa1
Marlen Brunner <klondikemarlen@gmail.com>
2023-10-26 20:26:14 -0700

    :sparkles: Improve initializer pipeline to create db if it does not exist.

--- api/src/initializers/index.ts
+++ api/src/initializers/index.ts
@@ -1,1 +1,20 @@
-import "./01-run-migrations"
+import * as fs from "fs/promises"
+import * as path from "path"
+
+async function importAndExecuteInitializers() {
+  const files = await fs.readdir(__dirname)
+
+  for (const file of files) {
+    if (/^index\.(ts|js)$/.test(file)) return
+
+    const modulePath = path.join(__dirname, file)
+    const { default: initializerAction } = await import(modulePath)
+    console.log(`Running: ${modulePath}`)
+    await initializerAction().catch((error: any) => {
+      console.error(`Initialization error in ${modulePath}:`, error)
+      process.exit(1)
+    })
+  }
+}
+
+importAndExecuteInitializers().then(() => process.exit(0))
