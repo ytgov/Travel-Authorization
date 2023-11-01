6bc9e132b79aeff2c56c53fa3114cd35166dce23
Marlen Brunner <klondikemarlen@gmail.com>
2023-11-01 10:30:10 -0700

    :recycle: Move travel authorization submit buttons inside details tab.
    Only the "back" button should be on every tab.
    
    It probably make sense to have a save button in every panel?
    And the "submit" button beside the "submit to" field like in the wireframe.

--- web/src/modules/travel-form/views/travel-form-edit/DetailsTab.vue
+++ web/src/modules/travel-form/views/travel-form-edit/DetailsTab.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="mt-4">
+  <v-form
+    class="mt-4"
+    ref="form"
+    lazy-validation
+  >
     <v-row>
       <v-col>
         <PurposeFormCard />
@@ -15,10 +19,28 @@
         <ApprovalsFormCard />
       </v-col>
     </v-row>
-  </div>
+    <v-row dense>
+      <v-col>
+        <v-btn
+          color="blue"
+          @click="submitForm"
+        >
+          Submit to Supervisor
+        </v-btn>
+        <v-btn
+          color="green"
+          class="ml-5"
+          @click="saveForm"
+          >Save Draft
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-form>
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex"
+
 import PurposeFormCard from "./details-tab/PurposeFormCard"
 import DetailsFormCard from "./details-tab/DetailsFormCard"
 import ApprovalsFormCard from "./details-tab/ApprovalsFormCard"
@@ -29,6 +51,42 @@ export default {
     PurposeFormCard,
     DetailsFormCard,
     ApprovalsFormCard,
+  },
+  computed: {
+    ...mapState("travelForm", ["currentForm"]),
+  },
+  methods: {
+    ...mapActions("travelForm", ["updateCurrentForm"]),
+    submitForm() {
+      if (!this.$refs.form.validate()) {
+        this.$snack("Form submission can't be sent until the form is complete.", { color: "error" })
+        return
+      }
+
+      this.currentForm.status = "submitted"
+      return this.updateCurrentForm()
+        .then(() => {
+          this.$router.push({ name: "TravelFormList" })
+        })
+        .catch((error) => {
+          this.$snack(error.message, { color: "error" })
+        })
+    },
+    saveForm() {
+      if (!this.$refs.form.validate()) {
+        this.$snack("Form submission can't be sent until the form is complete.", { color: "error" })
+        return
+      }
+
+      this.currentForm.status = "draft"
+      return this.updateCurrentForm()
+        .then(() => {
+          this.$snack("Form saved as a draft", { color: "success" })
+        })
+        .catch((error) => {
+          this.$snack(error.message, { color: "error" })
+        })
+    },
   },
 }
 </script>
