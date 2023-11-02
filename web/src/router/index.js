4d0b6baf7170e5bde6d9a566020bad29360aff12
Marlen Brunner <klondikemarlen@gmail.com>
2023-11-02 09:12:34 -0700

    :truck: Flatten router and store file structure.
    Why? If every file is called index it makes them harder to find and distinguish when viewing in an editor.
    
    Its also generally a bad practice to put a lot of code into the index file, as it can make it hard to import code laterally.

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -13,7 +13,7 @@ import TravelAgents from "../components/Administration/LookupTableManagement/Tra
 
 import preapprovedRouter from "../modules/preapproved/router/index.js"
 import travelDeskRouter from "../modules/travelDesk/router/index.js"
-import travelRequestRouter from "../modules/travel-form/router/index.js"
+import travelRequestRouter from "../modules/travel-form/router.js/index.js"
 import flightExpenseRouter from "../modules/flightExpenses/router/index.js"
 import reportsRouter from "../modules/reports/router/index.js"
 
