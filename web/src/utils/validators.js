11d3f3329717eb7ff5b13c1ebca5300718b3f770
Marlen Brunner <klondikemarlen@gmail.com>
2023-11-02 12:07:56 -0700

    :recycle: Move greaterThanOrEqualToDate validator to utils namespace.
    Why? So I can re-use it in web/src/modules/travel-authorizations/pages/travel-form-edit/details-tab/details-form-card/RoundTripStopsSection.vue

--- web/src/utils/validators.js
+++ web/src/utils/validators.js
@@ -2,6 +2,13 @@
 
 export const required = (v) => !!v || "This field is required"
 
+export const greaterThanOrEqualToDate =
+  (b, { referenceFieldLabel }) =>
+  (a) =>
+    new Date(a) >= new Date(b) ||
+    `This field must be greater than or equal to ${b || referenceFieldLabel}`
+
 export default {
+  greaterThanOrEqualToDate,
   required,
 }
