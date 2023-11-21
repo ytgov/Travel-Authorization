4bae9a2570e32f6b7f5b2d6fbe82b23d632e3f7e
Marlen Brunner <klondikemarlen@gmail.com>
2023-11-21 11:20:28 -0700

    :sparkles: Add approved by information in travel authorization approve service.

--- api/src/services/travel-authorizations/approve-service.ts
+++ api/src/services/travel-authorizations/approve-service.ts
@@ -15,8 +15,10 @@ export class ApproveService extends BaseService {
     if (this.travelAuthorization.status !== TravelAuthorization.Statuses.SUBMITTED) {
       throw new Error("Travel authorization must be in submitted state to approve.")
     } else {
-      // TODO: save approver id once that exists in the database
-      await this.travelAuthorization.update({ status: TravelAuthorization.Statuses.APPROVED })
+      await this.travelAuthorization.update({
+        approvedByUserId: this.approver.id,
+        status: TravelAuthorization.Statuses.APPROVED
+      })
     }
 
     return this.travelAuthorization.reload({ include: ["expenses", "stops", "purpose", "user"] })
