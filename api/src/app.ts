82f97f4605d1c39caaa7f18f67be677f8256f464
Marlen Brunner <klondikemarlen@gmail.com>
2023-11-27 16:04:23 -0700

    :mute: Reduce system log messages in test mode.
    Probably most of this code shouldn't be logging at all, or logging only on app boot.

--- api/src/app.ts
+++ api/src/app.ts
@@ -60,10 +60,12 @@ app.use(
 
 app.use(fileupload()) // Add FormData support
 
-console.log("host: ", process.env.DB_HOST)
-console.log("user: ", process.env.DB_USER)
-console.log("psss: ", "*********")
-console.log("db name: ", process.env.DB_NAME)
+if (config.NODE_ENV !== "test") {
+  console.log("host: ", process.env.DB_HOST)
+  console.log("user: ", process.env.DB_USER)
+  console.log("psss: ", "*********")
+  console.log("db name: ", process.env.DB_NAME)
+}
 
 // TODO: move these to the routes file
 app.use("/api", databaseHealthCheckMiddleware)
