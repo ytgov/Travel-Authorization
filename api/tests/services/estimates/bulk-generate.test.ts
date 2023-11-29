3feaf3f562b0226fbaa7af03b4d051a351966e26
Marlen Brunner <klondikemarlen@gmail.com>
2023-11-29 11:18:54 -0700

    :recycle: Use round trip helper to clean up test code.

--- api/tests/services/estimates/bulk-generate.test.ts
+++ api/tests/services/estimates/bulk-generate.test.ts
@@ -7,11 +7,12 @@ describe("api/src/services/estimates/bulk-generate.ts", () => {
   describe("BulkGenerate", () => {
     describe(".perform", () => {
       test("creates some new estimates against the travel authorization", async () => {
-        const travelAuthorization = await travelAuthorizationFactory.create({
-          // round trip
-          oneWayTrip: false,
-          multiStop: false,
-        })
+        const travelAuthorization = await travelAuthorizationFactory.create(
+          {},
+          {
+            transient: { roundTrip: true },
+          }
+        )
 
         const whitehorse = await locationFactory.create({ city: "Whitehorse", province: "YT" })
         await stopFactory.create(
