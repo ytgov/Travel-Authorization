50b2d9cc7d624335b817851896428ec72d0f3f29
Marlen Brunner <klondikemarlen@gmail.com>
2023-11-29 15:43:31 -0700

    :recycle: Test class methods first.
    Why? Consistency.

--- api/tests/models/travel-segment.test.ts
+++ api/tests/models/travel-segment.test.ts
@@ -4,24 +4,6 @@ import { fa, faker } from "@faker-js/faker"
 
 describe("api/src/models/travel-segment.ts", () => {
   describe("TravelSegment", () => {
-    describe("#departureAt", () => {
-      test("when the departureOn is null, departureAt is null", () => {
-        const travelSegment = travelSegmentFactory.build({ departureOn: null })
-
-        expect(travelSegment.departureAt).toBeNull()
-      })
-
-      test("when the departure time is null, time defaults to the beginning of the day", () => {
-        const travelSegment = travelSegmentFactory.build({
-          departureOn: new Date("2021-01-01"),
-          departureTime: null,
-          segmentNumber: 0,
-        })
-
-        expect(travelSegment.departureAt).toEqual(new Date("2021-01-01T00:00:00"))
-      })
-    })
-
     describe(".buildFromStops", () => {
       test("when the params are valid, builds the correct travel segment", async () => {
         const travelAuthorization = await travelAuthorizationFactory.create()
@@ -153,6 +135,24 @@ describe("api/src/models/travel-segment.ts", () => {
             accommodationTypeOther: null,
           })
         )
+      })
+    })
+
+    describe("#departureAt", () => {
+      test("when the departureOn is null, departureAt is null", () => {
+        const travelSegment = travelSegmentFactory.build({ departureOn: null })
+
+        expect(travelSegment.departureAt).toBeNull()
+      })
+
+      test("when the departure time is null, time defaults to the beginning of the day", () => {
+        const travelSegment = travelSegmentFactory.build({
+          departureOn: new Date("2021-01-01"),
+          departureTime: null,
+          segmentNumber: 0,
+        })
+
+        expect(travelSegment.departureAt).toEqual(new Date("2021-01-01T00:00:00"))
       })
     })
   })
