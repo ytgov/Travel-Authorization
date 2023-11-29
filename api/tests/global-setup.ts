1dbabb897fbb44ae60b16cc19511bd581b53478b
Marlen Brunner <klondikemarlen@gmail.com>
2023-11-29 13:45:06 -0700

    :hammer: Fix test setup so that it cleans database before each test runs.
    Why? This ensures that each test is properly isolated.
    
    Originally I was only cleaning the models that I've properly Sequelized, and so my tests were flaking out because some seed content existed, and some was getting wiped _after_ the first test had run.
    So whichever test rand first got a dirty environment.

--- api/tests/global-setup.ts
+++ api/tests/global-setup.ts
@@ -6,5 +6,6 @@ import "tsconfig-paths/register"
 import { importAndExecuteInitializers } from "@/initializers"
 
 export default async function globalSetup() {
+  // TODO: exclude seeding, or at least separate development and test seeding
   await importAndExecuteInitializers()
 }
