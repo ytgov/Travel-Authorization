923004ddfce362c401ace9e6a30c315dcedb44fc
Marlen Brunner <klondikemarlen@gmail.com>
2023-12-04 17:06:04 -0700

    :recycle: use more readable way of setting transient and association params.
    See https://github.com/thoughtbot/fishery#extending-factories

--- api/tests/services/estimates/bulk-generate/determine-location-from-date.test.ts
+++ api/tests/services/estimates/bulk-generate/determine-location-from-date.test.ts
@@ -6,51 +6,44 @@ import { determineLocationFromDate } from "@/services/estimates/bulk-generate/de
 describe("api/src/services/estimates/bulk-generate/determine-location-from-date.ts", () => {
   describe(".determineLocationFromDate", () => {
     test("example case of 3 day trip", async () => {
-      const travelAuthorization = await travelAuthorizationFactory.create(
-        {},
-        {
-          transient: { roundTrip: true },
-        }
-      )
+      const travelAuthorization = await travelAuthorizationFactory
+        .transient({ roundTrip: true })
+        .create()
       const whitehorse = await locationFactory.create({ city: "Whitehorse", province: "YT" })
       const vancouver = await locationFactory.create({ city: "Vancouver", province: "BC" })
-      const travelSegment1 = await travelSegmentFactory.create(
-        {
+      const travelSegment1 = await travelSegmentFactory
+        .associations({
+          travelAuthorization,
+          departureLocation: whitehorse,
+          arrivalLocation: vancouver,
+        })
+        .create({
           segmentNumber: 1,
           departureOn: new Date("2022-06-05"),
           departureTime: "00:00:00",
           modeOfTransport: TravelSegment.TravelMethods.AIRCRAFT,
           accommodationType: TravelSegment.AccommodationTypes.HOTEL,
-        },
-        {
-          associations: {
-            travelAuthorization,
-            departureLocation: whitehorse,
-            arrivalLocation: vancouver,
-          },
-        }
-      )
-      const travelSegment2 = await travelSegmentFactory.create(
-        {
+        })
+      const travelSegment2 = await travelSegmentFactory
+        .associations({
+          travelAuthorization,
+          departureLocation: vancouver,
+          arrivalLocation: whitehorse,
+        })
+        .create({
           segmentNumber: 2,
           departureOn: new Date("2022-06-07"),
           departureTime: "15:00:00",
           modeOfTransport: TravelSegment.TravelMethods.AIRCRAFT,
           accommodationType: null,
-        },
-        {
-          associations: {
-            travelAuthorization,
-            departureLocation: vancouver,
-            arrivalLocation: whitehorse,
-          },
-        }
-      )
+        })
       const travelSegments = [travelSegment1, travelSegment2]
 
       expect(determineLocationFromDate(travelSegments, new Date("2022-06-05"))).toEqual(vancouver)
       expect(determineLocationFromDate(travelSegments, new Date("2022-06-06"))).toEqual(vancouver)
-      expect(determineLocationFromDate(travelSegments, new Date("2022-06-07 15:00:00"))).toEqual(vancouver)
+      expect(determineLocationFromDate(travelSegments, new Date("2022-06-07 15:00:00"))).toEqual(
+        vancouver
+      )
     })
   })
 })
