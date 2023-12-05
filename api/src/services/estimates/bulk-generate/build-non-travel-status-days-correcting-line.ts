e239265313e49a13c9b00264db0d08542b4f7ff1
Marlen Brunner <klondikemarlen@gmail.com>
2023-12-05 10:28:07 -0700

    :sparkles: Rebuild non-travel status days so it makes more sense.
    Basic concept, non-travel status days pull from the estimates from latest to first.
    e.g we assume that the user is declaring non-travel status days at the end of their trip.

--- api/src/services/estimates/bulk-generate/build-non-travel-status-days-correcting-line.ts
+++ api/src/services/estimates/bulk-generate/build-non-travel-status-days-correcting-line.ts
@@ -1,9 +1,7 @@
 import { CreationAttributes } from "sequelize"
-import { sumBy } from "lodash"
+import { sortBy, reverse } from "lodash"
 
 import { Expense } from "@/models"
-
-const HOTEL_ALLOWANCE_PER_NIGHT = 250
 
 export function buildNonTravelStatusDaysCorrectingLine({
   estimates,
@@ -16,31 +14,53 @@ export function buildNonTravelStatusDaysCorrectingLine({
   travelAuthorizationId: number
   travelEndAt: Date
 }): CreationAttributes<Expense> {
-  const accommodationCostTotal = sumBy(estimates, (estimate) => {
-    if (estimate.expenseType === Expense.ExpenseTypes.ACCOMODATIONS) {
-      return estimate.cost
+  const estimatesByDateReversed = reverse(sortBy(estimates, "date"))
+
+  const accommodationEstimates = estimatesByDateReversed.filter(
+    (estimate) => estimate.expenseType === Expense.ExpenseTypes.ACCOMODATIONS
+  )
+  let accommodationReduction = 0
+  let accommodationReductionDays = 0
+  accommodationEstimates.forEach((estimate) => {
+    if (accommodationReductionDays >= daysOffTravelStatus) {
+      return
     }
-    return 0
-  })
-  const perDiemCostTotal = sumBy(estimates, (estimate) => {
-    if (estimate.expenseType === Expense.ExpenseTypes.MEALS_AND_INCIDENTALS) {
-      return estimate.cost
-    }
-    return 0
+
+    accommodationReduction += estimate.cost
+    accommodationReductionDays += 1
   })
 
-  const accommodationReduction = Math.min(
-    accommodationCostTotal,
-    daysOffTravelStatus * HOTEL_ALLOWANCE_PER_NIGHT
+  const perDiemEstimates = estimatesByDateReversed.filter(
+    (estimate) => estimate.expenseType === Expense.ExpenseTypes.MEALS_AND_INCIDENTALS
   )
-  const perDiemReduction = Math.min(perDiemCostTotal, daysOffTravelStatus * 777)
+  let perDiemReduction = 0
+  let perDiemReductionDays = 0
+  perDiemEstimates.forEach((estimate) => {
+    if (perDiemReductionDays >= daysOffTravelStatus) {
+      return
+    }
+
+    perDiemReduction += estimate.cost
+    perDiemReductionDays += 1
+  })
+
+  const formatter = new Intl.NumberFormat("en-CA", {
+    style: "decimal",
+    currency: "CAD",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  })
+  const perDiemReductionFormatted = formatter.format(perDiemReduction)
+  const perDiemReductionDetails = `${perDiemReductionDays} day @ non-travel status per diem = -${perDiemReductionFormatted}`
+  const accommodationReductionFormatted = formatter.format(accommodationReduction)
+  const accommodationReductionDetails = `${accommodationReductionDays} day @ non-travel status accomodation = -${accommodationReductionFormatted}`
 
   return {
     type: Expense.Types.ESTIMATE,
     expenseType: Expense.ExpenseTypes.NON_TRAVEL_STATUS,
     travelAuthorizationId,
     currency: "CAD",
-    description: `${daysOffTravelStatus} day @ non-travel status per diem = -${perDiemCostTotal} and ${daysOffTravelStatus} day @ non-travel status accomodation = -${accommodationReduction}`,
+    description: [perDiemReductionDetails, accommodationReductionDetails].join(" and "),
     cost: -(accommodationReduction + perDiemReduction),
     date: travelEndAt,
   }
--- api/tests/services/estimates/bulk-generate/build-non-travel-status-days-correcting-line.test.ts
+++ api/tests/services/estimates/bulk-generate/build-non-travel-status-days-correcting-line.test.ts
@@ -9,21 +9,37 @@ describe("api/src/services/estimates/bulk-generate/build-non-travel-status-days-
   describe("buildNonTravelStatusDaysCorrectingLine", () => {
     test("when given some estimates and daysOffTravelStatus, it returns an array of correcting lines", () => {
       // Arrange
-      const accommodation = expenseFactory
+      const accommodation1 = expenseFactory
         .estimate({ expenseType: Expense.ExpenseTypes.ACCOMODATIONS })
-        .build()
-      const mealsAndIncidentals = expenseFactory
-        .estimate({ expenseType: Expense.ExpenseTypes.MEALS_AND_INCIDENTALS })
-        .build()
-      const transportation = expenseFactory
+        .build({ cost: 250, date: new Date("2022-06-05") })
+      const accommodation2 = expenseFactory
+        .estimate({ expenseType: Expense.ExpenseTypes.ACCOMODATIONS })
+        .build({ cost: 250, date: new Date("2022-06-06") })
+      const transportation1 = expenseFactory
         .estimate({ expenseType: Expense.ExpenseTypes.TRANSPORTATION })
-        .build()
+        .build({ cost: 350, date: new Date("2022-06-05") })
+      const transportation2 = expenseFactory
+        .estimate({ expenseType: Expense.ExpenseTypes.TRANSPORTATION })
+        .build({ cost: 350, date: new Date("2022-06-07") })
+      const mealsAndIncidentals1 = expenseFactory
+        .estimate({ expenseType: Expense.ExpenseTypes.MEALS_AND_INCIDENTALS })
+        .build({ cost: 98.45, date: new Date("2022-06-05") })
+      const mealsAndIncidentals2 = expenseFactory
+        .estimate({ expenseType: Expense.ExpenseTypes.MEALS_AND_INCIDENTALS })
+        .build({ cost: 115.75, date: new Date("2022-06-06") })
+      const mealsAndIncidentals3 = expenseFactory
+        .estimate({ expenseType: Expense.ExpenseTypes.MEALS_AND_INCIDENTALS })
+        .build({ cost: 61.35, date: new Date("2022-06-07") })
       const estimates = [
-        accommodation.dataValues,
-        mealsAndIncidentals.dataValues,
-        transportation.dataValues,
+        accommodation1.dataValues,
+        accommodation2.dataValues,
+        transportation1.dataValues,
+        transportation2.dataValues,
+        mealsAndIncidentals1.dataValues,
+        mealsAndIncidentals2.dataValues,
+        mealsAndIncidentals3.dataValues,
       ]
-      const daysOffTravelStatus = 1
+      const daysOffTravelStatus = 2
       const travelAuthorizationId = faker.number.int({ min: 1, max: 1000 })
       const travelEndAt = faker.date.soon({ days: 30 })
 
@@ -36,18 +52,15 @@ describe("api/src/services/estimates/bulk-generate/build-non-travel-status-days-
       })
 
       // Assert
-      // "2 days @ non-travel status per diem = -230" and "2 days @ non-travel status accomodation = -500"
-      expect(result).toEqual(
-        {
-          type: Expense.Types.ESTIMATE,
-          expenseType: Expense.ExpenseTypes.NON_TRAVEL_STATUS,
-          travelAuthorizationId,
-          currency: "CAD",
-          cost: -(accommodation.cost + mealsAndIncidentals.cost),
-          description: `1 day @ non-travel status per diem = -${mealsAndIncidentals.cost} and 1 day @ non-travel status accomodation = -${accommodation.cost}`,
-          date: travelEndAt,
-        },
-      )
+      expect(result).toEqual({
+        type: Expense.Types.ESTIMATE,
+        expenseType: Expense.ExpenseTypes.NON_TRAVEL_STATUS,
+        travelAuthorizationId,
+        currency: "CAD",
+        cost: -677.1,
+        description: `2 day @ non-travel status per diem = -177.10 and 2 day @ non-travel status accomodation = -500.00`,
+        date: travelEndAt,
+      })
     })
   })
 })
