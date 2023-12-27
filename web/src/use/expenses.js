a7dfad80a2db8a1358b19513a1c33a38e582c749
Marlen Brunner <klondikemarlen@gmail.com>
2023-12-27 09:36:29 -0700

    :recycle: Iterate on use pattern so it actually works.

--- web/src/use/expenses.js
+++ web/src/use/expenses.js
@@ -1,44 +1,44 @@
-import { ref, computed } from "vue"
+import { reactive, computed, toRefs } from "vue"
 
 import expensesApi, { TYPES, EXPENSE_TYPES } from "@/api/expenses-api"
 
 export const useExpenses = () => {
-  const expenses = ref([])
-  const isLoading = ref(false)
-  const isErrored = ref(false)
-  const isCached = ref(false)
+  const state = reactive({
+    expenses: [],
+    isLoading: false,
+    isErrored: false,
+    isCached: false,
+  })
 
-  const estimates = computed(() => expenses.value.filter((item) => item.type === TYPES.ESTIMATE))
+  const estimates = computed(() => state.expenses.filter((item) => item.type === TYPES.ESTIMATE))
 
   async function ensure({ where, page, perPage, ...otherParams } = {}) {
-    if (isCached.value) return expenses.value
+    if (state.isCached) return state.expenses
 
     return fetch({ where, page, perPage, ...otherParams })
   }
 
   async function fetch({ where, page, perPage, ...otherParams } = {}) {
-    isLoading.value = true
+    state.isLoading = true
     try {
       const { expenses } = await expensesApi.list({ where, page, perPage, ...otherParams })
-      isErrored.value = false
-      expenses.value = expenses
-      isCached.value = true
+      state.isErrored = false
+      state.expenses = expenses
+      state.isCached = true
       return expenses
     } catch (error) {
       console.error("Failed to fetch expenses:", error)
-      isErrored.value = true
+      state.isErrored = true
       throw error
     } finally {
-      isLoading.value = false
+      state.isLoading = false
     }
   }
 
   return {
     TYPES,
     EXPENSE_TYPES,
-    expenses,
-    isLoading,
-    isErrored,
+    ...toRefs(state),
     estimates,
     ensure,
     fetch,
