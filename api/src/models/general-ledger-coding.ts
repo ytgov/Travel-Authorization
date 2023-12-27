fc10d8c28fde6be8e5bc2d2d5de5446a1a9a8dfd
Marlen Brunner <klondikemarlen@gmail.com>
2023-12-27 16:16:47 -0700

    :sparkles: Add general ledger coding model.

---
+++ api/src/models/general-ledger-coding.ts
@@ -0,0 +1,91 @@
+import {
+  Association,
+  BelongsToCreateAssociationMixin,
+  BelongsToGetAssociationMixin,
+  BelongsToSetAssociationMixin,
+  CreationOptional,
+  DataTypes,
+  ForeignKey,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+  NonAttribute,
+} from "sequelize"
+
+import sequelize from "@/db/db-client"
+
+import TravelAuthorization from "./travel-authorization"
+
+export class GeneralLedgerCoding extends Model<
+  InferAttributes<GeneralLedgerCoding>,
+  InferCreationAttributes<GeneralLedgerCoding>
+> {
+  declare id: CreationOptional<number>
+  declare travelAuthorizationId: ForeignKey<TravelAuthorization["id"]>
+  declare code: string
+  declare amount: number
+  declare createdAt: CreationOptional<Date>
+  declare updatedAt: CreationOptional<Date>
+
+  // https://sequelize.org/docs/v6/other-topics/typescript/#usage
+  // https://sequelize.org/docs/v6/core-concepts/assocs/#special-methodsmixins-added-to-instances
+  // https://sequelize.org/api/v7/types/_sequelize_core.index.belongstocreateassociationmixin
+  declare getTravelAuthorization: BelongsToGetAssociationMixin<TravelAuthorization>
+  declare setTravelAuthorization: BelongsToSetAssociationMixin<
+    TravelAuthorization,
+    TravelAuthorization["id"]
+  >
+  declare createTravelAuthorization: BelongsToCreateAssociationMixin<TravelAuthorization>
+
+  declare travelAuthorization?: NonAttribute<TravelAuthorization>
+
+  declare static associations: {
+    travelAuthorization: Association<GeneralLedgerCoding, TravelAuthorization>
+  }
+
+  static establishAssociations() {
+    this.belongsTo(TravelAuthorization, {
+      as: "travelAuthorization",
+      foreignKey: "travelAuthorizationId",
+      onDelete: "CASCADE",
+    })
+  }
+}
+
+GeneralLedgerCoding.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    travelAuthorizationId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    code: {
+      type: DataTypes.STRING(25),
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+  }
+)
+
+export default GeneralLedgerCoding
