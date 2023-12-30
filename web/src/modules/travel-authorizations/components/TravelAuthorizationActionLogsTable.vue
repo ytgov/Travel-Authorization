f347d666c3f8568a1c6010f359f214c1304ddfa0
Marlen Brunner <klondikemarlen@gmail.com>
2023-12-29 17:21:06 -0700

    :sparkles: Add useI18n helper shim until we get Vue 3.

--- web/src/modules/travel-authorizations/components/TravelAuthorizationActionLogsTable.vue
+++ web/src/modules/travel-authorizations/components/TravelAuthorizationActionLogsTable.vue
@@ -4,19 +4,26 @@
     :items="travelAuthorizationActionLogs"
     :loading="isLoading"
   >
+    <template #item.action="{ value }">
+      {{ formatAction(value) }}
+    </template>
   </v-data-table>
 </template>
 
 <script setup>
-import { useTravelAuthorizationActionLogs } from "@/use/travel-authorization-action-logs"
+import { startCase } from "lodash"
 import { onMounted } from "vue"
 
+import { useI18n } from "@/plugins/vue-i18n-plugin"
+import { useTravelAuthorizationActionLogs } from "@/use/travel-authorization-action-logs"
 const props = defineProps({
   travelAuthorizationId: {
     type: Number,
     required: true,
   },
 })
+
+const { t } = useI18n()
 
 const { travelAuthorizationActionLogs, isLoading, fetch } = useTravelAuthorizationActionLogs({
   where: {
@@ -50,4 +57,9 @@ const headers = [
 onMounted(async () => {
   await fetch()
 })
+
+function formatAction(value) {
+  const fallback = startCase(value.replace("_", " "))
+  return t(`global.status.${value}`, { $default: fallback })
+}
 </script>
