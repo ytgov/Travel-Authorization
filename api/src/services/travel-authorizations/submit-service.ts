f2872ad09bd201abf627ba4f8afd7c393ff963df
Marlen Brunner <klondikemarlen@gmail.com>
2024-01-02 17:58:19 -0700

    :sparkles: Support supervisor creation via submit service.
    Also makes sure supervisor exists.
    Also make submit service log the correct information.

--- api/src/services/travel-authorizations/submit-service.ts
+++ api/src/services/travel-authorizations/submit-service.ts
@@ -1,47 +1,65 @@
+import { isNil } from "lodash"
+
 import db from "@/db/db-client"
 
 import BaseService from "@/services/base-service"
 import { UpdateService } from "@/services/travel-authorizations"
+import { EnsureService } from "@/services/users"
 
 import { TravelAuthorization, TravelAuthorizationActionLog, User } from "@/models"
 
 export class SubmitService extends BaseService {
   private travelAuthorization: TravelAuthorization
   private attributes: Partial<TravelAuthorization>
-  private approver: User
+  private currentUser: User
 
   constructor(
     travelAuthorization: TravelAuthorization,
     attributes: Partial<TravelAuthorization>,
-    approver: User
+    currentUser: User
   ) {
     super()
     this.travelAuthorization = travelAuthorization
     this.attributes = attributes
-    this.approver = approver
+    this.currentUser = currentUser
   }
 
   async perform(): Promise<TravelAuthorization> {
     if (this.travelAuthorization.status !== TravelAuthorization.Statuses.DRAFT) {
       throw new Error("Travel authorization must be in draft state to submit.")
-    } else {
-      await db.transaction(async () => {
-        this.travelAuthorization = await UpdateService.perform(
-          this.travelAuthorization,
-          {
-            ...this.attributes,
-            status: TravelAuthorization.Statuses.SUBMITTED,
-          },
-          this.approver
-        )
-        await TravelAuthorizationActionLog.create({
-          travelAuthorizationId: this.travelAuthorization.id,
-          actorId: this.approver.id,
-          assigneeId: this.travelAuthorization.userId,
-          action: TravelAuthorizationActionLog.Actions.SUBMITTED,
-        })
-      })
     }
+
+    const supervisorEmail = this.travelAuthorization.supervisorEmail
+    if (isNil(supervisorEmail)) {
+      throw new Error("Supervisor email is required for travel authorization submission.")
+    }
+
+    await db.transaction(async () => {
+      this.travelAuthorization = await UpdateService.perform(
+        this.travelAuthorization,
+        {
+          ...this.attributes,
+          status: TravelAuthorization.Statuses.SUBMITTED,
+        },
+        this.currentUser
+      )
+
+      const supervisor = await EnsureService.perform(
+        {
+          email: supervisorEmail,
+        },
+        this.currentUser
+      ).catch((error) => {
+        throw new Error(`Failed to ensure supervisor: ${error}`)
+      })
+
+      await TravelAuthorizationActionLog.create({
+        travelAuthorizationId: this.travelAuthorization.id,
+        actorId: this.currentUser.id,
+        assigneeId: supervisor.id,
+        action: TravelAuthorizationActionLog.Actions.SUBMITTED,
+      })
+    })
 
     return this.travelAuthorization
   }
