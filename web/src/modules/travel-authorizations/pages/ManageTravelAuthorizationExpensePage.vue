09bed118dbf959dc24fcf0edf83137aedec7fd9d
Marlen Brunner <klondikemarlen@gmail.com>
2024-01-04 13:42:00 -0700

    :sparkles: Add expenses table to manage travel authorization page.

--- web/src/modules/travel-authorizations/pages/ManageTravelAuthorizationExpensePage.vue
+++ web/src/modules/travel-authorizations/pages/ManageTravelAuthorizationExpensePage.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    TODO: add read-only equivalents of components from
-    web/src/modules/travel-authorizations/pages/EditMyTravelAuthorizationExpensePage.vue
+  <div class="mt-4">
+    <v-row>
+      <v-col>
+        <h3>Traveler Expenses</h3>
+
+        <ExpensesTable :travel-authorization-id="travelAuthorizationId" />
+        * Meals and Incidentals are not included in this table.
+      </v-col>
+    </v-row>
+
     <v-row>
       <v-col>
         <TravelAuthorizationActionLogsTable :travel-authorization-id="travelAuthorizationId" />
@@ -11,6 +18,7 @@
 </template>
 
 <script setup>
+import ExpensesTable from "@/modules/travel-authorizations/components/read-travel-authorization-expense-page/ExpensesTable"
 import TravelAuthorizationActionLogsTable from "@/modules/travel-authorizations/components/TravelAuthorizationActionLogsTable"
 
 defineProps({
