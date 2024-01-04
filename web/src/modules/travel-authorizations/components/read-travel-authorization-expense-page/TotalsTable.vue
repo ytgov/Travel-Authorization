3ef548509a31be0cc4d4a0bc84d4100041bbce88
Marlen Brunner <klondikemarlen@gmail.com>
2024-01-04 14:02:28 -0700

    :sparkles: Add meals and incidentals and totals components to manage view.

---
+++ web/src/modules/travel-authorizations/components/read-travel-authorization-expense-page/TotalsTable.vue
@@ -0,0 +1,117 @@
+<template>
+  <v-skeleton-loader
+    v-if="isLoading"
+    type="table-heading@3"
+  />
+  <v-container
+    v-else
+    class="elevation-2"
+  >
+    <v-row>
+      <v-col
+        cols="6"
+        class="text-right"
+        >Subtotal Claim:</v-col
+      >
+      <v-col cols="6">{{ formatCurrency(subTotalClaim) }}</v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        cols="6"
+        class="text-right"
+        >Travel Advance:</v-col
+      >
+      <v-col cols="6">{{ formatCurrency(travelAdvance) }}</v-col>
+    </v-row>
+    <v-row class="mt-0">
+      <v-col cols="6"></v-col>
+      <v-col cols="6">
+        <div class="border-solid border-t border-black w-16"></div>
+      </v-col>
+    </v-row>
+    <v-row class="mt-0">
+      <v-col
+        cols="6"
+        class="text-right"
+      >
+        <strong>Total Claim:</strong>
+      </v-col>
+      <v-col cols="6">
+        <strong>
+          {{ formatCurrency(totalClaim) }}
+        </strong>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+import { sumBy } from "lodash"
+import { computed, watch } from "vue"
+
+import useExpenses from "@/use/expenses"
+import useTravelAuthorization from "@/use/travel-authorization"
+
+const props = defineProps({
+  travelAuthorizationId: {
+    type: Number,
+    required: true,
+  },
+})
+
+const { expenses, isLoading: isLoadingExpenses, fetch: fetchExpenses, TYPES } = useExpenses()
+const {
+  travelAuthorization,
+  isLoading: isLoadingTravelAuthorization,
+  fetch: fetchTravelAuthorization,
+} = useTravelAuthorization()
+
+const isLoading = computed(() => isLoadingExpenses.value || isLoadingTravelAuthorization.value)
+// Will need to be calculated in the back-end if data is multi-page.
+const subTotalClaim = computed(() => sumBy(expenses.value, "cost"))
+const travelAdvance = computed(() => travelAuthorization.value.travelAdvanceInCents / 100.0)
+const totalClaim = computed(() => subTotalClaim.value - travelAdvance.value)
+
+watch(
+  () => props.travelAuthorizationId,
+  async () => {
+    await Promise.all([
+      await fetchExpenses({
+        where: {
+          travelAuthorizationId: props.travelAuthorizationId,
+          type: TYPES.EXPENSE,
+        },
+      }),
+      await fetchTravelAuthorization(props.travelAuthorizationId),
+    ])
+  },
+  { immediate: true }
+)
+
+function formatCurrency(amount) {
+  const formatter = new Intl.NumberFormat("en-CA", {
+    style: "currency",
+    currency: "CAD",
+  })
+  return formatter.format(amount)
+}
+</script>
+
+<style scoped>
+/* See https://tailwindcss.com/docs/border-width */
+.border-solid {
+  border-style: solid;
+}
+.border-t {
+  border-width: 0px;
+  border-top-width: 1px;
+}
+
+.border-black {
+  border-color: rgb(0 0 0);
+}
+
+.w-16 {
+  width: 4rem; /* 64px */
+}
+</style>
--- web/src/modules/travel-authorizations/pages/ManageTravelAuthorizationExpensePage.vue
+++ web/src/modules/travel-authorizations/pages/ManageTravelAuthorizationExpensePage.vue
@@ -8,7 +8,17 @@
         * Meals and Incidentals are not included in this table.
       </v-col>
     </v-row>
-
+    <v-row>
+      <v-col>
+        <h3>Meals and Incidentals</h3>
+        <MealsAndIncidentalsTable :travel-authorization-id="travelAuthorizationId" />
+      </v-col>
+      <v-col>
+        <h3>Totals</h3>
+        <TotalsTable :travel-authorization-id="travelAuthorizationId" />
+      </v-col>
+    </v-row>
+    <!-- TODO: add the rest of the components -->
     <v-row>
       <v-col>
         <TravelAuthorizationActionLogsTable :travel-authorization-id="travelAuthorizationId" />
@@ -19,6 +29,8 @@
 
 <script setup>
 import ExpensesTable from "@/modules/travel-authorizations/components/read-travel-authorization-expense-page/ExpensesTable"
+import MealsAndIncidentalsTable from "@/modules/travel-authorizations/components/read-travel-authorization-expense-page/MealsAndIncidentalsTable"
+import TotalsTable from "@/modules/travel-authorizations/components/read-travel-authorization-expense-page/TotalsTable"
 import TravelAuthorizationActionLogsTable from "@/modules/travel-authorizations/components/TravelAuthorizationActionLogsTable"
 
 defineProps({
