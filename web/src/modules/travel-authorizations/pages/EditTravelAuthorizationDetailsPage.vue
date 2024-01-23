93fbe7322a5ac55f3dd81b8f8b9ef50c4def4428
Marlen Brunner <klondikemarlen@gmail.com>
2024-01-23 09:48:34 -0700

    :construction: Begin adding components for travel authorization page.
    Currently mocked up and running off current-user specific components which need to be swapped out.

--- web/src/modules/travel-authorizations/pages/EditTravelAuthorizationDetailsPage.vue
+++ web/src/modules/travel-authorizations/pages/EditTravelAuthorizationDetailsPage.vue
@@ -1,33 +1,65 @@
 <template>
-  <v-container>
-    TODO: Build out details page for travel authorization.
-    <pre>
-      {{ travelAuthorization }}
-    </pre>
-  </v-container>
+  <v-form
+    ref="form"
+    class="mt-4"
+    lazy-validation
+  >
+    <v-row>
+      <v-col>
+        <PurposeFormCard :travel-authorization-id="travelAuthorizationId" />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <DetailsFormCard :travel-authorization-id="travelAuthorizationId" />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <ApprovalsFormCard
+          :travel-authorization-id="travelAuthorizationId"
+          :validate-form="validateForm"
+        />
+      </v-col>
+    </v-row>
+    <div class="d-flex justify-end">
+      <SaveDraftButton
+        :travel-authorization-id="travelAuthorizationId"
+        :validate-form="validateForm"
+      />
+      <!-- TODO: re-add back button once travel authorizatons page exists -->
+      <!-- <v-btn
+        class="ml-3"
+        color="secondary"
+        :to="{ name: 'TravelAuthorizationsPage' }"
+        >Back</v-btn
+      > -->
+    </div>
+  </v-form>
 </template>
 
 <script setup>
-import { watch } from "vue"
+import { ref } from "vue"
 
-import { useTravelAuthorization } from "@/use/travel-authorization"
+import PurposeFormCard from "@/modules/travel-authorizations/components/edit-my-travel-authorization-details-page/PurposeFormCard"
+import DetailsFormCard from "@/modules/travel-authorizations/components/edit-my-travel-authorization-details-page/DetailsFormCard"
+import ApprovalsFormCard from "@/modules/travel-authorizations/components/edit-my-travel-authorization-details-page/ApprovalsFormCard"
 
-const props = defineProps({
+import SaveDraftButton from "@/modules/travel-authorizations/components/edit-my-travel-authorization-details-page/SaveDraftButton"
+
+defineProps({
   travelAuthorizationId: {
     type: Number,
     required: true,
   },
 })
 
-const { travelAuthorization, fetch } = useTravelAuthorization(props.travelAuthorizationId)
+/** @type {import('vue').Ref<{ validate: () => boolean } | null>} */
+const form = ref(null)
 
-watch(
-  () => props.travelAuthorizationId,
-  async () => {
-    await fetch()
-  },
-  {
-    immediate: true,
-  }
-)
+function validateForm() {
+  if (form.value.validate()) return true
+
+  return false
+}
 </script>
