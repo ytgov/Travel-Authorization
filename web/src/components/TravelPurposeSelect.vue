6919049923b0989b278f24daf6d5c97cf500831d
Marlen Brunner <klondikemarlen@gmail.com>
2024-01-23 10:40:08 -0700

    :sparkles: Add travel purpose select component.

---
+++ web/src/components/TravelPurposeSelect.vue
@@ -0,0 +1,36 @@
+<!-- See https://stackoverflow.com/a/50892881 for slot syntax -->
+<template>
+  <v-select
+    :value="value"
+    :items="travelPurposes"
+    :loading="isLoading"
+    item-text="purpose"
+    item-value="id"
+    label="Travel Purpose"
+    v-bind="$attrs"
+    v-on="$listeners"
+    @input="emit('input', $event)"
+    ><template
+      v-for="(_, slotName) in $scopedSlots"
+      #[slotName]="slotData"
+      ><slot
+        :name="slotName"
+        v-bind="slotData"
+      ></slot></template
+  ></v-select>
+</template>
+
+<script setup>
+import useTravelPurposes from "@/use/use-travel-purposes"
+
+defineProps({
+  value: {
+    type: Number,
+    default: null,
+  },
+})
+
+const emit = defineEmits(["input"])
+
+const { travelPurposes, isLoading } = useTravelPurposes()
+</script>
