c4e7ecc64a56c04fe2f1e907234a0026b06cd7c8
Marlen Brunner <klondikemarlen@gmail.com>
2024-01-23 19:38:57 -0700

    :memo: Attempting to get JSDoc to return correct types for use files.

--- web/src/use/helper-utils.js
+++ web/src/use/helper-utils.js
@@ -1,41 +1,59 @@
 import { unref, watch } from "vue"
 import { isNumber } from "lodash"
 
-function ensureStateFor(globalState, useFunction, id) {
-  if (!isNumber(id)) return null
+/**
+ * @template Options
+ * @template ReturnType
+ * @param {typeof Map} globalState - a Map instance for storing global state
+ * @param {function(Options): ReturnType} useFunction - use function that returns a reactive state object
+ * @param {Options} key - the key to ensure state for
+ * @returns {ReturnType} The reactive state object.
+ */
+function ensureStateFor(globalState, useFunction, key) {
+  if (!isNumber(key)) return null
 
-  if (globalState.has(id)) {
-    return globalState.get(id)
+  if (globalState.has(key)) {
+    return globalState.get(key)
   } else {
-    // NOTE: the use function is using an unreffed id here,
+    // NOTE: the use function is using an unreffed key here,
     // reactivity is maintined by the watch in the global factory function
-    const localState = useFunction(id)
-    globalState.set(id, localState)
+    const localState = useFunction(key)
+    globalState.set(key, localState)
     return localState
   }
 }
 
 /**
- *
+ * @template Options
+ * @template ReturnType
  * @param {typeof Map} globalState - a Map instance for storing global state
- * @param {() => any} useFunction - use function that returns a reactive state object
+ * @param {function(Options): ReturnType} useFunction - use function that returns a reactive state object
+ * @returns {function(Options): ReturnType} The enhanced use function that returns a reactive state object.
  */
 export function asGlobalUse(globalState, useFunction) {
-  return (id) => {
+  return (key) => {
     watch(
-      () => unref(id),
-      (newId) => {
-        ensureStateFor(globalState, useFunction, newId)
+      () => unref(key),
+      (newKey) => {
+        ensureStateFor(globalState, useFunction, newKey)
       },
       {
         immediate: true,
       }
     )
 
-    return ensureStateFor(globalState, useFunction, unref(id))
+    return ensureStateFor(globalState, useFunction, unref(key))
   }
 }
 
+/**
+ * A higher-order function that takes a function and adds global state management to it.
+ *
+ * @template Options
+ * @template ReturnType
+ * @param {function(Options): ReturnType} useFunction - use function that returns a reactive state object
+ * @returns {function(Options): ReturnType} The enhanced use function that returns a reactive state object.
+ */
 export function defineUse(useFunction) {
   const globalState = new Map()
 
--- web/src/use/use-user.js
+++ web/src/use/use-user.js
@@ -5,10 +5,26 @@ import usersApi from "@/api/users-api"
 import { defineUse } from "@/use/helper-utils"
 
 /**
+ * TODO: add other user fields
+ * @typedef {Object} User
+ * @property {number} id
+ */
+
+/**
  * This stores a global user state per id.
  *
+ * @callback UseUser
  * @param {import('vue').Ref<number>} userId
+ * @returns {{
+ *   user: import('vue').Ref<User>,
+ *   isLoading: import('vue').Ref<boolean>,
+ *   isErrored: import('vue').Ref<boolean>,
+ *   fetch: () => Promise<User>,
+ *   refresh: () => Promise<User>,
+ * }}
  */
+
+/** @type {UseUser} */
 export const useUser = defineUse((userId) => {
   const state = reactive({
     user: {},
