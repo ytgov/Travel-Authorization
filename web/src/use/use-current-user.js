5d1aeaab5b01c5e3bba44676b0bdb3c15247aa2e
Marlen Brunner <klondikemarlen@gmail.com>
2024-01-24 13:48:57 -0700

    :label: Add JSDoc for use-current-user function.

--- web/src/use/use-current-user.js
+++ web/src/use/use-current-user.js
@@ -14,7 +14,27 @@ const state = reactive({
   isCached: false,
 })
 
-// Immediately ensures current user
+/**
+ * This stores the global current user state.
+ * Immediately ensures current user
+ *
+ * @params {{ eager: boolean }} [options={}]
+ * @param {boolean} [options.eager=true] - Indicates whether to fetch the current user immediately.
+ * @returns {{
+ *   ROLES: typeof ROLES,
+ *   currentUser: import('vue').Ref<User>,
+ *   isLoading: import('vue').Ref<boolean>,
+ *   isErrored: import('vue').Ref<boolean>,
+ *   isCached: import('vue').Ref<boolean>,
+ *   isReady: import('vue').Ref<boolean>,
+ *   id: import('vue').Ref<number>,
+ *   fullName: import('vue').Ref<string>,
+ *   isAdmin: import('vue').Ref<boolean>,
+ *   ensure: () => Promise<User>,
+ *   fetch: () => Promise<User>,
+ *   unset: () => void,
+ * }}
+ */
 export function useCurrentUser({ eager = true } = {}) {
   const isReady = computed(() => state.isCached && !state.isLoading && !state.isErrored)
   const id = computed(() => state.currentUser.id)
