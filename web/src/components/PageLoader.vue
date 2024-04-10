b1d8a77c2a7295cca3b08b41f846642dcd9d9eed
Marlen Brunner <klondikemarlen@gmail.com>
2024-04-10 13:27:12 -0700

    :beetle: Fix excessive display change warning.
    Previously the overload was triggering a full refresh of more than 3x the visible screen.

---
+++ web/src/components/PageLoader.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- Vuetify v2 fill-height becomes Vuetify v3 h-screen -->
+  <div class="d-flex justify-center align-center fill-height">
+    <div class="d-flex flex-column align-center">
+      <v-progress-circular
+        indeterminate
+        size="64"
+        class="mb-5"
+        color="primary"
+      ></v-progress-circular>
+      <h1>{{ message }}</h1>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  message: {
+    type: String,
+    default: "Loading ...",
+  },
+})
+</script>
