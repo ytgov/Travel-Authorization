ytgov/Travel-Authorization

commit 10d40de4269d7b4239becc02bd6938feb99cb62e

:japanese_castle: Fix prefill travel authorization loading.
Travel authorization can now still be found, even if no estimates are listed.

api/src/controllers/travel-authorizations/expenses/prefill-controller.ts
@@ -40,12 +40,6 @@ export class PrefillController extends BaseController {
     return TravelAuthorization.findByPk(this.params.travelAuthorizationId, {
       include: [
         {
-          association: "expenses",
-          where: {
-            type: Expense.Types.ESTIMATE,
-          },
-        },
-        {
           association: "travelSegments",
           order: [["segmentNumber", "ASC"]],
         }

api/src/policies/expenses-policy.ts
@@ -31,7 +31,7 @@ export class ExpensesPolicy extends BasePolicy<Expense> {
       // state checks, that supersede roles
       // maybe shouldn't be in a policy?
       if (this.travelAuthorization?.status !== TravelAuthorization.Statuses.APPROVED) return false
-      if (!this.isAfterTravelStartDate) return false
+      if (!this.isAfterTravelStartDate()) return false
 
       if (this.user.roles.includes(User.Roles.ADMIN)) return true
       if (this.travelAuthorization.supervisorEmail === this.user.email) return true
@@ -65,7 +65,7 @@ export class ExpensesPolicy extends BasePolicy<Expense> {
     return new TravelAuthorizationsPolicy(this.user, this.travelAuthorization)
   }
 
-  private get isAfterTravelStartDate(): boolean {
+  private isAfterTravelStartDate(): boolean {
     if (this.travelSegments === undefined) return false
 
     const firstTravelSegment = this.travelSegments[0]

commit a6eda83c705eb28a1242356df4e18b8546e1bda2

:beetle: Fix prefill not working at all.
Can't order by expenses if there aren't any.

Bug introduced in https://github.com/icefoganalytics/travel-authorization/commit/10d40de4269d7b4239becc02bd6938feb99cb62e

Added `required: false` so that Travel authorization can now still be found, even if no estimates are listed.

Note that eager loading with a "where" clause switches the query from an outer join to an inner join.
See https://sequelize.org/docs/v6/advanced-association-concepts/eager-loading/#eager-loading-filtered-at-the-associated-model-level

api/src/controllers/travel-authorizations/expenses/prefill-controller.ts
@@ -40,9 +40,16 @@ export class PrefillController extends BaseController {
     return TravelAuthorization.findByPk(this.params.travelAuthorizationId, {
       include: [
         {
+          association: "expenses",
+          where: {
+            type: Expense.Types.ESTIMATE,
+          },
+          required: false
+        },
+        {
           association: "travelSegments",
           order: [["segmentNumber", "ASC"]],
-        }
+        },
       ],
       order: [["expenses", "date", "ASC"]],
     })