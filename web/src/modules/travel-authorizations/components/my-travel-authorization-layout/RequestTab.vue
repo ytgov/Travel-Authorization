0ea665c89b757d7b0b8d428f55c2748aed81f7d6
Marlen Brunner <klondikemarlen@gmail.com>
2024-04-17 09:58:16 -0700

    :construction: Wire travel desk submit into my-travel-request.

---
+++ web/src/modules/travel-authorizations/components/my-travel-authorization-layout/RequestTab.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-tab
+    :to="{
+      name: 'MyTravelRequestsRequestEditPage',
+      params: { travelAuthorizationId },
+    }"
+  >
+    Request
+  </v-tab>
+</template>
+
+<script setup>
+defineProps({
+  travelAuthorizationId: {
+    type: Number,
+    required: true,
+  },
+})
+</script>
---
+++ web/src/modules/travel-authorizations/pages/my-travel-requests/MyTravelRequestsRequestEditPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <NewTravelDeskRequest
+      type="Submit"
+      :authorized-travel="{
+        id: travelAuthorizationId,
+      }"
+    />
+  </v-container>
+</template>
+
+<script setup>
+import NewTravelDeskRequest from "@/modules/travelDesk/views/Requests/NewTravelDeskRequest.vue"
+
+defineProps({
+  travelAuthorizationId: {
+    type: Number,
+    required: true,
+  },
+})
+</script>
