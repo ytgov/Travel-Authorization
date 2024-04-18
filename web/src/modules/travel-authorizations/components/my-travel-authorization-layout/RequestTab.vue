49ad31fc9b7e3cc53442badd37a172ff64241fa8
Marlen Brunner <klondikemarlen@gmail.com>
2024-04-17 17:00:13 -0700

    :lock: Lock request tab unless request is approved.
    Or at least out of pre-approval states.
    In future should lock on denied states.

--- web/src/modules/travel-authorizations/components/my-travel-authorization-layout/RequestTab.vue
+++ web/src/modules/travel-authorizations/components/my-travel-authorization-layout/RequestTab.vue
@@ -1,5 +1,31 @@
 <template>
+  <v-tooltip
+    v-if="isWaitingForApproval"
+    bottom
+  >
+    <template #activator="{ on }">
+      <div
+        class="d-flex align-center"
+        v-on="on"
+      >
+        <v-tab
+          class="d-flex align-start"
+          disabled
+        >
+          Request
+          <v-icon
+            class="ml-1"
+            small
+          >
+            mdi-help-circle-outline
+          </v-icon>
+        </v-tab>
+      </div>
+    </template>
+    <span> Locked until request is approved. </span>
+  </v-tooltip>
   <v-tab
+    v-else
     :to="{
       name: 'MyTravelRequestsRequestEditPage',
       params: { travelAuthorizationId },
@@ -10,10 +36,23 @@
 </template>
 
 <script setup>
-defineProps({
+import { computed, toRefs } from "vue"
+
+import useTravelAuthorization from "@/use/use-travel-authorization"
+
+const props = defineProps({
   travelAuthorizationId: {
     type: Number,
     required: true,
   },
 })
+
+const { travelAuthorizationId } = toRefs(props)
+const { travelAuthorization, STATUSES } = useTravelAuthorization(travelAuthorizationId)
+// TODO: lock on denied states.
+const isWaitingForApproval = computed(
+  () =>
+    travelAuthorization.value.status === STATUSES.DRAFT ||
+    travelAuthorization.value.status === STATUSES.SUBMITTED
+)
 </script>
