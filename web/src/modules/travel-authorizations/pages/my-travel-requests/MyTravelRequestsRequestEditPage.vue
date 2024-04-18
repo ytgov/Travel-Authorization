9c90c872de2fc61a37061f64a97842f9fbbbcb17
Marlen Brunner <klondikemarlen@gmail.com>
2024-04-18 10:30:15 -0700

    :truck: Rename TravelDeskTravelRequestCreateCard to TravelDeskTravelRequestEditCard
    Now that I'm creating the TravelDeskTravelRequest as part of the "aproval" process,
    It is guaranteed to exist once the tab is unlocked.
    This means we can go straight to the edit form, and skip the need for a create form.

--- web/src/modules/travel-authorizations/pages/my-travel-requests/MyTravelRequestsRequestEditPage.vue
+++ web/src/modules/travel-authorizations/pages/my-travel-requests/MyTravelRequestsRequestEditPage.vue
@@ -1,11 +1,11 @@
 <template>
   <v-container>
-    <TravelDeskTravelRequestCreateCard :travel-authorization-id="travelAuthorizationId" />
+    <TravelDeskTravelRequestEditCard :travel-authorization-id="travelAuthorizationId" />
   </v-container>
 </template>
 
 <script setup>
-import TravelDeskTravelRequestCreateCard from "@/components/travel-desk-travel-requests/TravelDeskTravelRequestCreateCard.vue"
+import TravelDeskTravelRequestEditCard from "@/components/travel-desk-travel-requests/TravelDeskTravelRequestEditCard.vue"
 
 defineProps({
   travelAuthorizationId: {
