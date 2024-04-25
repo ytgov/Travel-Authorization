8abc71fccb0e529d9bb6a36e88c66bdedbb034b5
Marlen Brunner <klondikemarlen@gmail.com>
2024-04-25 08:54:10 -0700

    :art: Lint and clean up hotel request components.
    Note that components are still mutating props.
    See https://eslint.vuejs.org/rules/no-mutating-props.html
    
    I'll probably just fork these to avoid breaking other dependencies.

--- web/src/modules/travelDesk/views/Requests/RequestDialogs/HotelRequestTable.vue
+++ web/src/modules/travelDesk/views/Requests/RequestDialogs/HotelRequestTable.vue
@@ -1,161 +1,201 @@
 <template>
-	<div>
-		<title-card class="mt-10 mx-5" titleWidth="8.5rem">
-			<template #title>
-				<div>Hotel Request</div>
-			</template>
-			<template #body>							
-		
-				<v-row class="mt-3 mx-3">
-					<new-hotel-request
-						v-if="!readonly"
-						class="ml-auto mr-3"
-						type="Add New"
-						:minDate="minDate"
-						:maxDate="maxDate"
-						@updateTable="updateTable"
-						:flightRequests="flightRequests"
-						:hotelRequest="hotelRequest"/>
-				</v-row>
-				<v-row class="mb-3 mx-3">
-					<v-col cols="12" v-if="hotels?.length>0">
-						<v-data-table 
-							:headers="hotelHeaders" 
-							:items="hotels" 
-							hide-default-footer 
-							class="elevation-1">
+  <div>
+    <TitleCard
+      class="mt-10 mx-5"
+      title-width="8.5rem"
+    >
+      <template #title>
+        <div>Hotel Request</div>
+      </template>
+      <template #body>
+        <v-row class="mt-3 mx-3">
+          <NewHotelRequest
+            v-if="!readonly"
+            class="ml-auto mr-3"
+            type="Add New"
+            :min-date="minDate"
+            :max-date="maxDate"
+            :flight-requests="flightRequests"
+            :hotel-request="hotelRequest"
+            @updateTable="updateTable"
+          />
+        </v-row>
+        <v-row class="mb-3 mx-3">
+          <v-col
+            v-if="hotels?.length > 0"
+            cols="12"
+          >
+            <v-data-table
+              :headers="hotelHeaders"
+              :items="hotels"
+              hide-default-footer
+              class="elevation-1"
+            >
+              <template #item.rsvConferenceHotel="{ item }">
+                {{ item.rsvConferenceHotel ? "Yes" : "No" }}
+              </template>
 
-							<template v-slot:[`item.rsvConferenceHotel`]="{ item }">
-								{{item.rsvConferenceHotel? 'Yes' : 'No'}}
-							</template>
+              <template #item.checkIn="{ item }">
+                {{ item.checkIn | beautifyDateTime }}
+              </template>
 
-							<template v-slot:[`item.checkIn`]="{ item }">
-								{{item.checkIn | beautifyDateTime }}
-							</template>
+              <template #item.checkOut="{ item }">
+                {{ item.checkOut | beautifyDateTime }}
+              </template>
 
-							<template v-slot:[`item.checkOut`]="{ item }">
-								{{item.checkOut | beautifyDateTime }}
-							</template>
-
-							<template v-slot:[`item.edit`]="{ item }">
-
-								<v-row class="m-0 p-0">								
-									<new-hotel-request
-										v-if="!readonly"									
-										type="Edit"
-										:minDate="minDate"
-										:maxDate="maxDate"
-										@updateTable="updateTable"
-										:flightRequests="flightRequests"
-										:hotelRequest="item"/>
-									<v-btn
-										v-if="!readonly"
-										@click="removeHotel(item)"
-										style="min-width: 0"
-										color="transparent"
-										class="px-1 pt-2"
-										small><v-icon class="" color="red">mdi-close</v-icon>
-									</v-btn>
-								</v-row>							
-							</template>
-						</v-data-table>
-					</v-col>								
-				</v-row>
-			</template>
-		</title-card>
-	</div>
+              <template #item.edit="{ item }">
+                <v-row class="m-0 p-0">
+                  <NewHotelRequest
+                    v-if="!readonly"
+                    type="Edit"
+                    :min-date="minDate"
+                    :max-date="maxDate"
+                    :flight-requests="flightRequests"
+                    :hotel-request="item"
+                    @updateTable="updateTable"
+                  />
+                  <v-btn
+                    v-if="!readonly"
+                    style="min-width: 0"
+                    color="transparent"
+                    class="px-1 pt-2"
+                    small
+                    @click="removeHotel(item)"
+                    ><v-icon
+                      class=""
+                      color="red"
+                      >mdi-close</v-icon
+                    >
+                  </v-btn>
+                </v-row>
+              </template>
+            </v-data-table>
+          </v-col>
+        </v-row>
+      </template>
+    </TitleCard>
+  </div>
 </template>
 
 <script>
-	import TitleCard from '../../Common/TitleCard.vue';
-	import NewHotelRequest from "./NewHotelRequest.vue";
+import TitleCard from "@/modules/travelDesk/views/Common/TitleCard.vue"
+import NewHotelRequest from "@/modules/travelDesk/views/Requests/RequestDialogs/NewHotelRequest.vue"
 
-	export default {
-		components: {
-			TitleCard,
-			NewHotelRequest
-		},
-		name: "HotelRequestTable",
-		props: {
-			readonly: Boolean,
-			hotels: {},
-			flightRequests: {},
-			authorizedTravel: {required: false},
-		},
-		data() {
-			return {
-				hotelHeaders: [
-					{ text: "Check-in", 				value: "checkIn", 		   		class: "blue-grey lighten-4"},
-					{ text: "Check-out",				value: "checkOut",		        class: "blue-grey lighten-4"},
-					{ text: "City", 					value: "city",	   			    class: "blue-grey lighten-4", sortable: false},
-					{ text: "Conference Hotel?", 		value: "rsvConferenceHotel",	class: "blue-grey lighten-4", sortable: false},
-					{ text: "Conference/Meeting Name",  value: "conferenceName",   	    class: "blue-grey lighten-4", sortable: false},
-					{ text: "Conference/Meeting Hotel", value: "conferenceHotelName",   class: "blue-grey lighten-4", sortable: false},
-					{ text: "Additional Information",   value: "additionalInformation", class: "blue-grey lighten-4", sortable: false},
-					{ text: "",     					value: "edit", 			        class: "blue-grey lighten-4", width: "4rem", sortable: false}
-				],
-				hotelRequest:{},
-				tmpId:1,
-				admin: false,
-				travelerDetails: {},
-				savingData: false,
-				minDate:"",
-				maxDate:"",
-			};
-		},
-		mounted() {	
-			this.initForm()					
-		},
-		methods: {
-			updateTable(type) {
-				
-				if(type=='Add New'){
-					this.hotelRequest.tmpId=this.tmpId
-					this.hotels.push(JSON.parse(JSON.stringify(this.hotelRequest)))
-					this.tmpId++
-				}
-			},	
-			
-			initForm(){
-				
-				if(this.authorizedTravel?.startDate && this.authorizedTravel?.endDate){
-					this.minDate = this.authorizedTravel.startDate.slice(0,10)
-					this.maxDate = this.authorizedTravel.endDate.slice(0,10)
-				}
+export default {
+  name: "HotelRequestTable",
+  components: {
+    TitleCard,
+    NewHotelRequest,
+  },
+  props: {
+    readonly: {
+      type: Boolean,
+      default: false,
+    },
+    hotels: {
+      type: Array,
+      default: () => [],
+    },
+    flightRequests: {
+      type: Array,
+      default: () => [],
+    },
+    authorizedTravel: {
+      type: Object,
+      default: () => null,
+    },
+  },
+  data() {
+    return {
+      hotelHeaders: [
+        { text: "Check-in", value: "checkIn", class: "blue-grey lighten-4" },
+        { text: "Check-out", value: "checkOut", class: "blue-grey lighten-4" },
+        { text: "City", value: "city", class: "blue-grey lighten-4", sortable: false },
+        {
+          text: "Conference Hotel?",
+          value: "rsvConferenceHotel",
+          class: "blue-grey lighten-4",
+          sortable: false,
+        },
+        {
+          text: "Conference/Meeting Name",
+          value: "conferenceName",
+          class: "blue-grey lighten-4",
+          sortable: false,
+        },
+        {
+          text: "Conference/Meeting Hotel",
+          value: "conferenceHotelName",
+          class: "blue-grey lighten-4",
+          sortable: false,
+        },
+        {
+          text: "Additional Information",
+          value: "additionalInformation",
+          class: "blue-grey lighten-4",
+          sortable: false,
+        },
+        { text: "", value: "edit", class: "blue-grey lighten-4", width: "4rem", sortable: false },
+      ],
+      hotelRequest: {},
+      tmpId: 1,
+      admin: false,
+      travelerDetails: {},
+      savingData: false,
+      minDate: "",
+      maxDate: "",
+    }
+  },
+  mounted() {
+    this.initForm()
+  },
+  methods: {
+    updateTable(type) {
+      if (type == "Add New") {
+        this.hotelRequest.tmpId = this.tmpId
+        this.hotels.push(JSON.parse(JSON.stringify(this.hotelRequest)))
+        this.tmpId++
+      }
+    },
 
-				const hotelRequest = {}
-				hotelRequest.hotelID=null
-				hotelRequest.tmpId=null
-				hotelRequest.checkIn="";
-				hotelRequest.checkOut="";
-				hotelRequest.city="";	
-				hotelRequest.rsvConferenceHotel=true;
-				hotelRequest.conferenceName="";
-				hotelRequest.conferenceHotelName="";
-				hotelRequest.additionalInformation="";
-				hotelRequest.status="Requested";
+    initForm() {
+      if (this.authorizedTravel?.startDate && this.authorizedTravel?.endDate) {
+        this.minDate = this.authorizedTravel.startDate.slice(0, 10)
+        this.maxDate = this.authorizedTravel.endDate.slice(0, 10)
+      }
 
-				this.hotelRequest = hotelRequest								
-			},
+      const hotelRequest = {}
+      hotelRequest.hotelID = null
+      hotelRequest.tmpId = null
+      hotelRequest.checkIn = ""
+      hotelRequest.checkOut = ""
+      hotelRequest.city = ""
+      hotelRequest.rsvConferenceHotel = true
+      hotelRequest.conferenceName = ""
+      hotelRequest.conferenceHotelName = ""
+      hotelRequest.additionalInformation = ""
+      hotelRequest.status = "Requested"
 
-			editHotel(item) {				
-				this.hotelRequest=item						
-			},
+      this.hotelRequest = hotelRequest
+    },
 
-			removeHotel(item) {
-				console.log(item)
-				let delIndex = -1
-				if(item.hotelID>0)
-					delIndex = this.hotels.findIndex(hotel => (hotel.hotelID && hotel.hotelID == item.hotelID) );
-				else
-					delIndex = this.hotels.findIndex(hotel => (hotel.tmpId && hotel.tmpId == item.tmpId));				
-				console.log(delIndex)
-				if(delIndex>=0) this.hotels.splice(delIndex,1)
-			}
-		}
-	};
+    editHotel(item) {
+      this.hotelRequest = item
+    },
+
+    removeHotel(item) {
+      console.log(item)
+      let delIndex = -1
+      if (item.hotelID > 0)
+        delIndex = this.hotels.findIndex((hotel) => hotel.hotelID && hotel.hotelID == item.hotelID)
+      else delIndex = this.hotels.findIndex((hotel) => hotel.tmpId && hotel.tmpId == item.tmpId)
+      console.log(delIndex)
+      if (delIndex >= 0) {
+        this.hotels.splice(delIndex, 1)
+      }
+    },
+  },
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- web/src/modules/travelDesk/views/Requests/RequestDialogs/NewHotelRequest.vue
+++ web/src/modules/travelDesk/views/Requests/RequestDialogs/NewHotelRequest.vue
@@ -1,211 +1,238 @@
 <template>
-	<div>
-		<v-dialog v-model="hotelDialog" persistent max-width="80%">
-			<template v-slot:activator="{ on, attrs }">
-				<v-btn				
-				:class="type=='Add New'? 'my-4 right':'mx-0 px-0'"					
-					:color="type=='Add New'? 'primary':'transparent'"
-					style="min-width: 0;"
-					@click="initForm()"
-					v-bind="attrs"
-					v-on="on">
-					<div v-if="type=='Add New'" >Add Hotel</div>
-					<v-icon v-else class="mx-0 px-0" color="blue">mdi-pencil</v-icon>
-				</v-btn>
-			</template>
+  <div>
+    <v-dialog
+      v-model="hotelDialog"
+      persistent
+      max-width="80%"
+    >
+      <template #activator="{ on, attrs }">
+        <v-btn
+          :class="type == 'Add New' ? 'my-4 right' : 'mx-0 px-0'"
+          :color="type == 'Add New' ? 'primary' : 'transparent'"
+          style="min-width: 0"
+          v-bind="attrs"
+          @click="initForm"
+          v-on="on"
+        >
+          <div v-if="type == 'Add New'">Add Hotel</div>
+          <v-icon
+            v-else
+            class="mx-0 px-0"
+            color="blue"
+            >mdi-pencil</v-icon
+          >
+        </v-btn>
+      </template>
 
-			<v-card>
-				<v-card-title class="blue">
-					<div class="text-h5">
-						Add Hotel
-					</div>
-				</v-card-title>
+      <v-card>
+        <v-card-title class="blue">
+          <div class="text-h5">Add Hotel</div>
+        </v-card-title>
 
-				<v-card-text>					
-					
-					<v-row class="mt-5 mx-3">
-						<v-col cols="4">
-							<v-text-field								
-								:error="state.checkInErr"
-								v-model="hotelRequest.checkIn"
-								@input="state.checkInErr = false"
-								label="Check-in Date"
-								outlined
-								:min="minDate"
-								:max="maxDate"
-								type="date"/>
-							<v-text-field								
-								:error="state.checkOutErr"
-								v-model="hotelRequest.checkOut"
-								@input="state.checkOutErr = false"
-								label="Check-out Date"
-								outlined
-								:min="minDate"
-								:max="maxDate"
-								type="date"/>
-							<v-autocomplete
-								:items="destinations"								
-								item-value="text"
-								:error="state.cityErr"
-								v-model="hotelRequest.city"
-								@input="state.cityErr = false"
-								label="City"
-								outlined/>
-							<v-radio-group																
-								:error="state.rsvConferenceHotelErr"
-								label="Conference/Meeting Hotel?"
-								v-model="hotelRequest.rsvConferenceHotel"								
-								outlined
-								row>
-								<v-radio label="Yes" :value="true"></v-radio>
-								<v-radio label="No" :value="false"></v-radio>
-							</v-radio-group>
-						</v-col>						
-						<v-col cols="8">
-							<v-textarea								
-								:error="state.additionalInfoErr"
-								v-model="hotelRequest.additionalInformation"
-								label="Additional Information"
-								rows="8"
-								outlined
-								:clearable="!readonly"/>	
-						</v-col>				
-					</v-row>
+        <v-card-text>
+          <v-row class="mt-5 mx-3">
+            <v-col cols="4">
+              <v-text-field
+                v-model="hotelRequest.checkIn"
+                :error="state.checkInErr"
+                label="Check-in Date"
+                outlined
+                :min="minDate"
+                :max="maxDate"
+                type="date"
+                @input="state.checkInErr = false"
+              />
+              <v-text-field
+                v-model="hotelRequest.checkOut"
+                :error="state.checkOutErr"
+                label="Check-out Date"
+                outlined
+                :min="minDate"
+                :max="maxDate"
+                type="date"
+                @input="state.checkOutErr = false"
+              />
+              <v-autocomplete
+                v-model="hotelRequest.city"
+                :items="destinations"
+                item-value="text"
+                :error="state.cityErr"
+                label="City"
+                outlined
+                @input="state.cityErr = false"
+              />
+              <v-radio-group
+                v-model="hotelRequest.rsvConferenceHotel"
+                :error="state.rsvConferenceHotelErr"
+                label="Conference/Meeting Hotel?"
+                outlined
+                row
+              >
+                <v-radio
+                  label="Yes"
+                  :value="true"
+                ></v-radio>
+                <v-radio
+                  label="No"
+                  :value="false"
+                ></v-radio>
+              </v-radio-group>
+            </v-col>
+            <v-col cols="8">
+              <v-textarea
+                v-model="hotelRequest.additionalInformation"
+                :error="state.additionalInfoErr"
+                label="Additional Information"
+                rows="8"
+                outlined
+                :clearable="!readonly"
+              />
+            </v-col>
+          </v-row>
 
-					<v-row class="mt-0 mx-3">
-						<v-col cols="4">
-							<v-text-field
-								:error="state.conferenceNameErr"
-								v-model="hotelRequest.conferenceName"
-								@input="state.conferenceNameErr = false"
-								label="Conference/Meeting Name"
-								outlined/>						
-							
-						</v-col>						
-						<v-col cols="8">
-							<v-text-field								
-								:error="state.conferenceHotelNameErr"
-								v-model="hotelRequest.conferenceHotelName"
-								@input="state.conferenceHotelNameErr = false"
-								label="Conference/Meeting Hotel"
-								outlined/>	
-						</v-col>				
-					</v-row>
-				</v-card-text>
+          <v-row class="mt-0 mx-3">
+            <v-col cols="4">
+              <v-text-field
+                v-model="hotelRequest.conferenceName"
+                :error="state.conferenceNameErr"
+                label="Conference/Meeting Name"
+                outlined
+                @input="state.conferenceNameErr = false"
+              />
+            </v-col>
+            <v-col cols="8">
+              <v-text-field
+                v-model="hotelRequest.conferenceHotelName"
+                :error="state.conferenceHotelNameErr"
+                label="Conference/Meeting Hotel"
+                outlined
+                @input="state.conferenceHotelNameErr = false"
+              />
+            </v-col>
+          </v-row>
+        </v-card-text>
 
-				<v-card-actions>
-					<v-btn color="grey darken-5" @click="hotelDialog = false">
-						<div v-if="type == 'View'">Close</div>
-						<div v-else>Cancel</div>
-					</v-btn>					
-					<v-btn						
-						class="ml-auto"
-						color="green darken-1"
-						@click="saveHotelRequest()">
-						<div v-if="type == 'View'">Save</div>
-						<div v-else>Add</div>
-					</v-btn>
-				</v-card-actions>
-			</v-card>
-		</v-dialog>	
-	</div>
+        <v-card-actions>
+          <v-btn
+            color="grey darken-5"
+            @click="hotelDialog = false"
+          >
+            <div v-if="type == 'View'">Close</div>
+            <div v-else>Cancel</div>
+          </v-btn>
+          <v-btn
+            class="ml-auto"
+            color="green darken-1"
+            @click="saveHotelRequest"
+          >
+            <div v-if="type == 'View'">Save</div>
+            <div v-else>Add</div>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
-	import Vue from "vue";
+import Vue from "vue"
 
-	export default {		
-		name: "NewHotelRequest",
-		props: {
-			type: {
-				type: String
-			},
-			hotelRequest: {},
-			flightRequests: {},
-			minDate: { type: String, default:""	},
-			maxDate: { type: String, default:""	},
-		},
-		data() {
-			return {			
-				checkIn: "",
-				checkOut: "",				
-				hotelDialog: false,
-				readonly: false,
+export default {
+  name: "NewHotelRequest",
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+    hotelRequest: {
+      type: Object,
+      default: () => ({}),
+    },
+    flightRequests: {
+      type: Array,
+      default: () => [],
+    },
+    minDate: {
+      type: String,
+      default: "",
+    },
+    maxDate: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      checkIn: "",
+      checkOut: "",
+      hotelDialog: false,
+      readonly: false,
 
-				state: {
-					checkInErr: false,
-					checkOutErr: false,
-					cityErr: false,
-					rsvConferenceHotelErr: false,
-					conferenceNameErr: false,
-					conferenceHotelNameErr: false,
-					additionalInfoErr: false					
-				},
-				destinations:[],
-			};
-		},
-		mounted() {
-			this.destinations = this.$store.state.traveldesk.destinations;		
-		},
-		methods: {
+      state: {
+        checkInErr: false,
+        checkOutErr: false,
+        cityErr: false,
+        rsvConferenceHotelErr: false,
+        conferenceNameErr: false,
+        conferenceHotelNameErr: false,
+        additionalInfoErr: false,
+      },
+      destinations: [],
+    }
+  },
+  mounted() {
+    this.destinations = this.$store.state.traveldesk.destinations
+  },
+  methods: {
+    checkFields() {
+      this.state.checkInErr = this.hotelRequest.checkIn ? false : true
+      this.state.checkOutErr = this.hotelRequest.checkOut ? false : true
+      this.state.cityErr = this.hotelRequest.city ? false : true
+      this.state.rsvConferenceHotelErr = this.hotelRequest.rsvConferenceHotel != null ? false : true
+      this.state.conferenceNameErr = this.hotelRequest.conferenceName ? false : true
+      this.state.conferenceHotelNameErr = this.hotelRequest.conferenceHotelName ? false : true
+      this.state.additionalInfoErr = false
 
-			checkFields() {
+      for (const key of Object.keys(this.state)) {
+        if (this.state[key]) return false
+      }
+      return true
+    },
 
-				this.state.checkInErr = this.hotelRequest.checkIn? false:true;
-				this.state.checkOutErr = this.hotelRequest.checkOut? false:true;
-				this.state.cityErr = this.hotelRequest.city? false:true;
-				this.state.rsvConferenceHotelErr = this.hotelRequest.rsvConferenceHotel != null? false: true;
-				this.state.conferenceNameErr = this.hotelRequest.conferenceName? false:true;
-				this.state.conferenceHotelNameErr = this.hotelRequest.conferenceHotelName? false:true;
-				this.state.additionalInfoErr = false;
+    saveHotelRequest() {
+      if (this.checkFields()) {
+        this.$emit("updateTable", this.type)
+        this.hotelDialog = false
+      }
+    },
 
-				for (const key of Object.keys(this.state)) {
-					if (this.state[key]) return false;
-				}
-				return true;
-			},
+    initForm() {
+      this.initStates()
+      const flightDates = Vue.filter("flightStartEnd")(this.flightRequests)
 
-			saveHotelRequest() {
-				if (this.checkFields()) {
-					this.$emit("updateTable", this.type);
-					this.hotelDialog = false;
-				}
-				
-			},
+      if (this.type == "Add New") {
+        this.hotelRequest.checkIn = flightDates.start
+        this.hotelRequest.checkOut = flightDates.end
+        this.hotelRequest.city = ""
+        this.hotelRequest.rsvConferenceHotel = true
+        this.hotelRequest.conferenceName = ""
+        this.hotelRequest.conferenceHotelName = ""
+        this.hotelRequest.additionalInformation = ""
+      }
+    },
 
-			initForm() {
-
-				this.initStates();
-				const flightDates = Vue.filter("flightStartEnd")(this.flightRequests)				
-
-				if(this.type == "Add New"){
-
-					this.hotelRequest.checkIn=flightDates.start;
-					this.hotelRequest.checkOut=flightDates.end;
-					this.hotelRequest.city="";
-					this.hotelRequest.rsvConferenceHotel=true;
-					this.hotelRequest.conferenceName="";
-					this.hotelRequest.conferenceHotelName="";
-					this.hotelRequest.additionalInformation="";
-					
-				}
-					
-			},
-
-			initStates() {				
-				for (const key of Object.keys(this.state)) {
-					this.state[key] = false;
-				}
-			}
-		}
-	};
+    initStates() {
+      for (const key of Object.keys(this.state)) {
+        this.state[key] = false
+      }
+    },
+  },
+}
 </script>
 
 <style scoped lang="css" src="@/styles/_travel_desk.css">
-
 .label {
-	font-weight: 600;
-	font-size: 10pt !important;
+  font-weight: 600;
+  font-size: 10pt !important;
 }
-
 </style>
