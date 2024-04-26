2d98e2af787f0be0579374c1ccd401fae7a451c4
Marlen Brunner <klondikemarlen@gmail.com>
2024-04-26 08:45:42 -0700

    :recycle: Lint and standardize transportation request code.
    Note that code is still mutating model props, which I'll deal by forking
    the components and fixing independently.

--- web/src/modules/travelDesk/views/Requests/RequestDialogs/TransportationRequestTable.vue
+++ web/src/modules/travelDesk/views/Requests/RequestDialogs/TransportationRequestTable.vue
@@ -1,144 +1,181 @@
 <template>
-	<div>
-		<title-card class="mt-10 mx-5 mb-5" titleWidth="16.5rem">
-			<template #title>
-				<div>Other Transportation Request</div>
-			</template>
-			<template #body >
-				<v-row class="mt-3 mx-3">
-					<new-transportation-request
-						v-if="!readonly"
-						class="ml-auto mr-3"
-						type="Add New"
-						:minDate="minDate"
-						:maxDate="maxDate"
-						@updateTable="updateTable"
-						:otherTransportationRequest="otherTransportationRequest"/>
-				</v-row>
-				<v-row class="mb-3 mx-3">
-					<v-col cols="12" v-if="otherTransportations?.length>0">
-						<v-data-table 
-							:headers="otherTransportationHeaders" 
-							:items="otherTransportations" 
-							hide-default-footer 
-							class="elevation-1">
-							<template v-slot:[`item.date`]="{ item }">
-								{{item.date | beautifyDate }}
-							</template>
-							<template v-slot:[`item.edit`]="{ item }">
-								<v-row class="m-0 p-0">								
-									<new-transportation-request
-										v-if="!readonly"									
-										type="Edit"
-										:minDate="minDate"
-										:maxDate="maxDate"
-										@updateTable="updateTable"
-										:otherTransportationRequest="item"/>
-									<v-btn
-										v-if="!readonly"
-										@click="removeOtherTransportation(item)"
-										style="min-width: 0"
-										color="transparent"
-										class="px-1 pt-2"
-										small><v-icon class="" color="red">mdi-close</v-icon>
-									</v-btn>
-								</v-row>
-								
-							</template>
-						</v-data-table>
-					</v-col>								
-				</v-row>
-			</template>	
-		</title-card>		
-	</div>
+  <div>
+    <TitleCard
+      class="mt-10 mx-5 mb-5"
+      title-width="16.5rem"
+    >
+      <template #title>
+        <div>Other Transportation Request</div>
+      </template>
+      <template #body>
+        <v-row class="mt-3 mx-3">
+          <NewTransportationRequest
+            v-if="!readonly"
+            class="ml-auto mr-3"
+            type="Add New"
+            :min-date="minDate"
+            :max-date="maxDate"
+            :other-transportation-request="otherTransportationRequest"
+            @updateTable="updateTable"
+          />
+        </v-row>
+        <v-row class="mb-3 mx-3">
+          <v-col
+            v-if="otherTransportations?.length > 0"
+            cols="12"
+          >
+            <v-data-table
+              :headers="otherTransportationHeaders"
+              :items="otherTransportations"
+              hide-default-footer
+              class="elevation-1"
+            >
+              <template #item.date="{ item }">
+                {{ item.date | beautifyDate }}
+              </template>
+              <template #item.edit="{ item }">
+                <v-row class="m-0 p-0">
+                  <NewTransportationRequest
+                    v-if="!readonly"
+                    type="Edit"
+                    :min-date="minDate"
+                    :max-date="maxDate"
+                    :other-transportation-request="item"
+                    @updateTable="updateTable"
+                  />
+                  <v-btn
+                    v-if="!readonly"
+                    style="min-width: 0"
+                    color="transparent"
+                    class="px-1 pt-2"
+                    small
+                    @click="removeOtherTransportation(item)"
+                    ><v-icon
+                      class=""
+                      color="red"
+                      >mdi-close</v-icon
+                    >
+                  </v-btn>
+                </v-row>
+              </template>
+            </v-data-table>
+          </v-col>
+        </v-row>
+      </template>
+    </TitleCard>
+  </div>
 </template>
 
 <script>
-	import TitleCard from '../../Common/TitleCard.vue';
-	import NewTransportationRequest from "./NewTransportationRequest.vue";
+import TitleCard from "@/modules/travelDesk/views/Common/TitleCard.vue"
+import NewTransportationRequest from "@/modules/travelDesk/views/Requests/RequestDialogs/NewTransportationRequest.vue"
 
-	export default {
-		components: {
-			TitleCard,
-			NewTransportationRequest
-		},
-		name: "TransportationRequestTable",
-		props: {
-			readonly: Boolean,
-			otherTransportations: {},
-			authorizedTravel: {required: false},
-		},
-		data() {
-			return {
-				
-				otherTransportationHeaders: [
-					{ text: "Type", 				  value: "transportationType", 	class: "blue-grey lighten-4", sortable: false},
-					{ text: "Depart Location",   	  value: "depart",  		class: "blue-grey lighten-4", sortable: false},
-					{ text: "Arrive Location",  	  value: "arrive",  		class: "blue-grey lighten-4", sortable: false},
-					{ text: "Date",       			  value: "date",       		class: "blue-grey lighten-4"},					
-					{ text: "Additional Information", value: "additionalNotes",  class: "blue-grey lighten-4", sortable: false},
-					{ text: "",     				  value: "edit", 			class: "blue-grey lighten-4", width: "4rem", sortable: false}
-				],
-				otherTransportationRequest:{},
-				tmpId:1,
-				admin: false,
-				travelerDetails: {},
-				savingData: false,
-				minDate:"",
-				maxDate:"",
-			};
-		},
-		mounted() {	
-			this.initForm()					
-		},
-		methods: {
-			updateTable(type) {
-				
-				if(type=='Add New'){
-					this.otherTransportationRequest.tmpId=this.tmpId
-					this.otherTransportations.push(JSON.parse(JSON.stringify(this.otherTransportationRequest)))
-					this.tmpId++
-				}
-			},	
-			
-			initForm(){
+export default {
+  name: "TransportationRequestTable",
+  components: {
+    TitleCard,
+    NewTransportationRequest,
+  },
+  props: {
+    readonly: {
+      type: Boolean,
+      default: false,
+    },
+    otherTransportations: {
+      type: Array,
+      default: () => [],
+    },
+    authorizedTravel: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+      otherTransportationHeaders: [
+        {
+          text: "Type",
+          value: "transportationType",
+          class: "blue-grey lighten-4",
+          sortable: false,
+        },
+        { text: "Depart Location", value: "depart", class: "blue-grey lighten-4", sortable: false },
+        { text: "Arrive Location", value: "arrive", class: "blue-grey lighten-4", sortable: false },
+        { text: "Date", value: "date", class: "blue-grey lighten-4" },
+        {
+          text: "Additional Information",
+          value: "additionalNotes",
+          class: "blue-grey lighten-4",
+          sortable: false,
+        },
+        { text: "", value: "edit", class: "blue-grey lighten-4", width: "4rem", sortable: false },
+      ],
+      otherTransportationRequest: {},
+      tmpId: 1,
+      admin: false,
+      travelerDetails: {},
+      savingData: false,
+      minDate: "",
+      maxDate: "",
+    }
+  },
+  mounted() {
+    this.initForm()
+  },
+  methods: {
+    updateTable(type) {
+      if (type == "Add New") {
+        this.otherTransportationRequest.tmpId = this.tmpId
+        this.otherTransportations.push(JSON.parse(JSON.stringify(this.otherTransportationRequest)))
+        this.tmpId++
+      }
+    },
 
-				if(this.authorizedTravel?.startDate && this.authorizedTravel?.endDate){
-					this.minDate = this.authorizedTravel.startDate.slice(0,10)
-					this.maxDate = this.authorizedTravel.endDate.slice(0,10)
-				}
+    initForm() {
+      if (this.authorizedTravel?.startDate && this.authorizedTravel?.endDate) {
+        this.minDate = this.authorizedTravel.startDate.slice(0, 10)
+        this.maxDate = this.authorizedTravel.endDate.slice(0, 10)
+      }
 
-				const otherTransportationRequest = {}
-				otherTransportationRequest.transportationID=null
-				otherTransportationRequest.tmpId=null
-				otherTransportationRequest.transportationType=""
-				otherTransportationRequest.depart=""
-				otherTransportationRequest.arrive=""
-				otherTransportationRequest.date=""				
-				otherTransportationRequest.additionalNotes=""
-				otherTransportationRequest.status="Requested"
+      const otherTransportationRequest = {}
+      otherTransportationRequest.transportationID = null
+      otherTransportationRequest.tmpId = null
+      otherTransportationRequest.transportationType = ""
+      otherTransportationRequest.depart = ""
+      otherTransportationRequest.arrive = ""
+      otherTransportationRequest.date = ""
+      otherTransportationRequest.additionalNotes = ""
+      otherTransportationRequest.status = "Requested"
 
-				this.otherTransportationRequest = otherTransportationRequest								
-			},
+      this.otherTransportationRequest = otherTransportationRequest
+    },
 
-			editOtherTransportation(item) {				
-				this.otherTransportationRequest=item						
-			},
+    editOtherTransportation(item) {
+      this.otherTransportationRequest = item
+    },
 
-			removeOtherTransportation(item) {
-				console.log(item)
-				let delIndex = -1
-				if(item.transportationID>0)
-					delIndex = this.otherTransportations.findIndex(otherTransportation => (otherTransportation.transportationID && otherTransportation.transportationID == item.transportationID) );
-				else
-					delIndex = this.otherTransportations.findIndex(otherTransportation => (otherTransportation.tmpId && otherTransportation.tmpId == item.tmpId));				
-				console.log(delIndex)
-				if(delIndex>=0) this.otherTransportations.splice(delIndex,1)
-			}
-		}
-	};
+    removeOtherTransportation(item) {
+      console.log(item)
+      let delIndex = -1
+      if (item.transportationID > 0) {
+        delIndex = this.otherTransportations.findIndex(
+          (otherTransportation) =>
+            otherTransportation.transportationID &&
+            otherTransportation.transportationID == item.transportationID
+        )
+      } else {
+        delIndex = this.otherTransportations.findIndex(
+          (otherTransportation) =>
+            otherTransportation.tmpId && otherTransportation.tmpId == item.tmpId
+        )
+      }
+      console.log(delIndex)
+      if (delIndex >= 0) {
+        this.otherTransportations.splice(delIndex, 1)
+      }
+    },
+  },
+}
 </script>
 
-<style scoped lang="css" src="@/styles/_travel_desk.css">
-
-</style>
+<style scoped lang="css" src="@/styles/_travel_desk.css"></style>
