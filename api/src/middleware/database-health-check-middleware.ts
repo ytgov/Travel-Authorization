3333c034552fc51298b289630ee65815090828c3
Marlen Brunner <klondikemarlen@gmail.com>
2024-06-11 14:55:06 -0700

    :mute: Make dataset health check only run once per app boot.
    Maybe it should just be an initializer?

--- api/src/middleware/database-health-check-middleware.ts
+++ api/src/middleware/database-health-check-middleware.ts
@@ -5,6 +5,8 @@ import dbLegacy from "@/db/db-client-legacy"
 import db from "@/db/db-client"
 import logger from "@/utils/logger"
 
+let isDatabaseConnectionHealthly = false
+
 // TODO: make this a generic error hanlder instead and suppress logs?
 // It's clearly not something that should spam the logs on every request.
 export async function databaseHealthCheckMiddleware(
@@ -12,6 +14,8 @@ export async function databaseHealthCheckMiddleware(
   res: Response,
   next: NextFunction
 ) {
+  if (isDatabaseConnectionHealthly) return next()
+
   try {
     await dbLegacy.raw("SELECT 1")
     if (NODE_ENV !== "test") {
@@ -22,6 +26,8 @@ export async function databaseHealthCheckMiddleware(
     if (NODE_ENV !== "test") {
       logger.info("Sequelize database connection has been established successfully.")
     }
+
+    isDatabaseConnectionHealthly = true
 
     return next()
   } catch (error) {
