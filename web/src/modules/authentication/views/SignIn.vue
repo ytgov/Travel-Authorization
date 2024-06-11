0b3f4da059393ecf1a963916dccfd73b1f0e9226
Marlen Brunner <klondikemarlen@gmail.com>
2024-06-11 16:42:12 -0700

    :ruler: Lint sign in page to current standard.

--- web/src/modules/authentication/views/SignIn.vue
+++ web/src/modules/authentication/views/SignIn.vue
@@ -1,40 +1,72 @@
 <template>
-  <div id="login" class="fill-height justify-center" tag="section">
+  <div
+    id="login"
+    class="fill-height justify-center"
+    tag="section"
+  >
     <v-row justify="center">
-      <v-col lg="11" sm="8" xl="7">
-        <v-card class="elevation-5" style="overflow: hidden">
+      <v-col
+        lg="11"
+        sm="8"
+        xl="7"
+      >
+        <v-card
+          class="elevation-5"
+          style="overflow: hidden"
+        >
           <v-row>
-            <v-col lg="7" style="background-color: #f9f4d4" class="d-none d-md-flex align-center justify-center">
+            <v-col
+              lg="7"
+              style="background-color: #f9f4d4"
+              class="d-none d-md-flex align-center justify-center"
+            >
               <div class="d-none d-sm-block">
-                <img src="@/assets/logo.svg" alt="Logo" class="d-md-block pl-6" />
+                <img
+                  src="@/assets/logo.svg"
+                  alt="Logo"
+                  class="d-md-block pl-6"
+                />
                 <div class="align-center pa-6">
-                  <h2 class="display-1 font-weight-medium" style="line-height: 40px">
+                  <h2
+                    class="display-1 font-weight-medium"
+                    style="line-height: 40px"
+                  >
                     {{ title }}
                   </h2>
-                  <h6 class="subtitle-1 mt-4 op-5 font-weight-regular">
-                    Yukon Government
-                  </h6>
+                  <h6 class="subtitle-1 mt-4 op-5 font-weight-regular">Yukon Government</h6>
                 </div>
               </div>
             </v-col>
             <v-col lg="5">
               <div class="pa-7 pa-sm-12">
-                <div style="background-color: #f9f4d4" class="pa-5 d-md-none">
-                  <img src="@/assets/logo.svg" alt="Logo" class="d-md-inline" />
-                  <h2 class="display-1 font-weight-medium" style="line-height: 40px">
+                <div
+                  style="background-color: #f9f4d4"
+                  class="pa-5 d-md-none"
+                >
+                  <img
+                    src="@/assets/logo.svg"
+                    alt="Logo"
+                    class="d-md-inline"
+                  />
+                  <h2
+                    class="display-1 font-weight-medium"
+                    style="line-height: 40px"
+                  >
                     {{ title }}
                   </h2>
-                  <h6 class="subtitle-1 mt-4 op-5 font-weight-regular">
-                    Yukon Government
-                  </h6>
+                  <h6 class="subtitle-1 mt-4 op-5 font-weight-regular">Yukon Government</h6>
                 </div>
 
                 <h2 class="font-weight-bold mt-4 text--darken-2">Sign in</h2>
                 <h6 class="subtitle-1 mt-3 mb-5">
-                  This application is only available to authorized users. If you have an account, click the button
-                  below.
+                  This application is only available to authorized users. If you have an account,
+                  click the button below.
                 </h6>
-                <v-btn v-if="!$auth.isAuthenticated" @click="login" color="primary">
+                <v-btn
+                  v-if="!$auth.isAuthenticated"
+                  color="primary"
+                  @click="login"
+                >
                   Sign In
                 </v-btn>
               </div>
@@ -47,7 +79,7 @@
 </template>
 
 <script>
-import { APPLICATION_NAME } from "@/config";
+import { APPLICATION_NAME } from "@/config"
 
 import useCurrentUser from "@/use/use-current-user"
 
@@ -63,8 +95,8 @@ export default {
   },
   methods: {
     async login() {
-      this.$auth.loginWithRedirect();
+      this.$auth.loginWithRedirect()
     },
   },
-};
+}
 </script>
