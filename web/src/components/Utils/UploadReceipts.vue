38538ef5cdf7af7dd27ac2128d0ce3b5f9a273ee
Marlen Brunner <klondikemarlen@gmail.com>
2024-06-12 12:03:35 -0700

    :ruler: Lint UploadReceipts.vue to current standard.

--- web/src/components/Utils/UploadReceipts.vue
+++ web/src/components/Utils/UploadReceipts.vue
@@ -1,46 +1,51 @@
 <template>
   <v-card
     :style="{
-      padding: '10px'
+      padding: '10px',
     }"
   >
-    <v-file-input label="File input" @change="Preview_image" v-model="image"> </v-file-input>
+    <v-file-input
+      v-model="image"
+      label="File input"
+      @change="Preview_image"
+    >
+    </v-file-input>
   </v-card>
 </template>
 
 <script>
-import { securePost } from "@/store/jwt";
-import { FORM_URL } from "../../urls";
+import { securePost } from "@/store/jwt"
+import { FORM_URL } from "@/urls"
 
 export default {
-  name: "imageUpload",
+  name: "UploadReceipts",
   data() {
     return {
       url: null,
-      image: null
-    };
+      image: null,
+    }
   },
   methods: {
     Preview_image() {
       if (this.image) {
-        this.url = URL.createObjectURL(this.image);
+        this.url = URL.createObjectURL(this.image)
       } else {
-        this.url = "";
+        this.url = ""
       }
     },
     uploadImage() {
       securePost(`${FORM_URL}/uploadReceipt`, this.image, {
         headers: {
-          "Content-Type": "multipart/form-data"
-        }
+          "Content-Type": "multipart/form-data",
+        },
       })
-        .then(res => {
-          console.log(res);
+        .then((res) => {
+          console.log(res)
         })
-        .catch(err => {
-          console.log(err);
-        });
-    }
-  }
-};
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+  },
+}
 </script>
