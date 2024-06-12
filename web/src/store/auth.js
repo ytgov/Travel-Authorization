8f511d7f03b373037ef9b42e423a38b63b751fe4
Marlen Brunner <klondikemarlen@gmail.com>
2024-06-12 10:33:53 -0700

    :construction: Convert "auth" store to using newer http-client.
    http-client uses newer Auth0 plugin.

--- web/src/store/auth.js
+++ web/src/store/auth.js
@@ -1,57 +1,77 @@
-import { PROFILE_URL } from "../urls";
-import { secureGet } from "./jwt";
+import usersApi from "@/api/users-api"
 
+/**
+ * @typedef {import("@/api/users-api").User} User
+ */
+
+/**
+ * @typedef State
+ * @type {Object}
+ * @property {(User|null)} user
+ * @property {string} fullName
+ * @property {string[]} roles
+ * @property {string} department
+ */
+
+/** @type {State} */
 const state = {
   user: null,
   fullName: "",
   roles: [],
-  department: ""
-};
+  department: "",
+}
+
+/**
+ * @typedef {} Getters
+ */
+
+/** @type {import('vuex').GetterTree<State, any>} */
 const getters = {
-  isAuthenticated: state => !!state.user,
-  fullName: state => {
-    return state.fullName;
+  isAuthenticated: (state) => !!state.user,
+  fullName: (state) => {
+    return state.fullName
   },
-  user: state => {
-    return state.user;
-  }
-};
+  user: (state) => {
+    return state.user
+  },
+}
+
 const actions = {
   async checkAuthentication({ commit }) {
-    await secureGet(PROFILE_URL)
-      .then(resp => {
-        commit("setUser", resp.data.user);
-      })
-      .catch(() => {
-        commit("clearUser");
-      });
+    try {
+      const { user } = await usersApi.me()
+      commit("setUser", user)
+    } catch {
+      commit("clearUser")
+    }
   },
   UpdateUserDepartment({ commit }, value) {
-    commit("setDepartment", value);
-  }
-};
+    commit("setDepartment", value)
+  },
+}
+
 const mutations = {
   setUser(state, user) {
-    state.user = user;
-    state.fullName = user.displayName;
-    state.roles = user.roles;
-    state.department = user.department;
+    state.user = user
+    state.fullName = user.displayName
+    state.roles = user.roles
+    state.department = user.department
   },
   clearUser(state) {
-    state.user = null;
-    state.fullName = null;
-    state.roles = [];
-    state.department = null;
+    state.user = null
+    state.fullName = null
+    state.roles = []
+    state.department = null
   },
   setDepartment(state, department) {
-    state.department = department;
-  }
-};
+    state.department = department
+  },
+}
 
 export default {
   namespaced: true,
   state,
   getters,
   actions,
-  mutations
-};
+  mutations,
+}
