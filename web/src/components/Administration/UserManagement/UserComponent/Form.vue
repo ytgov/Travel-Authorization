2eb14446d689f6470feda682a8f3e0db701198b9
Marlen Brunner <klondikemarlen@gmail.com>
2024-06-12 12:01:03 -0700

    :ruler: Lint UserComponent/Form.vue to current standard.

--- web/src/components/Administration/UserManagement/UserComponent/Form.vue
+++ web/src/components/Administration/UserManagement/UserComponent/Form.vue
@@ -15,14 +15,27 @@
     <Breadcrumbs />
 
     <v-row>
-      <v-col cols="12" md="12">
-        <v-alert v-if="alertMsg" :color="alertType + ' accent-4'" dense dark dismissible>{{ alertMsg }}</v-alert>
+      <v-col
+        cols="12"
+        md="12"
+      >
+        <v-alert
+          v-if="alertMsg"
+          :color="alertType + ' accent-4'"
+          dense
+          dark
+          dismissible
+          >{{ alertMsg }}</v-alert
+        >
         <v-card class="default">
           <v-card-title>User Details</v-card-title>
           <v-card-text>
             <v-form>
               <v-row>
-                <v-col cols="12" sm="6">
+                <v-col
+                  cols="12"
+                  sm="6"
+                >
                   <v-text-field
                     v-model="user.firstName"
                     label="First name"
@@ -33,7 +46,10 @@
                     hide-details
                   ></v-text-field>
                 </v-col>
-                <v-col cols="12" sm="6">
+                <v-col
+                  cols="12"
+                  sm="6"
+                >
                   <v-text-field
                     v-model="user.lastName"
                     label="Last name"
@@ -72,27 +88,36 @@
                 </v-col>
                 <v-col cols="12">
                   <v-select
+                    v-model="pendingRoles"
                     label="Roles"
                     :items="roles"
-                    v-model="pendingRoles"
                     outlined
                     dense
                     multiple
                     small-chips
                     background-color="white"
-                    @change="alertMsg = ''"
                     clearable
                     item-value="id"
                     item-text="name"
                     hide-details
+                    @change="alertMsg = ''"
                   ></v-select>
                 </v-col>
               </v-row>
             </v-form>
             <v-row class="mt-5">
-              <v-col cols="12" class="d-flex py-0">
+              <v-col
+                cols="12"
+                class="d-flex py-0"
+              >
                 <v-spacer></v-spacer>
-                <v-btn color="primary" class="mr-5 mt-0" @click="saveUser"> Save user </v-btn>
+                <v-btn
+                  color="primary"
+                  class="mr-5 mt-0"
+                  @click="saveUser"
+                >
+                  Save user
+                </v-btn>
               </v-col>
             </v-row>
           </v-card-text>
@@ -103,31 +128,31 @@
 </template>
 
 <script>
-import { secureGet, securePut } from "@/store/jwt";
-import { USERS_URL, LOOKUP_URL } from "../../../../urls";
-import Breadcrumbs from "../../../Breadcrumbs.vue";
+import { secureGet, securePut } from "@/store/jwt"
+import { USERS_URL, LOOKUP_URL } from "@/urls"
+import Breadcrumbs from "@/components/Breadcrumbs.vue"
 
 export default {
   components: {
-    Breadcrumbs
+    Breadcrumbs,
   },
   data: () => ({
     overlay: false,
     accessItem: {
       AccessType: 1,
-      AccessText: 1
+      AccessText: 1,
     },
     /* VALIDATION*/
     dataAccessValidation: false,
     menu: null,
 
-    rules: [value => !!value || "Required."],
+    rules: [(value) => !!value || "Required."],
 
     user: {
       first_name: "",
       last_name: "",
       email: "",
-      roles: []
+      roles: [],
     },
 
     pendingRoles: [],
@@ -139,58 +164,58 @@ export default {
     roles: [],
     showAccessDialog: false,
     alertMsg: "",
-    alertType: ""
+    alertType: "",
   }),
   async mounted() {
-    await this.loadDepartments();
-    await this.loadRoles();
-    await this.loadUser(this.$route.params.id);
+    await this.loadDepartments()
+    await this.loadRoles()
+    await this.loadUser(this.$route.params.id)
   },
 
   methods: {
     async saveUser() {
-      this.alertMsg = "";
-      this.alertType = "red";
+      this.alertMsg = ""
+      this.alertType = "red"
       let permsObject = {
         first_name: this.user.first_name,
         last_name: this.user.last_name,
         departments: this.pendingDepartments,
-        roles: this.pendingRoles
-      };
+        roles: this.pendingRoles,
+      }
       securePut(`${USERS_URL}/${this.$route.params.id}/permissions`, permsObject)
-        .then(resp => {
-          console.log(resp);
-          this.alertMsg = "Permissions and Department Saved Successfully.";
-          this.alertType = "teal";
+        .then((resp) => {
+          console.log(resp)
+          this.alertMsg = "Permissions and Department Saved Successfully."
+          this.alertType = "teal"
         })
-        .catch(e => (this.alertMsg = e.response.data));
+        .catch((e) => (this.alertMsg = e.response.data))
       // this.showAccessDialog = false;
     },
     async loadUser(id) {
-      secureGet(`${USERS_URL}/${id}`).then(resp => {
-        this.user = resp.data;
-        if (this.user.is_active == 1) this.user.status = "active";
-        else this.user.status = "inactive";
-      });
-      secureGet(`${USERS_URL}/${id}/permissions`).then(resp => {
-        this.pendingDepartments = resp.data.departments;
-        this.pendingRoles = resp.data.roles;
-      });
+      secureGet(`${USERS_URL}/${id}`).then((resp) => {
+        this.user = resp.data
+        if (this.user.is_active == 1) this.user.status = "active"
+        else this.user.status = "inactive"
+      })
+      secureGet(`${USERS_URL}/${id}/permissions`).then((resp) => {
+        this.pendingDepartments = resp.data.departments
+        this.pendingRoles = resp.data.roles
+      })
     },
     async loadDepartments() {
-      return secureGet(`${LOOKUP_URL}/department-branch`).then(resp => {
+      return secureGet(`${LOOKUP_URL}/department-branch`).then((resp) => {
         for (const key of Object.keys(resp.data))
           this.departments.push({
-            name: key
-          });
-      });
+            name: key,
+          })
+      })
     },
     async loadRoles() {
-      return secureGet(`${LOOKUP_URL}/roles`).then(resp => {
-        console.log(resp.data);
-        this.roles = resp.data;
-      });
-    }
-  }
-};
+      return secureGet(`${LOOKUP_URL}/roles`).then((resp) => {
+        console.log(resp.data)
+        this.roles = resp.data
+      })
+    },
+  },
+}
 </script>
