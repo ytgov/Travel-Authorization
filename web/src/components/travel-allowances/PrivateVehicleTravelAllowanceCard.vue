29c031ad4733c3dc19631a3d100d2d68e2faf8fa
Marlen Brunner <klondikemarlen@gmail.com>
2024-07-26 15:39:23 -0700

    :sparkles: Make private vehicle allowance card load data from back-end.

--- web/src/components/travel-allowances/PrivateVehicleTravelAllowanceCard.vue
+++ web/src/components/travel-allowances/PrivateVehicleTravelAllowanceCard.vue
@@ -3,7 +3,18 @@
     <v-card-title><h3>Distance allowance for private vehicle</h3></v-card-title>
     <v-card-text>
       <ul>
-        <li><p>70.0 cents (CAD) per kilometer;</p></li>
+        <li>
+          <p>
+            <v-progress-circular
+              v-if="isLoading"
+              indeterminate
+              size="12"
+              width="1"
+            />
+            <span v-else>{{ travelAllowancePerKilometer }}</span>
+            cents (CAD) per kilometer;
+          </p>
+        </li>
       </ul>
       <p class="mt-4">
         Refer to
@@ -16,3 +27,43 @@
     </v-card-text>
   </v-card>
 </template>
+
+<script setup>
+import { computed } from "vue"
+import { isEmpty, isNil } from "lodash"
+
+import useTravelAllowances, { TRAVEL_ALLOWANCE_ALLOWANCE_TYPES } from "@/use/use-travel-allowances"
+
+const travelAllowancesQuery = computed(() => {
+  return {
+    where: {
+      allowanceType: TRAVEL_ALLOWANCE_ALLOWANCE_TYPES.DISTANCE_ALLOWANCE_PER_KILOMETER,
+    },
+    perPage: 1,
+  }
+})
+const { travelAllowances, isLoading } = useTravelAllowances(travelAllowancesQuery)
+
+const travelAllowancePerKilometer = computed(() => {
+  if (isEmpty(travelAllowances.value)) {
+    return ""
+  }
+
+  const privateVehicleTravelAllowance = travelAllowances.value[0]
+  const { amount: amountInDollars } = privateVehicleTravelAllowance
+  return formatInCents(amountInDollars)
+})
+
+function formatInCents(amountInDollars) {
+  if (isNil(amountInDollars)) {
+    return ""
+  }
+
+  const formatter = new Intl.NumberFormat("en-CA", {
+    style: "decimal",
+    minimumFractionDigits: 1,
+    maximumFractionDigits: 1,
+  })
+  return formatter.format(amountInDollars * 100)
+}
+</script>
