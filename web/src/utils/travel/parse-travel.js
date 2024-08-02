6a09cc7366b7f54ea40db3425d5f569e203edb84
Marlen Brunner <klondikemarlen@gmail.com>
2024-08-01 17:53:05 -0700

    :fire: Delete code that is untested to avoid confusion.

--- web/src/utils/travel/parse-travel.js
+++ web/src/utils/travel/parse-travel.js
@@ -18,39 +18,10 @@ export function parseTravel(text) {
   }
 
   const result = {
-    passengers: [],
     flights: [],
-    hotels: [],
-    cars: [],
   }
 
-  cst.children.section.forEach((section) => {
-    if (tokenMatcher(section.children[0], Passenger)) {
-      result.passengers.push(
-        section.children.Text.map((t) => t.image)
-          .join(" ")
-          .trim()
-      )
-    } else if (tokenMatcher(section.children[0], Flights)) {
-      result.flights.push(
-        section.children.Text.map((t) => t.image)
-          .join(" ")
-          .trim()
-      )
-    } else if (tokenMatcher(section.children[0], Hotels)) {
-      result.hotels.push(
-        section.children.Text.map((t) => t.image)
-          .join(" ")
-          .trim()
-      )
-    } else if (tokenMatcher(section.children[0], Cars)) {
-      result.cars.push(
-        section.children.Text.map((t) => t.image)
-          .join(" ")
-          .trim()
-      )
-    }
-  })
+  console.log("cst:", cst)
 
   return result
 }
