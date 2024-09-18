88e360e85321a59d8eb2d0a4062c2ac85142e863
Marlen Brunner <klondikemarlen@gmail.com>
2024-09-18 14:16:35 -0700

    :recycle: Upgrade initializing runner to get better error logging.
    Code take from https://github.com/icefoganalytics/wrap/blob/fab2ba7e252f04285eee27a4e42ba9541089d121/api/src/initializers/index.ts

--- api/src/initializers/index.ts
+++ api/src/initializers/index.ts
@@ -8,29 +8,33 @@ const NON_INITIALIZER_REGEX = /^index\.(ts|js)$/
 export async function importAndExecuteInitializers() {
   const files = await fs.readdir(__dirname)
 
-  return files.reduce(async (previousInitializerAction, file) => {
-    await previousInitializerAction
-
-    if (NON_INITIALIZER_REGEX.test(file)) return
+  for (const file of files) {
+    if (NON_INITIALIZER_REGEX.test(file)) continue
 
     const modulePath = path.join(__dirname, file)
     logger.info(`Running initializer: ${modulePath}`)
 
-    const { default: initializerAction } = await import(modulePath)
+    try {
+      const { default: initializerAction } = await require(modulePath)
+      await initializerAction()
+    } catch (error) {
+      logger.error(`Failed to run initializer: ${modulePath}`, { error })
+      throw error
+    }
+  }
 
-    return initializerAction().catch((error: unknown) => {
-      logger.error(`Initialization error in ${modulePath}: ${error}`, { error })
-      return Promise.reject(error)
-    })
-  }, Promise.resolve())
+  return true
 }
 
 if (require.main === module) {
   // TODO: add some kind of middleware that 503s? if initialization failed?
-  importAndExecuteInitializers()
-    .then(() => process.exit(0))
-    .catch(() => {
-      logger.info("Failed to complete initialization!")
-      return process.exit(0)
-    })
+  ;(async () => {
+    try {
+      await importAndExecuteInitializers()
+    } catch {
+      logger.error("Failed to complete initialization!")
+    }
+
+    process.exit(0)
+  })()
 }
