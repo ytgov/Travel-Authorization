817b4b0082c29ce31f693cd54287ccd97bf0aff9
Marlen Brunner <klondikemarlen@gmail.com>
2024-09-18 14:36:33 -0700

    :broom: Fix type error in general-ledger-coding.test.ts

--- api/tests/models/general-ledger-coding.test.ts
+++ api/tests/models/general-ledger-coding.test.ts
@@ -2,35 +2,41 @@ import { GeneralLedgerCoding } from "@/models"
 
 describe("api/src/models/general-ledger-coding.ts", () => {
   describe("GeneralLedgerCoding", () => {
-    describe("#code - validity - Code must be in the format: vote (3 characters) - Program (6 characters) - object code (4 digits) - subledger-1 (0-4 characters) - subleger-2 (0-5 characters)", () => {
+    describe("#code - valid cases", () => {
       test.each([
-        ["123-123456-1234", true],
-        ["123-123456-1234-", false],
-        ["123-123456-1234-1", true],
-        ["123-123456-1234-1-", false],
-        ["123-123456-1234-1234", true],
-        ["123-123456-1234-12345", false],
-        ["123-123456-1234-1-1", true],
-        ["123-123456-1234-1-12345", true],
-        ["123-123456-1234-1-123456", false],
-        ["123-123456-1234-1234-12345", true],
-      ])('.isValid("%s") == %s', async (code, expected) => {
+        "123-123456-1234",
+        "123-123456-1234-1",
+        "123-123456-1234-1234",
+        "123-123456-1234-1-1",
+        "123-123456-1234-1-12345",
+        "123-123456-1234-1234-12345",
+      ])('.isValid("%s") should resolve', async (code) => {
         const modelInstance = GeneralLedgerCoding.build({
           travelAuthorizationId: -1, // not relevant to this test
           amount: 0, // not relevant to this test
           code,
         })
-        let isValid
-        try {
-          await modelInstance.validate()
-          isValid = true
-        } catch (error: any) {
-          expect(error.message).toBe(
-            "Validation error: Code must be in the format: vote (3 characters) - Program (6 characters) - object code (4 digits) - subledger-1 (0-4 characters) - subleger-2 (0-5 characters)"
-          )
-          isValid = false
-        }
-        expect(isValid).toBe(expected)
+
+        await expect(modelInstance.validate()).resolves.to.not.toThrowError()
+      })
+    })
+
+    describe("#code - invalid cases", () => {
+      test.each([
+        "123-123456-1234-",
+        "123-123456-1234-1-",
+        "123-123456-1234-12345",
+        "123-123456-1234-1-123456",
+      ])('.isValid("%s") should reject', async (code) => {
+        const modelInstance = GeneralLedgerCoding.build({
+          travelAuthorizationId: -1, // not relevant to this test
+          amount: 0, // not relevant to this test
+          code,
+        })
+
+        await expect(modelInstance.validate()).rejects.toThrowError(
+          "Validation error: Code must be in the format: vote (3 characters) - Program (6 characters) - object code (4 digits) - subledger-1 (0-4 characters) - subleger-2 (0-5 characters)"
+        )
       })
     })
   })
