65214c2990d5050aecd5b1c3dce9b58dbcacab7a
Marlen Brunner <klondikemarlen@gmail.com>
2024-09-18 15:03:02 -0700

    :recycle: Run initializers via npm script.
    Why? To keep the references in sync.

--- api/bin/boot-app.sh
+++ api/bin/boot-app.sh
@@ -2,8 +2,7 @@
 
 if [ "$NODE_ENV" != "production" ]; then
   # Run initializers in development
-  # Keep in sync with api/tests/global-setup.ts
-  npm run ts-node -- --files src/initializers/index.ts
+  npm run initializers
 else
   # Run initializers in production
   node ./dist/initializers/index.js
--- api/tests/global-setup.ts
+++ api/tests/global-setup.ts
@@ -3,7 +3,7 @@ import { execSync } from "child_process"
 export default async function globalSetup() {
   try {
     // Keep in sync with api/bin/boot-app.sh
-    execSync(`npm run ts-node -- --files src/initializers/index.ts`, { stdio: "inherit" })
+    execSync(`npm run initializers`, { stdio: "inherit" })
   } catch (error) {
     console.error("Failed to run initializers:", error)
     process.exit(1)
