f62a9c95846a0fe6a8099babbc66f8365d48b121
Marlen Brunner <klondikemarlen@gmail.com>
2024-09-19 15:42:28 -0700

    :recycle: Upgrade travel agents page to use script setup form.
    Also fix is admin check.

--- web/src/components/Administration/LookupTableManagement/TravelAgents.vue
+++ web/src/components/Administration/LookupTableManagement/TravelAgents.vue
@@ -12,8 +12,8 @@
       class="px-5 pb-5"
     >
       <v-row class="mx-0 my-1">
-        <new-travel-agent
-          :disabled="!admin"
+        <NewTravelAgent
+          :disabled="!isAdmin"
           class="mt-4 mr-5 ml-auto"
           type="New"
           :agency-info="agencyInfo"
@@ -29,7 +29,7 @@
       >
         <template #item.edit="{ item }">
           <v-row class="mx-0">
-            <new-travel-agent
+            <NewTravelAgent
               type="Edit"
               :agency-info="item"
               @save="saveTravelAgent(item)"
@@ -50,96 +50,85 @@
   </div>
 </template>
 
-<script>
-import Vue from "vue"
+<script setup>
+import { onMounted, ref } from "vue"
 import { TRAVEL_DESK_URL } from "@/urls"
 
 import http from "@/api/http-client"
+import useCurrentUser from "@/use/use-current-user"
+
 import Breadcrumbs from "@/components/Breadcrumbs.vue"
 import NewTravelAgent from "@/components/Administration/LookupTableManagement/TravelAgentsComponents/NewTravelAgent.vue"
 
-export default {
-  name: "TravelAgents",
-  components: {
-    NewTravelAgent,
-    Breadcrumbs,
-  },
-  data() {
-    return {
-      admin: false,
-      agencyInfo: { agencyName: "", agencyInfo: "" },
-      travelAgentsInfo: [],
-      savingData: false,
-      headers: [
-        { text: "Agency Name", value: "agencyName", class: "blue-grey lighten-4" },
-        { text: "Agency Info", value: "agencyInfo", class: "blue-grey lighten-4", sortable: false },
-        {
-          text: "",
-          value: "edit",
-          class: "blue-grey lighten-4",
-          width: "8rem",
-          cellClass: "px-0 mx-0",
-          sortable: false,
-        },
-      ],
-      alertMsg: "",
-    }
-  },
-  async mounted() {
-    await this.initForm()
-  },
-  methods: {
-    async initForm() {
-      this.savingData = true
-      this.admin = Vue.filter("isSystemAdmin")()
-      this.travelAgentsInfo = await this.getTravelAgentsInfo()
-      this.savingData = false
-    },
+const { isAdmin } = useCurrentUser()
 
-    async getTravelAgentsInfo() {
-      this.alertMsg = ""
-      return http
-        .get(`${TRAVEL_DESK_URL}/travel-agents/`)
-        .then((resp) => {
-          return resp.data
-        })
-        .catch((e) => {
-          console.log(e)
-          this.alertMsg = e.response.data
-          this.savingData = false
-        })
-    },
-
-    deleteTravelAgent(item) {
-      this.savingData = true
-      http
-        .delete(`${TRAVEL_DESK_URL}/travel-agents/${item.agencyID}`)
-        .then(() => {
-          //console.log(resp)
-          this.initForm()
-        })
-        .catch((e) => {
-          console.log(e)
-          this.savingData = false
-        })
-    },
-
-    saveTravelAgent(agencyInfo) {
-      this.savingData = true
-      const body = agencyInfo
-
-      http
-        .post(`${TRAVEL_DESK_URL}/travel-agents/${agencyInfo.agencyID}`, body)
-        .then(() => {
-          //console.log(resp)
-          this.initForm()
-        })
-        .catch((e) => {
-          console.log(e)
-          this.savingData = false
-        })
-    },
+const agencyInfo = ref({ agencyName: "", agencyInfo: "" })
+const travelAgentsInfo = ref([])
+const savingData = ref(false)
+const alertMsg = ref("")
+const headers = ref([
+  { text: "Agency Name", value: "agencyName", class: "blue-grey lighten-4" },
+  { text: "Agency Info", value: "agencyInfo", class: "blue-grey lighten-4", sortable: false },
+  {
+    text: "",
+    value: "edit",
+    class: "blue-grey lighten-4",
+    width: "8rem",
+    cellClass: "px-0 mx-0",
+    sortable: false,
   },
+])
+
+onMounted(async () => {
+  await initForm()
+})
+
+async function initForm() {
+  savingData.value = true
+  travelAgentsInfo.value = await getTravelAgentsInfo()
+  savingData.value = false
+}
+
+async function getTravelAgentsInfo() {
+  alertMsg.value = ""
+  return http
+    .get(`${TRAVEL_DESK_URL}/travel-agents/`)
+    .then((resp) => {
+      return resp.data
+    })
+    .catch((e) => {
+      console.log(e)
+      alertMsg.value = e.response.data
+      savingData.value = false
+    })
+}
+
+async function deleteTravelAgent(item) {
+  savingData.value = true
+  http
+    .delete(`${TRAVEL_DESK_URL}/travel-agents/${item.agencyID}`)
+    .then(() => {
+      initForm()
+    })
+    .catch((e) => {
+      console.log(e)
+      savingData.value = false
+    })
+}
+
+async function saveTravelAgent(agencyInfo) {
+  savingData.value = true
+  const body = agencyInfo
+
+  http
+    .post(`${TRAVEL_DESK_URL}/travel-agents/${agencyInfo.agencyID}`, body)
+    .then(() => {
+      initForm()
+    })
+    .catch((e) => {
+      console.log(e)
+      savingData.value = false
+    })
 }
 </script>
 
