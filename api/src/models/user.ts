9af6471c8fce8ce1416b823aad2f49ac9fa98930
Marlen Brunner <klondikemarlen@gmail.com>
2024-09-20 11:29:46 -0700

    :broom: Remove unused associations types from User model.

--- api/src/models/user.ts
+++ api/src/models/user.ts
@@ -2,16 +2,6 @@ import {
   Association,
   CreationOptional,
   DataTypes,
-  HasManyAddAssociationMixin,
-  HasManyAddAssociationsMixin,
-  HasManyCountAssociationsMixin,
-  HasManyCreateAssociationMixin,
-  HasManyGetAssociationsMixin,
-  HasManyHasAssociationMixin,
-  HasManyHasAssociationsMixin,
-  HasManyRemoveAssociationMixin,
-  HasManyRemoveAssociationsMixin,
-  HasManySetAssociationsMixin,
   InferAttributes,
   InferCreationAttributes,
   Model,
@@ -52,41 +42,6 @@ export class User extends Model<InferAttributes<User>, InferCreationAttributes<U
   declare updatedAt: CreationOptional<Date>
 
   // Associations
-  // https://sequelize.org/docs/v6/other-topics/typescript/#usage
-  // https://sequelize.org/docs/v6/core-concepts/assocs/#special-methodsmixins-added-to-instances
-  // https://sequelize.org/api/v7/types/_sequelize_core.index.belongstocreateassociationmixin
-  declare getTravelAuthorizations: HasManyGetAssociationsMixin<TravelAuthorization>
-  declare setTravelAuthorizations: HasManySetAssociationsMixin<
-    TravelAuthorization,
-    TravelAuthorization["id"]
-  >
-  declare hasTravelAuthorization: HasManyHasAssociationMixin<
-    TravelAuthorization,
-    TravelAuthorization["id"]
-  >
-  declare hasTravelAuthorizations: HasManyHasAssociationsMixin<
-    TravelAuthorization,
-    TravelAuthorization["id"]
-  >
-  declare addTravelAuthorization: HasManyAddAssociationMixin<
-    TravelAuthorization,
-    TravelAuthorization["id"]
-  >
-  declare addTravelAuthorizations: HasManyAddAssociationsMixin<
-    TravelAuthorization,
-    TravelAuthorization["id"]
-  >
-  declare removeTravelAuthorization: HasManyRemoveAssociationMixin<
-    TravelAuthorization,
-    TravelAuthorization["id"]
-  >
-  declare removeTravelAuthorizations: HasManyRemoveAssociationsMixin<
-    TravelAuthorization,
-    TravelAuthorization["id"]
-  >
-  declare countTravelAuthorizations: HasManyCountAssociationsMixin
-  declare createTravelAuthorization: HasManyCreateAssociationMixin<TravelAuthorization>
-
   declare travelAuthorizations?: NonAttribute<TravelAuthorization[]>
 
   declare static associations: {
