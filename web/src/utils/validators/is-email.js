8a501b824fd63e937bedfcd8ec742c1e8e6a463e
Marlen Brunner <klondikemarlen@gmail.com>
2024-09-23 10:18:11 -0700

    :recycle: Make isEmail and isPhoneNumber validators non-required.
    This means that if the value is empty, the validator will return true instead of an error message.
    If the fields is "required" it should be validated as "required" separately.

--- web/src/utils/validators/is-email.js
+++ web/src/utils/validators/is-email.js
@@ -1,6 +1,11 @@
+import { isEmpty, isNil, isUndefined } from "lodash"
 import { isEmail as isEmailValidator } from "validator"
 
 export const isEmail = (value) => {
+  if (isNil(value) || isUndefined(value) || isEmpty(value)) {
+    return true
+  }
+
   return isEmailValidator(value) || "Invalid e-mail."
 }
 
--- web/src/utils/validators/is-phone-number.js
+++ web/src/utils/validators/is-phone-number.js
@@ -1,7 +1,13 @@
+import { isNil, isUndefined, isEmpty } from "lodash"
 import { isMobilePhone } from "validator"
 
 export const isPhoneNumber = (value) => {
-  return isMobilePhone(value) || "Invalid phone number"
+  if (isNil(value) || isUndefined(value) || isEmpty(value)) {
+    return true
+  }
+
+  const valueWithoutDashes = value.replace(/-/g, "")
+  return isMobilePhone(valueWithoutDashes) || "Invalid phone number"
 }
 
 export default isPhoneNumber
