12e42d6f2103369894a922bbbaa7a1299d63e0c6
Marlen Brunner <klondikemarlen@gmail.com>
2024-09-24 17:52:51 -0700

    :factory: Standaridze and enhance travel segment factory.
    How? use new helper functions from WRAP project.

--- api/tests/factories/location-factory.ts
+++ api/tests/factories/location-factory.ts
@@ -3,10 +3,11 @@ import { faker } from "@faker-js/faker/locale/en_CA"
 
 import { Location } from "@/models"
 
-export const locationFactory = Factory.define<Location>(({ onCreate }) => {
+export const locationFactory = Factory.define<Location>(({ onCreate, sequence }) => {
   onCreate((location) => location.save())
 
   return Location.build({
+    id: sequence,
     province: faker.location.state(),
     city: faker.location.city(),
   })
--- api/tests/factories/travel-segment-factory.ts
+++ api/tests/factories/travel-segment-factory.ts
@@ -4,33 +4,36 @@ import { Includeable } from "sequelize"
 
 import { TravelSegment } from "@/models"
 import { locationFactory, travelAuthorizationFactory } from "@/factories"
-import { anytime, presence } from "@/factories/helpers"
+import { ensureModelId, presence, saveModelIfNew } from "@/factories/helpers"
 
 type TransientParam = {
   include?: Includeable | Includeable[]
 }
 
 export const travelSegmentFactory = Factory.define<TravelSegment, TransientParam>(
-  ({ associations, params, onCreate, transientParams }) => {
+  ({ associations, params, onCreate, sequence, transientParams }) => {
+    const { id: travelAuthorizationId, model: travelAuthorizationModel } = ensureModelId(
+      params.travelAuthorizationId,
+      associations.travelAuthorization,
+      () => travelAuthorizationFactory.build()
+    )
+
+    const { id: departureLocationId, model: departureLocationModel } = ensureModelId(
+      params.departureLocationId,
+      associations.departureLocation,
+      () => locationFactory.build()
+    )
+
+    const { id: arrivalLocationId, model: arrivalLocationModel } = ensureModelId(
+      params.arrivalLocationId,
+      associations.arrivalLocation,
+      () => locationFactory.build()
+    )
+
     onCreate(async (travelSegment) => {
-      if (travelSegment.travelAuthorizationId === undefined) {
-        const travelAuthorization =
-          associations.travelAuthorization || travelAuthorizationFactory.build()
-        await travelAuthorization.save()
-        travelSegment.travelAuthorizationId = travelAuthorization.id
-      }
-
-      if (travelSegment.departureLocationId === undefined) {
-        const departureLocation = associations.departureLocation || locationFactory.build()
-        await departureLocation.save()
-        travelSegment.departureLocationId = departureLocation.id
-      }
-
-      if (travelSegment.arrivalLocationId === undefined) {
-        const arrivalLocation = associations.arrivalLocation || locationFactory.build()
-        await arrivalLocation.save()
-        travelSegment.arrivalLocationId = arrivalLocation.id
-      }
+      await saveModelIfNew(travelAuthorizationModel, { nested: true })
+      await saveModelIfNew(departureLocationModel)
+      await saveModelIfNew(arrivalLocationModel)
 
       await travelSegment.save()
 
@@ -47,23 +50,16 @@ export const travelSegmentFactory = Factory.define<TravelSegment, TransientParam
       params.modeOfTransport,
       faker.helpers.enumValue(TravelSegment.TravelMethods)
     )
-    const accommodationType = presence(
-      params.accommodationType,
-      faker.helpers.enumValue(TravelSegment.AccommodationTypes)
-    )
 
     return TravelSegment.build({
+      id: sequence,
+      travelAuthorizationId,
+      departureLocationId,
+      arrivalLocationId,
       segmentNumber: faker.number.int({ min: 0, max: 100 }),
-      departureOn: faker.date.soon(),
-      departureTime: anytime(),
       modeOfTransport,
       modeOfTransportOther:
         modeOfTransport === TravelSegment.TravelMethods.OTHER ? faker.hacker.ingverb() : null,
-      accommodationType,
-      accommodationTypeOther:
-        accommodationType === TravelSegment.AccommodationTypes.OTHER
-          ? faker.company.buzzAdjective()
-          : null,
     })
   }
 )
