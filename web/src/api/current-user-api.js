ccbaa37e0a232495ff1fed3f3bf272b40c3b0377
Marlen Brunner <klondikemarlen@gmail.com>
2024-11-27 17:09:16 -0700

    :bug: Add some missing export to current-user-api.

--- web/src/api/current-user-api.js
+++ web/src/api/current-user-api.js
@@ -1,9 +1,15 @@
 import http from "@/api/http-client"
 
+import { USER_ROLES } from "@/api/users-api"
+
+export { USER_ROLES }
+
 /** @typedef {import('@/api/base-api.js').Policy} Policy */
 /** @typedef {import('@/api/users-api.js').UserDetailedView} UserDetailedView */
+/** @typedef {USER_ROLES[keyof USER_ROLES]} UserRoles */
 
 export const currentUserApi = {
+  ROLES: USER_ROLES,
   /**
    * @returns {Promise<{
    *   user: UserDetailedView;
