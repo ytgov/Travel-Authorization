2d05716fc8bb714f37644ee6dc4777cc6ac9fcbc
Marlen Brunner <klondikemarlen@gmail.com>
2024-12-02 15:33:05 -0700

    :cherry_blossom: Make traveler details card more readable.

--- web/src/components/travel-desk-travel-requests/TravelerDetailsCard.vue
+++ web/src/components/travel-desk-travel-requests/TravelerDetailsCard.vue
@@ -3,253 +3,344 @@
     v-if="isNil(travelDeskTravelRequest)"
     type="card"
   />
-  <v-card
-    v-else
-    :loading="isLoading"
-  >
+  <v-card v-else>
     <v-card-title>
       <h3>Traveler Details</h3>
     </v-card-title>
-    <v-card-text>
-      <!--
-        TODO: make this less ugly and hard to read.
-        Vuetify 3 has a "text" variant that looks better than the "outlined" variant + lock icon.
-        An alternative is the DescriptionElement from the WRAP project.
-        See https://github.com/icefoganalytics/wrap/blob/8bcf794a3fdfd5272a578058bcf98663b181d7ab/web/src/components/DescriptionElement.vue
-      -->
-      <v-row class="mt-5 mx-3">
-        <v-col
-          cols="12"
-          md="2"
-        >
-          <v-text-field
-            :value="travelDeskTravelRequest.legalFirstName"
-            label="Legal First Name *"
-            outlined
-            readonly
-            append-icon="mdi-lock"
-          />
-        </v-col>
-        <v-col
-          cols="12"
-          md="2"
-        >
-          <v-text-field
-            :value="travelDeskTravelRequest.legalMiddleName"
-            label="Legal Middle Name"
-            outlined
-            readonly
-            append-icon="mdi-lock"
-          />
-        </v-col>
-        <v-col
-          cols="12"
-          md="2"
-        >
-          <v-text-field
-            :value="travelDeskTravelRequest.legalLastName"
-            label="Legal Last Name *"
-            outlined
-            readonly
-            append-icon="mdi-lock"
-          />
-        </v-col>
-        <v-col
-          cols="12"
-          md="2"
-        >
-          <v-text-field
-            :value="travelDeskTravelRequest.birthDate"
-            label="Birth Date"
-            outlined
-            type="date"
-            readonly
-            append-icon="mdi-lock"
-          />
-        </v-col>
-        <v-col
-          cols="12"
-          md="2"
-        >
-          <v-text-field
-            :value="prettyTravelAuthorizationId"
-            label="Travel Auth"
-            outlined
-            readonly
-            append-icon="mdi-lock"
-          />
-        </v-col>
-      </v-row>
-      <v-row class="mt-0 mx-3">
-        <v-col
-          cols="12"
-          md="2"
-        >
-          <v-text-field
-            :value="travelDeskTravelRequest.strAddress"
-            label="Address *"
-            outlined
-            readonly
-            append-icon="mdi-lock"
-          />
-        </v-col>
-        <v-col
-          cols="12"
-          md="2"
-        >
-          <v-text-field
-            :value="travelDeskTravelRequest.city"
-            label="City *"
-            outlined
-            readonly
-            append-icon="mdi-lock"
-          />
-        </v-col>
-        <v-col
-          cols="12"
-          md="2"
-        >
-          <v-text-field
-            :value="travelDeskTravelRequest.province"
-            label="Province *"
-            outlined
-            readonly
-            append-icon="mdi-lock"
-          />
-        </v-col>
-        <v-col
-          cols="12"
-          md="2"
-        >
-          <v-text-field
-            :value="travelDeskTravelRequest.postalCode"
-            label="Postal Code *"
-            outlined
-            readonly
-            append-icon="mdi-lock"
-          />
-        </v-col>
-        <v-col
-          cols="12"
-          md="2"
-        >
-          <v-checkbox
-            :value="travelDeskTravelRequest.isInternationalTravel"
-            label="International travel"
-            readonly
-            append-icon="mdi-lock"
-          />
-        </v-col>
-      </v-row>
-      <v-row
-        v-if="travelDeskTravelRequest.isInternationalTravel"
-        class="mt-0 mx-3"
-      >
-        <v-col
-          cols="12"
-          md="3"
-        >
-          <v-text-field
-            :value="travelDeskTravelRequest.passportNum"
-            label="Passport Number *"
-            outlined
-            readonly
-            append-icon="mdi-lock"
-          />
-        </v-col>
-        <v-col
-          cols="12"
-          md="3"
-        >
-          <v-text-field
-            :value="travelDeskTravelRequest.passportCountry"
-            label="Passport Country *"
-            outlined
-            readonly
-            append-icon="mdi-lock"
-          />
-        </v-col>
-      </v-row>
 
-      <v-row class="mt-0 mx-3">
-        <v-col
-          cols="12"
-          md="2"
-        >
-          <v-text-field
-            :value="travelDeskTravelRequest.busPhone"
-            label="Business Phone *"
-            outlined
-            readonly
-            append-icon="mdi-lock"
-          />
-        </v-col>
-        <v-col
-          cols="12"
-          md="2"
-        >
-          <v-text-field
-            :value="travelDeskTravelRequest.busEmail"
-            label="Business Email *"
-            outlined
-            readonly
-            append-icon="mdi-lock"
-          />
-        </v-col>
-        <v-col
-          cols="12"
-          md="3"
-        >
-          <v-checkbox
-            :value="travelDeskTravelRequest.travelContact"
-            label="Contact information different for travel"
-            readonly
-            append-icon="mdi-lock"
-          />
-        </v-col>
-      </v-row>
-      <v-row
-        v-if="travelDeskTravelRequest.travelContact"
-        class="mt-0 mx-3"
-      >
-        <v-col
-          cols="12"
-          md="2"
-        >
-          <v-text-field
-            :value="travelDeskTravelRequest.travelPhone"
-            label="Travel Phone *"
-            outlined
-            readonly
-            append-icon="mdi-lock"
-          />
-        </v-col>
-        <v-col
-          cols="12"
-          md="3"
-        >
-          <v-text-field
-            :value="travelDeskTravelRequest.travelEmail"
-            label="Travel Email *"
-            outlined
-            readonly
-            append-icon="mdi-lock"
-          />
-        </v-col>
-      </v-row>
-      <v-row class="mt-0 mx-3">
-        <v-col cols="12">
-          <v-textarea
-            :value="travelDeskTravelRequest?.additionalInformation"
-            label="Additional Information"
-            outlined
-            auto-grow
-            counter
-            readonly
-            append-icon="mdi-lock"
-          />
-        </v-col>
-      </v-row>
+    <v-divider></v-divider>
+
+    <v-card-text class="body-1 text--primary">
+      <dl>
+        <v-row>
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <dt class="font-weight-bold">
+              <v-icon
+                size="18"
+                class="mr-2"
+                aria-hidden="true"
+                >mdi-account-details</v-icon
+              >
+              <span>Legal First Name</span>
+            </dt>
+            <dd class="mt-2 font-weight-medium">
+              {{ travelDeskTravelRequest.legalFirstName }}
+            </dd>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <dt class="d-flex align-center font-weight-bold">
+              <v-icon
+                size="18"
+                class="mr-2"
+                aria-hidden="true"
+                >mdi-account-details</v-icon
+              >
+              <span>Legal Middle Name</span>
+            </dt>
+            <dd class="mt-2 font-weight-medium">
+              {{ travelDeskTravelRequest.legalMiddleName || "N/A" }}
+            </dd>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <dt class="d-flex align-center font-weight-bold">
+              <v-icon
+                size="18"
+                class="mr-2"
+                aria-hidden="true"
+                >mdi-account-details</v-icon
+              >
+              <span>Legal Last Name</span>
+            </dt>
+            <dd class="mt-2 font-weight-medium">
+              {{ travelDeskTravelRequest.legalLastName }}
+            </dd>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <dt class="d-flex align-center font-weight-bold">
+              <v-icon
+                size="18"
+                class="mr-2"
+                aria-hidden="true"
+                >mdi-calendar</v-icon
+              >
+              <span>Birth Date</span>
+            </dt>
+            <dd class="mt-2 font-weight-medium">{{ travelDeskTravelRequest.birthDate }}</dd>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <dt class="d-flex align-center font-weight-bold">
+              <v-icon
+                size="18"
+                class="mr-2"
+                aria-hidden="true"
+                >mdi-key</v-icon
+              >
+              <span>Travel Auth</span>
+            </dt>
+            <dd class="mt-2 font-weight-medium">{{ prettyTravelAuthorizationId }}</dd>
+          </v-col>
+        </v-row>
+
+        <v-divider class="my-4"></v-divider>
+
+        <v-row>
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <dt class="d-flex align-center font-weight-bold">
+              <v-icon
+                size="18"
+                class="mr-2"
+                aria-hidden="true"
+                >mdi-phone</v-icon
+              >
+              <span>Business Phone</span>
+            </dt>
+            <dd class="mt-2 font-weight-medium">{{ travelDeskTravelRequest.busPhone }}</dd>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <dt class="d-flex align-center font-weight-bold">
+              <v-icon
+                size="18"
+                class="mr-2"
+                aria-hidden="true"
+                >mdi-email</v-icon
+              >
+              <span>Business Email</span>
+            </dt>
+            <dd class="mt-2 font-weight-medium">{{ travelDeskTravelRequest.busEmail }}</dd>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <dt class="d-flex align-center font-weight-bold">
+              <v-icon
+                size="18"
+                class="mr-2"
+                aria-hidden="true"
+                >mdi-airplane</v-icon
+              >
+              <span>International Travel</span>
+            </dt>
+            <dd class="mt-2 font-weight-medium">
+              {{ travelDeskTravelRequest.isInternationalTravel ? "Yes" : "No" }}
+            </dd>
+          </v-col>
+        </v-row>
+
+        <v-row v-if="travelDeskTravelRequest.isInternationalTravel">
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <dt class="d-flex align-center font-weight-bold">
+              <v-icon
+                size="18"
+                class="mr-2"
+                aria-hidden="true"
+                >mdi-passport</v-icon
+              >
+              <span>Passport Number</span>
+            </dt>
+            <dd class="mt-2 font-weight-medium">
+              {{ travelDeskTravelRequest.passportNum }}
+            </dd>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <dt class="d-flex align-center font-weight-bold">
+              <v-icon
+                size="18"
+                class="mr-2"
+                aria-hidden="true"
+                >mdi-flag</v-icon
+              >
+              <span>Passport Country</span>
+            </dt>
+            <dd class="mt-2 font-weight-medium">
+              {{ travelDeskTravelRequest.passportCountry }}
+            </dd>
+          </v-col>
+        </v-row>
+
+        <v-divider class="my-4"></v-divider>
+
+        <v-row>
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <dt class="d-flex align-center font-weight-bold">
+              <v-icon
+                size="18"
+                class="mr-2"
+                aria-hidden="true"
+                >mdi-home</v-icon
+              >
+              <span>Address</span>
+            </dt>
+            <dd class="mt-2 font-weight-medium">{{ travelDeskTravelRequest.strAddress }}</dd>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <dt class="d-flex align-center font-weight-bold">
+              <v-icon
+                size="18"
+                class="mr-2"
+                aria-hidden="true"
+                >mdi-city</v-icon
+              >
+              <span>City</span>
+            </dt>
+            <dd class="mt-2 font-weight-medium">{{ travelDeskTravelRequest.city }}</dd>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <dt class="d-flex align-center font-weight-bold">
+              <v-icon
+                size="18"
+                class="mr-2"
+                aria-hidden="true"
+                >mdi-map</v-icon
+              >
+              <span>Province</span>
+            </dt>
+            <dd class="mt-2 font-weight-medium">{{ travelDeskTravelRequest.province }}</dd>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <dt class="d-flex align-center font-weight-bold">
+              <v-icon
+                size="18"
+                class="mr-2"
+                aria-hidden="true"
+                >mdi-mailbox</v-icon
+              >
+              <span>Postal Code</span>
+            </dt>
+            <dd class="mt-2 font-weight-medium">{{ travelDeskTravelRequest.postalCode }}</dd>
+          </v-col>
+        </v-row>
+
+        <v-divider class="my-4"></v-divider>
+
+        <v-row>
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <dt class="d-flex align-center font-weight-bold">
+              <v-icon
+                size="18"
+                class="mr-2"
+                aria-hidden="true"
+                >mdi-account-switch</v-icon
+              >
+              <span>Different Travel Contact</span>
+            </dt>
+            <dd class="mt-2 font-weight-medium">
+              {{ travelDeskTravelRequest.travelContact ? "Yes" : "No" }}
+            </dd>
+          </v-col>
+        </v-row>
+
+        <v-row v-if="travelDeskTravelRequest.travelContact">
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <dt class="d-flex align-center font-weight-bold">
+              <v-icon
+                size="18"
+                class="mr-2"
+                aria-hidden="true"
+                >mdi-phone</v-icon
+              >
+              <span>Travel Phone</span>
+            </dt>
+            <dd class="mt-2 font-weight-medium">{{ travelDeskTravelRequest.travelPhone }}</dd>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <dt class="d-flex align-center font-weight-bold">
+              <v-icon
+                size="18"
+                class="mr-2"
+                aria-hidden="true"
+                >mdi-email</v-icon
+              >
+              <span>Travel Email</span>
+            </dt>
+            <dd class="mt-2 font-weight-medium">{{ travelDeskTravelRequest.travelEmail }}</dd>
+          </v-col>
+        </v-row>
+
+        <v-divider class="my-4"></v-divider>
+
+        <v-row>
+          <v-col cols="12">
+            <dt class="d-flex align-center font-weight-bold">
+              <v-icon
+                size="18"
+                class="mr-2"
+                aria-hidden="true"
+                >mdi-text</v-icon
+              >
+              <span>Additional Information</span>
+            </dt>
+            <dd class="mt-2 font-weight-medium">
+              {{ travelDeskTravelRequest?.additionalInformation || "N/A" }}
+            </dd>
+          </v-col>
+        </v-row>
+      </dl>
     </v-card-text>
   </v-card>
 </template>
@@ -274,5 +365,3 @@ const prettyTravelAuthorizationId = computed(() => {
   return travelDeskTravelRequest.value.travelAuthorizationId.toString().padStart(5, "0")
 })
 </script>
-
-<style scoped></style>
