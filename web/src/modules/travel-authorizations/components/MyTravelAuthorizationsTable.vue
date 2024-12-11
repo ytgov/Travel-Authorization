506a72e83d089ca9420783a0d7822abef130b33a
Marlen Brunner <klondikemarlen@gmail.com>
2024-12-11 11:56:10 -0700

    :recycle: Standardize My Travel Authorizations table.
    Why? I need to update the redirect and call-to-action logic, and its easier using
    script setup syntax.

--- web/src/modules/travel-authorizations/components/MyTravelAuthorizationsTable.vue
+++ web/src/modules/travel-authorizations/components/MyTravelAuthorizationsTable.vue
@@ -1,83 +1,86 @@
 <template>
-  <div>
-    <v-data-table
-      :headers="headers"
-      :items="items"
-      :loading="isLoading"
-      :items-per-page.sync="perPage"
-      :page.sync="page"
-      :server-items-length="totalCount"
-      class="elevation-2"
-      @click:row="goToFormDetails"
-    >
-      <template #top>
-        <DeleteTravelAuthorizationDialog
-          ref="deleteDialog"
-          @deleted="refresh"
-        />
+  <v-data-table
+    :headers="headers"
+    :items="travelAuthorizations"
+    :loading="isLoading"
+    :items-per-page.sync="perPage"
+    :page.sync="page"
+    :server-items-length="totalCount"
+    @click:row="goToFormDetails"
+  >
+    <template #top>
+      <DeleteTravelAuthorizationDialog
+        ref="deleteDialog"
+        @deleted="refresh"
+      />
+    </template>
+    <template #item.phase="{ value }">
+      <span>{{ formatPhase(value) }}</span>
+    </template>
+    <template #item.finalDestination="{ value }">
+      <span>{{ formatLocation(value) }}</span>
+    </template>
+    <template #item.departingAt="{ value }">
+      <span>{{ formatDateWrapper(value) }}</span>
+    </template>
+    <template #item.returningAt="{ value }">
+      <span>{{ formatDateWrapper(value) }}</span>
+    </template>
+    <template #item.status="{ value }">
+      <span>{{ formatStatus(value) }}</span>
+    </template>
+    <template #item.action="{ value: actions, item }">
+      <template v-if="isEmpty(actions)">
+        <!-- no action: this is a valid state -->
       </template>
-      <template #item.phase="{ value }">
-        <span>{{ formatPhase(value) }}</span>
-      </template>
-      <template #item.finalDestination="{ value }">
-        <span>{{ formatLocation(value) }}</span>
-      </template>
-      <template #item.departingAt="{ value }">
-        <span>{{ formatDate(value) }}</span>
-      </template>
-      <template #item.returningAt="{ value }">
-        <span>{{ formatDate(value) }}</span>
-      </template>
-      <template #item.status="{ value }">
-        <span>{{ formatStatus(value) }}</span>
-      </template>
-      <template #item.action="{ value: actions, item }">
-        <template v-if="isEmpty(actions)">
-          <!-- no action: this is a valid state -->
-        </template>
-        <v-btn
-          v-else-if="actions.includes('delete')"
-          class="ml-2"
-          color="error"
-          @click.stop="showDeleteDialog(item)"
-          >Delete</v-btn
-        >
-        <SubmitTravelDeskRequestButton
-          v-else-if="actions.includes('submit_travel_desk_request')"
-          :travel-authorization-id="item.id"
-        />
-        <TravelDeskOptionsProvidedButton
-          v-else-if="actions.includes('travel_desk_options_provided')"
-          :travel-authorization-id="item.id"
-        />
-        <SubmitExpenseClaimButton
-          v-else-if="actions.includes('submit_expense_claim')"
-          :travel-authorization-id="item.id"
-        />
-        <ViewItineraryButton
-          v-else-if="actions.includes('view_itinerary')"
-          :travel-authorization-id="item.id"
-        />
-        <AddExpenseButton
-          v-else-if="actions.includes('add_expense')"
-          :travel-authorization-id="item.id"
-        />
-        <SubmitPoolVehicleRequestButton
-          v-else-if="actions.includes('submit_pool_vehicle_request')"
-          :travel-authorization-id="item.id"
-        />
-        <span v-else> ERROR: unknown actions: {{ actions }}</span>
-      </template>
-    </v-data-table>
-  </div>
+      <v-btn
+        v-else-if="actions.includes('delete')"
+        class="ml-2"
+        color="error"
+        @click.stop="showDeleteDialog(item)"
+        >Delete</v-btn
+      >
+      <!-- Add call-to-action for this page: web/src/pages/my-travel-requests/AwaitingRequestBookingPage.vue -->
+      <SubmitTravelDeskRequestButton
+        v-else-if="actions.includes('submit_travel_desk_request')"
+        :travel-authorization-id="item.id"
+      />
+      <TravelDeskOptionsProvidedButton
+        v-else-if="actions.includes('travel_desk_options_provided')"
+        :travel-authorization-id="item.id"
+      />
+      <SubmitExpenseClaimButton
+        v-else-if="actions.includes('submit_expense_claim')"
+        :travel-authorization-id="item.id"
+      />
+      <ViewItineraryButton
+        v-else-if="actions.includes('view_itinerary')"
+        :travel-authorization-id="item.id"
+      />
+      <AddExpenseButton
+        v-else-if="actions.includes('add_expense')"
+        :travel-authorization-id="item.id"
+      />
+      <SubmitPoolVehicleRequestButton
+        v-else-if="actions.includes('submit_pool_vehicle_request')"
+        :travel-authorization-id="item.id"
+      />
+      <span v-else> ERROR: unknown actions: {{ actions }}</span>
+    </template>
+  </v-data-table>
 </template>
 
-<script>
-import { mapActions, mapGetters } from "vuex"
+<script setup>
+import { computed, onMounted, ref } from "vue"
+import { useRoute, useRouter } from "vue2-helpers/vue-router"
 import { isNil, isEmpty } from "lodash"
-import { DateTime } from "luxon"
 
+import { useI18n } from "@/plugins/vue-i18n-plugin"
+import formatDate from "@/utils/format-date"
 import { STATUSES as TRAVEL_AUTHORIZATION_STATUSES } from "@/api/travel-authorizations-api"
+import useRouteQuery, { integerTransformer } from "@/use/utils/use-route-query"
+import useCurrentUser from "@/use/use-current-user"
+import useTravelAuthorizations from "@/use/use-travel-authorizations"
 
 import AddExpenseButton from "@/modules/travel-authorizations/components/my-travel-authorizations-table/AddExpenseButton.vue"
 import DeleteTravelAuthorizationDialog from "@/modules/travel-authorizations/components/my-travel-authorizations-table/DeleteTravelAuthorizationDialog.vue"
@@ -87,133 +90,127 @@ import SubmitPoolVehicleRequestButton from "@/modules/travel-authorizations/comp
 import SubmitTravelDeskRequestButton from "@/modules/travel-authorizations/components/my-travel-authorizations-table/SubmitTravelDeskRequestButton.vue"
 import ViewItineraryButton from "@/modules/travel-authorizations/components/my-travel-authorizations-table/ViewItineraryButton.vue"
 
-export default {
-  name: "MyTravelAuthorizationsTable",
-  components: {
-    AddExpenseButton,
-    DeleteTravelAuthorizationDialog,
-    TravelDeskOptionsProvidedButton,
-    SubmitExpenseClaimButton,
-    SubmitPoolVehicleRequestButton,
-    SubmitTravelDeskRequestButton,
-    ViewItineraryButton,
+const headers = ref([
+  {
+    text: "Phase",
+    value: "phase",
   },
-  data: () => ({
-    headers: [
-      {
-        text: "Phase",
-        value: "phase",
-      },
-      {
-        text: "Location",
-        value: "finalDestination",
-      },
-      {
-        text: "Description",
-        value: "eventName",
-      },
-      {
-        text: "Start Date",
-        value: "departingAt",
-      },
-      {
-        text: "End Date",
-        value: "returningAt",
-      },
-      {
-        text: "Travel Auth Status",
-        value: "status",
-      },
-      {
-        text: "Travel Action",
-        value: "action",
-      },
-    ],
-    perPage: 10,
-    page: 1,
-  }),
-  computed: {
-    ...mapGetters("current/user", {
-      currentUserId: "id",
-    }),
-    ...mapGetters("travelAuthorizations", ["items", "totalCount", "isLoading"]),
+  {
+    text: "Location",
+    value: "finalDestination",
   },
-  watch: {
-    page() {
-      this.refresh()
-    },
-    perPage() {
-      this.refresh()
-    },
+  {
+    text: "Description",
+    value: "eventName",
   },
-  async mounted() {
-    await this.ensureCurrentUser()
-    await this.refresh()
-    this.showDeleteDialogForRouteQuery()
+  {
+    text: "Start Date",
+    value: "departingAt",
   },
-  methods: {
-    ...mapActions("current/user", {
-      ensureCurrentUser: "ensure",
-    }),
-    ...mapActions("travelAuthorizations", ["ensure"]),
-    isEmpty,
-    refresh() {
-      return this.ensure({
-        where: { userId: this.currentUserId },
-        page: this.page,
-        perPage: this.perPage,
-      })
-    },
-    goToFormDetails(travelAuthorization) {
-      const travelAuthorizationId = travelAuthorization.id
-      if (travelAuthorization.status === TRAVEL_AUTHORIZATION_STATUSES.DRAFT) {
-        this.$router.push({
-          name: "my-travel-requests/details/DetailsEditPurposePage",
-          params: { travelAuthorizationId },
-        })
-      } else if (travelAuthorization.status === TRAVEL_AUTHORIZATION_STATUSES.SUBMITTED) {
-        this.$router.push({
-          name: "my-travel-requests/AwaitingApprovalPage",
-          params: { travelAuthorizationId },
-        })
-      } else {
-        this.$router.push({
-          name: "my-travel-requests/details/DetailsPage",
-          params: { travelAuthorizationId },
-        })
-      }
-    },
-    showDeleteDialog(item) {
-      this.$refs.deleteDialog.show(item)
-    },
-    showDeleteDialogForRouteQuery() {
-      const itemId = parseInt(this.$route.query.showDelete)
-      if (isNaN(itemId)) return
+  {
+    text: "End Date",
+    value: "returningAt",
+  },
+  {
+    text: "Travel Auth Status",
+    value: "status",
+  },
+  {
+    text: "Travel Action",
+    value: "action",
+  },
+])
 
-      const item = this.items.find((item) => item.id === itemId)
-      if (!item) return
+const page = useRouteQuery("page", "1", {
+  transform: integerTransformer,
+})
 
-      this.showDeleteDialog(item)
-    },
-    formatDate(value) {
-      if (isNil(value)) return "Unknown"
+const perPage = useRouteQuery("perPage", "10", {
+  transform: integerTransformer,
+})
 
-      const date = DateTime.fromISO(value, { zone: "utc" })
-      return date.toFormat("dd-LLL-yyyy")
-    },
-    formatLocation(value) {
-      if (isNil(value) || isNil(value.city)) return "Unknown"
+const { currentUser } = useCurrentUser()
+const currentUserId = computed(() => currentUser.value.id)
 
-      return value.city
-    },
-    formatStatus(value) {
-      return this.$t(`global.status.${value}`, { $default: "Unknown" })
-    },
-    formatPhase(value) {
-      return this.$t(`global.phase.${value}`, { $default: "Unknown" })
-    },
-  },
+const travelAuthorizationsQuery = computed(() => ({
+  where: { userId: currentUserId.value },
+  page: page.value,
+  perPage: perPage.value,
+}))
+const { travelAuthorizations, totalCount, isLoading, refresh } =
+  useTravelAuthorizations(travelAuthorizationsQuery)
+
+onMounted(() => {
+  showDeleteDialogForRouteQuery()
+})
+
+const router = useRouter()
+
+function goToFormDetails(travelAuthorization) {
+  const travelAuthorizationId = travelAuthorization.id
+  if (travelAuthorization.status === TRAVEL_AUTHORIZATION_STATUSES.DRAFT) {
+    router.push({
+      name: "my-travel-requests/details/DetailsEditPurposePage",
+      params: { travelAuthorizationId },
+    })
+  } else if (travelAuthorization.status === TRAVEL_AUTHORIZATION_STATUSES.SUBMITTED) {
+    router.push({
+      name: "my-travel-requests/AwaitingApprovalPage",
+      params: { travelAuthorizationId },
+    })
+    // TODO: handle redirect to this page web/src/pages/my-travel-requests/AwaitingRequestBookingPage.vue
+  } else {
+    router.push({
+      name: "my-travel-requests/details/DetailsPage",
+      params: { travelAuthorizationId },
+    })
+  }
 }
+
+/** @type {import("vue").Ref<InstanceType<typeof DeleteTravelAuthorizationDialog> | null>} */
+const deleteDialog = ref(null)
+
+function showDeleteDialog(item) {
+  deleteDialog.value?.show(item)
+}
+
+const route = useRoute()
+
+function showDeleteDialogForRouteQuery() {
+  const itemId = parseInt(route.query.showDelete)
+  if (isNaN(itemId)) return
+
+  const item = travelAuthorizations.value.find((item) => item.id === itemId)
+  if (!item) return
+
+  showDeleteDialog(item)
+}
+
+function formatDateWrapper(value) {
+  if (isNil(value)) return "Unknown"
+
+  return formatDate(value, "dd-LLL-yyyy")
+}
+
+function formatLocation(value) {
+  if (isNil(value) || isNil(value.city)) return "Unknown"
+
+  return value.city
+}
+
+const { t } = useI18n()
+
+function formatStatus(value) {
+  return t(`global.status.${value}`, { $default: "Unknown" })
+}
+
+function formatPhase(value) {
+  return t(`global.phase.${value}`, { $default: "Unknown" })
+}
+
+defineExpose({
+  refresh,
+})
 </script>
 
 <style scoped></style>
