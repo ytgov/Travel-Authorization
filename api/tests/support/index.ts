bbff76715965a0c183c29e4168d6deeb2fff8001
Marlen Brunner <klondikemarlen@gmail.com>
2025-01-16 12:22:51 -0700

    :heavy_check_mark: Fix estimate generation controller tests.
    Rework controller with newer patterns.
    Add test with custom log level support helper to make it quieter when testing error states.

--- api/tests/support/index.ts
+++ api/tests/support/index.ts
@@ -1,3 +1,4 @@
 export { mockCurrentUser } from "./mock-current-user"
 export { mockedAxios } from "./mock-axios"
 export { request } from "./request"
+export { testWithCustomLogLevel } from "./test-with-custom-log-level"
