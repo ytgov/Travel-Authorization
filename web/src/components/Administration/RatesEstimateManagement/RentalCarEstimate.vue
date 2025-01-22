478d31db1bb1b1c8176ed40a102c4b5fb266044b
Marlen Brunner <klondikemarlen@gmail.com>
2025-01-22 16:55:34 -0700

    :broom: Lint rental car estimates page to current standards.

--- web/src/components/Administration/RatesEstimateManagement/RentalCarEstimate.vue
+++ web/src/components/Administration/RatesEstimateManagement/RentalCarEstimate.vue
@@ -2,16 +2,35 @@
   <v-app>
     <div fluid>
       <H1>Rental Car Estimates</H1>
-      <Breadcrumbs class="mb-5"/>
-      <v-data-table :headers="headers" :items="vehicles" class="elevation-1">
-        <template v-slot:top>
+      <Breadcrumbs class="mb-5" />
+      <v-data-table
+        :headers="headers"
+        :items="vehicles"
+        class="elevation-1"
+      >
+        <template #top>
           <v-toolbar flat>
             <v-toolbar-title>Rental Vehicle Cost Estimate</v-toolbar-title>
-            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-divider
+              class="mx-4"
+              inset
+              vertical
+            ></v-divider>
             <v-spacer></v-spacer>
-            <v-dialog v-model="dialog" max-width="500px">
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on"> New Item </v-btn>
+            <v-dialog
+              v-model="dialog"
+              max-width="500px"
+            >
+              <template #activator="{ on, attrs }">
+                <v-btn
+                  color="primary"
+                  dark
+                  class="mb-2"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  New Item
+                </v-btn>
               </template>
               <v-card>
                 <v-card-title>
@@ -20,52 +39,114 @@
 
                 <v-card-text>
                   <v-row>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field dense outlined v-model="editedItem.type" label="Type"></v-text-field>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-model="editedItem.type"
+                        dense
+                        outlined
+                        label="Type"
+                      ></v-text-field>
                     </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field dense outlined v-model="editedItem.cost" label="Cost per km"></v-text-field>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-model="editedItem.cost"
+                        dense
+                        outlined
+                        label="Cost per km"
+                      ></v-text-field>
                     </v-col>
                   </v-row>
                 </v-card-text>
 
                 <v-card-actions>
                   <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="close"> Cancel </v-btn>
-                  <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+                  <v-btn
+                    color="blue darken-1"
+                    text
+                    @click="close"
+                  >
+                    Cancel
+                  </v-btn>
+                  <v-btn
+                    color="blue darken-1"
+                    text
+                    @click="save"
+                  >
+                    Save
+                  </v-btn>
                 </v-card-actions>
               </v-card>
             </v-dialog>
-            <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-dialog
+              v-model="dialogDelete"
+              max-width="500px"
+            >
               <v-card>
-                <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+                <v-card-title class="text-h5"
+                  >Are you sure you want to delete this item?</v-card-title
+                >
                 <v-card-actions>
                   <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                  <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                  <v-btn
+                    color="blue darken-1"
+                    text
+                    @click="closeDelete"
+                    >Cancel</v-btn
+                  >
+                  <v-btn
+                    color="blue darken-1"
+                    text
+                    @click="deleteItemConfirm"
+                    >OK</v-btn
+                  >
                   <v-spacer></v-spacer>
                 </v-card-actions>
               </v-card>
             </v-dialog>
           </v-toolbar>
         </template>
-        <template v-slot:[`item.actions`]="{ item }">
-          <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
-          <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+        <template #item.actions="{ item }">
+          <v-icon
+            small
+            class="mr-2"
+            @click="editItem(item)"
+          >
+            mdi-pencil
+          </v-icon>
+          <v-icon
+            small
+            @click="deleteItem(item)"
+          >
+            mdi-delete
+          </v-icon>
         </template>
-        <template v-slot:no-data>
-          <v-btn color="primary" @click="initialize"> Reset </v-btn>
+        <template #no-data>
+          <v-btn
+            color="primary"
+            @click="initialize"
+          >
+            Reset
+          </v-btn>
         </template>
       </v-data-table>
     </div>
   </v-app>
 </template>
 <script>
-import Breadcrumbs from "../../Breadcrumbs.vue";
+import Breadcrumbs from "@/components/Breadcrumbs.vue"
+
 export default {
-  name: "flightEstimates",
+  name: "RentalCarEstimate",
   components: {
-    Breadcrumbs
+    Breadcrumbs,
   },
   data: () => ({
     dialog: false,
@@ -75,42 +156,42 @@ export default {
         text: "Vehicle Type",
         align: "start",
         sortable: false,
-        value: "type"
+        value: "type",
       },
       {
         text: "Cost",
-        value: "cost"
-      }
+        value: "cost",
+      },
     ],
     vehicles: [],
     editedIndex: -1,
     editedItem: {
       type: "",
-      cost: 0
+      cost: 0,
     },
     defaultItem: {
       type: "",
-      cost: 0
-    }
+      cost: 0,
+    },
   }),
 
   computed: {
     formTitle() {
-      return this.editedIndex === -1 ? "New Item" : "Edit Item";
-    }
+      return this.editedIndex === -1 ? "New Item" : "Edit Item"
+    },
   },
 
   watch: {
     dialog(val) {
-      val || this.close();
+      val || this.close()
     },
     dialogDelete(val) {
-      val || this.closeDelete();
-    }
+      val || this.closeDelete()
+    },
   },
 
   created() {
-    this.initialize();
+    this.initialize()
   },
 
   methods: {
@@ -118,64 +199,64 @@ export default {
       this.vehicles = [
         {
           type: "Compact Car",
-          cost: 150
+          cost: 150,
         },
         {
           type: "SUV",
-          cost: 237
+          cost: 237,
         },
         {
           type: "Van",
-          cost: 262
+          cost: 262,
         },
         {
           type: "Truck",
-          cost: 262
-        }
-      ];
+          cost: 262,
+        },
+      ]
     },
 
     editItem(item) {
-      this.editedIndex = this.vehicles.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialog = true;
+      this.editedIndex = this.vehicles.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
     },
 
     deleteItem(item) {
-      this.editedIndex = this.vehicles.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialogDelete = true;
+      this.editedIndex = this.vehicles.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialogDelete = true
     },
 
     deleteItemConfirm() {
-      this.vehicles.splice(this.editedIndex, 1);
-      this.closeDelete();
+      this.vehicles.splice(this.editedIndex, 1)
+      this.closeDelete()
     },
 
     close() {
-      this.dialog = false;
+      this.dialog = false
       this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
     },
 
     closeDelete() {
-      this.dialogDelete = false;
+      this.dialogDelete = false
       this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
     },
 
     save() {
       if (this.editedIndex > -1) {
-        Object.assign(this.vehicles[this.editedIndex], this.editedItem);
+        Object.assign(this.vehicles[this.editedIndex], this.editedItem)
       } else {
-        this.vehicles.push(this.editedItem);
+        this.vehicles.push(this.editedItem)
       }
-      this.close();
-    }
-  }
-};
+      this.close()
+    },
+  },
+}
 </script>
