8ac1bf482b171812b1a6bb7acc231905d5b971f4
Marlen Brunner <klondikemarlen@gmail.com>
2025-01-31 12:19:06 -0700

    :art: Standarize name of model file to singular.

--- api/src/integrations/trav-com-integration/models/index.ts
+++ api/src/integrations/trav-com-integration/models/index.ts
@@ -1,3 +1,3 @@
-export { type ArInvoice, type ArInvoiceNoHealthRaw } from "./ar-invoices"
+export { type ArInvoice, type ArInvoiceNoHealthRaw } from "./ar-invoice"
 export { type ArInvoiceDetail, type ArInvoiceDetailNoHealthRaw } from "./ar-invoice-details"
 export { type Segment, type SegmentNoHealthRaw } from "./segments"
