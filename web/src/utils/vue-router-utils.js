b6e11d094a5baceabab6acec93af6b73eebeafe0
Marlen Brunner <klondikemarlen@gmail.com>
2025-02-21 14:28:21 -0700

    :loud_sound: Mark some early concept code as deprecated.
    Prefer doing route param conversion in the page layer as this is more flexible.

--- web/src/utils/vue-router-utils.js
+++ web/src/utils/vue-router-utils.js
@@ -1,16 +1,17 @@
 import { cloneDeep } from "lodash"
 
-/*
-CONSIDER: building chainable caster so you can do
-cast("formId", parseInt).cast("foo", barFunction)
-
-Currently you would have to do:
-(route) => {
-   const route2 = cast('formId', parseInt)(route)
-   return cast('foo', barFunction)(route2)
-}
-*/
-
+/**
+ * CONSIDER: building chainable caster so you can do
+ * cast("formId", parseInt).cast("foo", barFunction)
+ *
+ * Currently you would have to do:
+ * (route) => {
+ *    const route2 = cast('formId', parseInt)(route)
+ *    return cast('foo', barFunction)(route2)
+ * }
+ *
+ * @deprecated - prefer converting prop inside of page layer component as this is more flexible
+ */
 export function cast(param, converter) {
   return (route) => {
     const props = cloneDeep(route.params)
