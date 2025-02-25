110dc916efa852d35c989c7232a511a0131a452f
Marlen Brunner <klondikemarlen@gmail.com>
2025-02-24 17:41:30 -0700

    :bug: Fix flight reconciliation create service now that we're tracking more data.

--- api/src/services/flight-reconciliations/create-service.ts
+++ api/src/services/flight-reconciliations/create-service.ts
@@ -14,17 +14,43 @@ export class CreateService extends BaseService {
   }
 
   async perform(): Promise<FlightReconciliation> {
-    const { externalTravComIdentifier, ...optionalAttributes } =
-      this.attributes
+    const {
+      externalTravComIdentifier,
+      invoiceDetailSellingFare,
+      invoiceDetailVendorName,
+      invoiceDetailComputedTravelerFirstName,
+      invoiceDetailComputedTravelerLastName,
+      ...optionalAttributes
+    } = this.attributes
 
     if (isNil(externalTravComIdentifier)) {
       throw new Error("External TravCom identifier is required.")
+    }
+
+    if (isNil(invoiceDetailSellingFare)) {
+      throw new Error("Invoice detail selling fare is required.")
+    }
+
+    if (isNil(invoiceDetailVendorName)) {
+      throw new Error("Invoice detail vendor name is required.")
+    }
+
+    if (isNil(invoiceDetailComputedTravelerFirstName)) {
+      throw new Error("Invoice detail computed traveler first name is required.")
+    }
+
+    if (isNil(invoiceDetailComputedTravelerLastName)) {
+      throw new Error("Invoice detail computed traveler last name is required.")
     }
 
     const flightReconciliation = await FlightReconciliation.create({
       ...optionalAttributes,
       reconcilerId: this.currentUser.id,
       externalTravComIdentifier,
+      invoiceDetailSellingFare,
+      invoiceDetailVendorName,
+      invoiceDetailComputedTravelerFirstName,
+      invoiceDetailComputedTravelerLastName,
     })
     return flightReconciliation
   }
